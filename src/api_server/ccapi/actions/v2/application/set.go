23c537aed89534c8b1085c2cba61117a36376ad9
rentiansheng <rentiansheng@163.com>
2018-04-08 14:29:04 +0800

    del set ChnName property

--- src/api_server/ccapi/actions/v2/application/set.go
+++ src/api_server/ccapi/actions/v2/application/set.go
@@ -1,15 +1,15 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package application
 
 import (
@@ -234,7 +234,7 @@ func (cli *setAction) AddSet(req *restful.Request, resp *restful.Response) {
 			reqParam[k] = v
 		}
 	}
-
+	delete(reqParam, "ChnName")
 	reqParam, err = logics.AutoInputV3Filed(reqParam, common.BKInnerObjIDSet, set.CC.TopoAPI(), req.Request.Header)
 	blog.Debug("add set reqParam:%v", reqParam)
 
@@ -345,6 +345,8 @@ func (cli *setAction) UpdateSet(req *restful.Request, resp *restful.Response) {
 	if len(formData["ServiceStatus"]) > 0 {
 		reqData[common.BKSetStatusField] = status
 	}
+
+	delete(reqData, "ChnName")
 
 	reqParam["data"] = reqData
 
