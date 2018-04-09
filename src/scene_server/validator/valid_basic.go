e95d96faab91e26739493f9852d113e473598a63
owenlxu <owenlxu@tencent.com>
2018-04-09 14:54:11 +0800

    repair validator bug

--- src/scene_server/validator/valid_basic.go
+++ src/scene_server/validator/valid_basic.go
@@ -1,15 +1,15 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package validator
 
 import (
@@ -580,9 +580,14 @@ func (valid *ValidMap) validDate(val interface{}, key string) (bool, error) {
 		blog.Error("params in need")
 		return false, valid.ccError.Errorf(common.CCErrCommParamsNeedSet, key)
 	}
-	valStr := val.(string)
+	valStr, ok := val.(string)
+	if false == ok {
+		blog.Error("date can shoule be string")
+		return false, valid.ccError.Errorf(common.CCErrCommParamsShouldBeString, key)
+
+	}
 	if isIn && 0 == len(valStr) {
-		blog.Error("params  can not be empty")
+		blog.Error("date params  can not be empty")
 		return false, valid.ccError.Errorf(common.CCErrCommParamsNeedSet, key)
 	}
 	result := util.IsDate(valStr)
@@ -610,7 +615,12 @@ func (valid *ValidMap) validTime(val interface{}, key string) (bool, error) {
 		return false, valid.ccError.Errorf(common.CCErrCommParamsNeedSet, key)
 	}
 
-	valStr := val.(string)
+	valStr, ok := val.(string)
+	if false == ok {
+		blog.Error("date can shoule be string")
+		return false, valid.ccError.Errorf(common.CCErrCommParamsShouldBeString, key)
+
+	}
 	if isIn && 0 == len(valStr) {
 		blog.Error("params  can not be empty")
 		return false, valid.ccError.Errorf(common.CCErrCommParamsNeedSet, key)
