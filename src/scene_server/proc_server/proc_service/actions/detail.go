ead74620b14189bc765dcc7180a4de7c1800e052
eldonwu <eldonwu@tencent.com>
2018-04-10 17:01:39 +0800

    adapt topo process audit log data structure #117

--- src/scene_server/proc_server/proc_service/actions/detail.go
+++ src/scene_server/proc_server/proc_service/actions/detail.go
@@ -19,8 +19,11 @@ import (
 	httpcli "configcenter/src/common/http/httpclient"
 	"configcenter/src/common/util"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"reflect"
 	"strconv"
+	"strings"
 
 	simplejson "github.com/bitly/go-simplejson"
 	"github.com/emicklei/go-restful"
@@ -114,6 +117,14 @@ func (cli *procAction) getProcDetail(req *restful.Request, ownerID string, appID
 		return nil, err
 	}
 	procAttrArr := gProcAttr["data"].([]interface{})
+
+	rstmap, errorno := cli.getObjectAsst(common.BKInnerObjIDProc, ownerID)
+	if common.CCSuccess != errorno {
+		return nil, fmt.Errorf("get object asst faile")
+	}
+
+	fmt.Printf("rst map : %v\n", rstmap)
+
 	reResult := make([]map[string]interface{}, 0)
 	for _, i := range procAttrArr {
 		attr := i.(map[string]interface{})
@@ -126,7 +137,287 @@ func (cli *procAction) getProcDetail(req *restful.Request, ownerID string, appID
 		data[common.BKPropertyNameField] = attr[common.BKPropertyNameField]
 
 		data[common.BKPropertyValueField] = proc[propertyID]
+		// key is the association object filed，val is association object id
+		if val, ok := rstmap[propertyID]; ok {
+			keyItemStr := fmt.Sprintf("%v", proc[propertyID])
+			blog.Debug("keyitemstr:%s", keyItemStr)
+			retData, _, retErr := cli.getInstAsst(req, ownerID, val, strings.Split(keyItemStr, ","), nil)
+			if common.CCSuccess != retErr {
+				blog.Error("failed to get inst details")
+			}
+			data[common.BKPropertyValueField] = retData
+		}
 		reResult = append(reResult, data)
 	}
 	return reResult, nil
+}
+
+// instNameAsst  association inst name
+type instNameAsst struct {
+	ID         string `json:"id"`
+	ObjID      string `json:"bk_obj_id"`
+	ObjIcon    string `json:"bk_obj_icon"`
+	InstID     int    `json:"bk_inst_id"`
+	ObjectName string `json:"bk_obj_name"`
+	InstName   string `json:"bk_inst_name"`
+}
+
+// getObjectAsst read association objectid the return key is engilish property name, value is the objectid
+func (cli *procAction) getObjectAsst(objID, ownerID string) (map[string]string, int) {
+
+	rstmap := map[string]string{}
+
+	// get module
+	cli.objcli.SetAddress(cli.CC.ObjCtrl())
+	att := map[string]interface{}{}
+	att[common.BKObjIDField] = objID
+	att[common.BKOwnerIDField] = ownerID
+	searchData, jsErr := json.Marshal(att)
+	if nil != jsErr {
+		blog.Error("failed to marshal the data[%+v], error info is %s", searchData, jsErr.Error())
+		return nil, common.CCErrCommJSONMarshalFailed
+	}
+	rests, restErr := cli.objcli.SearchMetaObjectAtt(searchData)
+	if nil != restErr {
+		blog.Error("failed to read the object att, error is %s ", restErr.Error())
+		return nil, common.CCErrTopoInstSelectFailed
+	}
+
+	// 组织模型名和对应的字段
+	for _, item := range rests {
+
+		switch item.PropertyType {
+
+		case common.FiledTypeSingleAsst:
+
+			asst := map[string]interface{}{}
+			asst["bk_object_att_id"] = item.PropertyID
+			asst[common.BKOwnerIDField] = item.OwnerID
+			asst[common.BKObjIDField] = item.ObjectID
+
+			searchData, jsErr := json.Marshal(asst)
+
+			if nil != jsErr {
+				blog.Error("failed to marshal the data[%+v], error info is %s", searchData, jsErr.Error())
+			}
+
+			asstRst, asstRstErr := cli.objcli.SearchMetaObjectAsst(searchData)
+			if nil != asstRstErr {
+				blog.Error("failed to read the object asst, error is %s ", asstRstErr.Error())
+				return nil, common.CCErrTopoInstSelectFailed
+			}
+
+			if len(asstRst) > 0 { // only one association map
+				rstmap[item.PropertyID] = asstRst[0].AsstObjID
+			}
+
+		case common.FieldTypeMultiAsst:
+
+			asst := map[string]interface{}{}
+			asst["bk_object_att_id"] = item.PropertyID
+			asst[common.BKOwnerIDField] = item.OwnerID
+			asst[common.BKObjIDField] = item.ObjectID
+			searchData, jsErr := json.Marshal(asst)
+
+			if nil != jsErr {
+				blog.Error("failed to marshal the data[%+v], error info is %s", searchData, jsErr.Error())
+			}
+
+			asstRst, asstRstErr := cli.objcli.SearchMetaObjectAsst(searchData)
+			if nil != asstRstErr {
+				blog.Error("failed to read the object asst, error is %s ", asstRstErr.Error())
+				return nil, common.CCErrTopoInstSelectFailed
+			}
+
+			if len(asstRst) > 0 { // only association with one object by one filed
+				rstmap[item.PropertyID] = asstRst[0].AsstObjID
+			}
+		}
+
+	}
+
+	// rstmap: key is the bk_property_id  value is the association object id
+	return rstmap, common.CCSuccess
+}
+
+func (cli *procAction) getInstAsst(req *restful.Request, ownerID, objID string, ids []string, page map[string]interface{}) ([]instNameAsst, int, int) {
+
+	tmpIDS := []int{}
+	for _, id := range ids {
+		tmpID, _ := strconv.Atoi(id)
+		tmpIDS = append(tmpIDS, tmpID)
+	}
+
+	condition := make(map[string]interface{})
+	searchParams := make(map[string]interface{})
+
+	searchParams["fields"] = ""
+	if val, ok := page["fields"]; ok {
+		searchParams["fields"] = val
+	}
+	searchParams["start"] = 0
+	if val, ok := page["start"]; ok {
+		searchParams["start"] = val
+	}
+	searchParams["limit"] = common.BKDefaultLimit
+	if val, ok := page["limit"]; ok {
+		searchParams["limit"] = val
+	}
+
+	var targetOBJ string
+	var instName string
+	var instID string
+
+	targetPre := cli.CC.ObjCtrl() + "/object/v1/insts/"
+	switch objID {
+	case common.BKInnerObjIDHost:
+		targetOBJ = ""
+		instName = common.BKHostInnerIPField
+		instID = common.BKHostIDField
+		targetPre = cli.CC.HostCtrl() + "/host/v1/hosts"
+		if 0 != len(tmpIDS) {
+			condition[common.BKHostIDField] = map[string]interface{}{common.BKDBIN: tmpIDS}
+		}
+	case common.BKInnerObjIDApp:
+		targetOBJ = common.BKInnerObjIDApp
+		instName = common.BKAppNameField
+		instID = common.BKAppIDField
+		searchParams["sort"] = common.BKAppIDField
+		condition[common.BKOwnerIDField] = ownerID
+		if 0 != len(tmpIDS) {
+			condition[common.BKAppIDField] = map[string]interface{}{common.BKDBIN: tmpIDS}
+		}
+	case common.BKInnerObjIDSet:
+		targetOBJ = common.BKInnerObjIDSet
+		instID = common.BKSetIDField
+		instName = common.BKSetNameField
+		searchParams["sort"] = common.BKSetIDField
+		condition[common.BKSetIDField] = map[string]interface{}{common.BKDBIN: tmpIDS}
+		condition[common.BKOwnerIDField] = ownerID
+	case common.BKInnerObjIDModule:
+		targetOBJ = common.BKInnerObjIDModule
+		instID = common.BKModuleIDField
+		instName = common.BKModuleNameField
+		searchParams["sort"] = common.BKModuleIDField
+		condition[common.BKOwnerIDField] = ownerID
+		if 0 != len(tmpIDS) {
+			condition[common.BKModuleIDField] = map[string]interface{}{common.BKDBIN: tmpIDS}
+		}
+	case common.BKInnerObjIDPlat:
+		targetOBJ = common.BKInnerObjIDPlat
+		instID = common.BKCloudIDField
+		instName = common.BKCloudNameField
+		searchParams["sort"] = common.BKCloudIDField
+		if 0 != len(tmpIDS) {
+			condition[common.BKCloudIDField] = map[string]interface{}{common.BKDBIN: tmpIDS}
+		}
+	default:
+		targetOBJ = common.BKINnerObjIDObject
+		instName = common.BKInstNameField
+		instID = common.BKInstIDField
+		condition[common.BKOwnerIDField] = ownerID
+		condition[common.BKObjIDField] = objID
+		if 0 != len(tmpIDS) {
+			condition[common.BKInstIDField] = map[string]interface{}{common.BKDBIN: tmpIDS}
+		}
+		searchParams["sort"] = common.BKInstIDField
+
+	}
+
+	searchParams["condition"] = condition
+
+	//search
+	sURL := targetPre + targetOBJ + "/search"
+	inputJSON, _ := json.Marshal(searchParams)
+	objRes, err := httpcli.ReqHttp(req, sURL, common.HTTPSelectPost, []byte(inputJSON))
+	blog.Debug("search inst api(%s) detail params: %s ", sURL, string(inputJSON))
+	if nil != err {
+		blog.Error("search inst defail failed, error: %v", err)
+		return nil, 0, common.CCErrCommJSONMarshalFailed
+	}
+
+	js, err := simplejson.NewJson([]byte(objRes))
+	if nil != err {
+		blog.Error("the input json is invalid, error info is %s", err.Error())
+		return nil, 0, common.CCErrCommJSONUnmarshalFailed
+	}
+
+	input, jsErr := js.Map()
+	if nil != jsErr {
+		blog.Error("the input json is invalid, error info is %s", jsErr.Error())
+		return nil, 0, common.CCErrCommJSONUnmarshalFailed
+	}
+
+	delArryFunc := func(s []string, i int) []string {
+		s[len(s)-1], s[i] = s[i], s[len(s)-1]
+		return s[:len(s)-1]
+	}
+
+	rstName := []instNameAsst{}
+
+	cnt := 0
+	if data, ok := input["data"].(map[string]interface{}); ok {
+
+		switch reflect.TypeOf(data["count"]).Kind() {
+		case reflect.String:
+			cnt, _ = strconv.Atoi(reflect.ValueOf(data["count"]).String())
+		case reflect.Int:
+			cnt = int(reflect.ValueOf(data["count"]).Int())
+		default:
+			blog.Warn("unknown the count type, value is %v type is %s", data["count"], reflect.TypeOf(data["count"]).Kind())
+		}
+
+		if info, infoOk := data["info"].([]interface{}); infoOk {
+			for _, infoItem := range info {
+
+				if dataItem, dataItemOk := infoItem.(map[string]interface{}); dataItemOk {
+
+					if dataItemVal, dataItemValOk := dataItem[instName]; dataItemValOk {
+
+						// 提取实例名
+						inst := instNameAsst{}
+						if dataItemValStr, convOk := dataItemVal.(string); convOk {
+							inst.InstName = dataItemValStr
+							inst.ObjID = objID
+						}
+
+						// 删除已经存在的ID
+						if dataItemVal, dataItemValOk := dataItem[instID]; dataItemValOk {
+
+							//fmt.Println(key, dataItemVal)
+							switch d := dataItemVal.(type) {
+							case json.Number:
+								if 0 != len(ids) {
+									for idx, key := range ids {
+										if val, err := d.Int64(); nil == err && key == strconv.Itoa(int(val)) {
+											//fmt.Println("key:", key, "val:", val)
+											inst.ID = ids[idx]
+											inst.InstID, _ = strconv.Atoi(ids[idx])
+											ids = delArryFunc(ids, idx)
+											rstName = append(rstName, inst)
+											goto next
+										}
+									}
+								} else if val, err := d.Int64(); nil == err {
+									inst.ID = strconv.Itoa(int(val))
+									inst.InstID = int(val)
+									rstName = append(rstName, inst)
+								}
+
+							}
+						next:
+						}
+
+					}
+				}
+			}
+		}
+	}
+
+	// deal the other inst name
+	for _, id := range ids {
+		rstName = append(rstName, instNameAsst{ID: id})
+	}
+
+	return rstName, cnt, common.CCSuccess
 }
