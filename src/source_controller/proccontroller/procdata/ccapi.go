7cc796b3a579060ef51d22841e370eca50fac42d
wusendong <wusendong@gmail.com>
2018-04-18 15:48:04 +0800

    WIP

--- src/source_controller/proccontroller/procdata/ccapi.go
+++ src/source_controller/proccontroller/procdata/ccapi.go
@@ -1,15 +1,15 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package ccapi
 
 import (
@@ -18,6 +18,7 @@ import (
 	"configcenter/src/common/core/cc/config"
 	"configcenter/src/common/errors"
 	"configcenter/src/common/http/httpserver"
+	"configcenter/src/common/metric"
 	confCenter "configcenter/src/source_controller/proccontroller/procdata/config"
 	"configcenter/src/source_controller/proccontroller/procdata/rdiscover"
 	"configcenter/src/storage"
@@ -173,4 +174,22 @@ func (ccAPI *CCAPIServer) initHttpServ() error {
 	ccAPI.httpServ.RegisterWebServer("/process/{version}", nil, a.Actions)
 
 	return nil
+}
+
+func metric() error {
+	conf := metric.Config{}
+	if err := metric.NewMetricController(conf, HealthMetric, nil); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// HealthMetric check netservice is health
+func HealthMetric() metric.HealthMeta {
+	return metric.HealthMeta{
+		CurrentRole: metric.SlaveRole,
+		IsHealthy:   true,
+		Message:     "proccontroller is healthy",
+	}
 }
