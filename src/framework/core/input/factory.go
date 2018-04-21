f52ef309848b0816ba9edbb5a889b0676a807214
uriwang <uriwang@holmes.local>
2018-04-22 00:01:32 +0800

    update:增加事件触发机制

--- src/framework/core/input/factory.go
+++ src/framework/core/input/factory.go
@@ -2,6 +2,9 @@ package input
 
 // New create a new Manager instance
 func New() Manager {
-	mgr := &manager{inputers: MapInputer{}}
+	mgr := &manager{
+		inputers:    MapInputer{},
+		inputerChan: make(chan *wrapInputer, 1024),
+	}
 	return mgr
 }
