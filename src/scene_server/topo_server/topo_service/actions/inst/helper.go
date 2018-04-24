d77b93f357617b4ac00cb4e0e00fa23e2f9cd064
uriwang <uriwang@tencent.com>
2018-04-24 11:01:24 +0800

    feature:增加接口可以支持内置所有模型的实例查找

--- src/scene_server/topo_server/topo_service/actions/inst/helper.go
+++ src/scene_server/topo_server/topo_service/actions/inst/helper.go
@@ -1,15 +1,15 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package inst
 
 import (
@@ -51,4 +51,17 @@ func hasHost(req *restful.Request, hostCtr string, condition map[string][]int) (
 		return false, fmt.Errorf("not found the data in result")
 	}
 	return true, nil
+}
+
+func getInnerNameByObjectID(objID string) string {
+	switch objID {
+	case common.BKInnerObjIDModule:
+		return common.BKInnerObjIDModule
+	case common.BKInnerObjIDApp:
+		return common.BKInnerObjIDApp
+	case common.BKInnerObjIDSet:
+		return common.BKInnerObjIDSet
+	default:
+		return common.BKINnerObjIDObject
+	}
 }
