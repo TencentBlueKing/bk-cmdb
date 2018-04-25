9af8538ff6e518dc1e827bda0188a9dbfefec0e9
wusendong <wusendong@gmail.com>
2018-04-25 20:42:01 +0800

    update pre model

--- src/scene_server/admin_server/migrate_service/models/obj_des_data.go
+++ src/scene_server/admin_server/migrate_service/models/obj_des_data.go
@@ -1,15 +1,15 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package models
 
 import (
@@ -91,7 +91,15 @@ func getObjectDesData(ownerID string) []*metadata.ObjectDes {
 		&metadata.ObjectDes{ObjCls: "bk_biz_topo", ObjectID: common.BKInnerObjIDSet, ObjectName: "集群", ObjIcon: "icon-cc-set"},
 		&metadata.ObjectDes{ObjCls: "bk_organization", ObjectID: common.BKInnerObjIDApp, ObjectName: "业务", ObjIcon: "icon-cc-business"},
 		&metadata.ObjectDes{ObjCls: "bk_host_manage", ObjectID: common.BKInnerObjIDProc, ObjectName: "进程", ObjIcon: "icon-cc-process"},
-		&metadata.ObjectDes{ObjCls: "bk_host_manage", ObjectID: common.BKInnerObjIDPlat, ObjectName: "子网区域", ObjIcon: "icon-cc-subnet"},
+
+		&metadata.ObjectDes{ObjCls: "bk_network", ObjectID: "bk_switch", ObjectName: "交换机", ObjIcon: "icon-cc-switch2"},
+		&metadata.ObjectDes{ObjCls: "bk_network", ObjectID: "bk_router", ObjectName: "路由器", ObjIcon: "icon-cc-router"},
+		&metadata.ObjectDes{ObjCls: "bk_network", ObjectID: "bk_load_balance", ObjectName: "防火墙", ObjIcon: "icon-cc-firewall"},
+		&metadata.ObjectDes{ObjCls: "bk_network", ObjectID: "bk_firewall", ObjectName: "负载均衡", ObjIcon: "icon-cc-balance"},
+
+		&metadata.ObjectDes{ObjCls: "bk_middleware", ObjectID: "bk_weblogic", ObjectName: "weblogic", ObjIcon: "icon-cc-weblogic"},
+		&metadata.ObjectDes{ObjCls: "bk_middleware", ObjectID: "bk_apache", ObjectName: "apache", ObjIcon: "icon-cc-tomcat"},
+		&metadata.ObjectDes{ObjCls: "bk_middleware", ObjectID: "bk_tomcat", ObjectName: "tomcat", ObjIcon: "icon-cc-apache"},
 	}
 	t := new(time.Time)
 	*t = time.Now()
