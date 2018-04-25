2fc9fa7f4593da897f1a02eee0c7fda8e91cf14a
uriwang <uriwang@tencent.com>
2018-04-25 16:55:39 +0800

    update:内置blog日志系统

--- src/framework/api/condition.go
+++ src/framework/api/condition.go
@@ -3,6 +3,6 @@ package api
 import "configcenter/src/framework/common"
 
 // CreateCondition create a condition object
-func CreateCondition(tableName string) common.Condition {
-	return common.CreateCondition(tableName)
+func CreateCondition() common.Condition {
+	return common.CreateCondition()
 }
