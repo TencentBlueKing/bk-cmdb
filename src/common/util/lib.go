7fe49215ddb9d52304bcdadb0298e94fd9e5212c
owenlxu <owenlxu@tencent.com>
2018-04-25 10:39:59 +0800

    更新公用 的lib

--- src/common/util/lib.go
+++ src/common/util/lib.go
@@ -1,15 +1,15 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package util
 
 import (
@@ -53,6 +53,17 @@ func StrArrayUnique(a []string) (ret []string) {
 	length := len(a)
 	for i := 0; i < length; i++ {
 		if !Contains(ret, a[i]) {
+			ret = append(ret, a[i])
+		}
+	}
+	return ret
+}
+
+//IntArrayUnique get unique int array
+func IntArrayUnique(a []int) (ret []int) {
+	length := len(a)
+	for i := 0; i < length; i++ {
+		if !ContainsInt(ret, a[i]) {
 			ret = append(ret, a[i])
 		}
 	}
