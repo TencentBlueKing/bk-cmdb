c782256c491e64c0e557c1ae42f7136f9b57f152
uriwang <uriwang@tencent.com>
2018-04-25 16:55:39 +0800

    update:内置blog日志系统

--- src/framework/core/log/log.go
+++ src/framework/core/log/log.go
@@ -1,14 +1,19 @@
 package log
 
 import (
-	"fmt"
+	"configcenter/src/common/blog"
 )
 
 // the globle log instance
 var logInst Loger = log{}
 
+func init() {
+	blog.InitLogs()
+}
+
 // SetLoger set a new loger instance
 func SetLoger(log Loger) {
+	blog.CloseLogs()
 	logInst = log
 }
 
@@ -17,42 +22,42 @@ type log struct{}
 
 // Info logs to the INFO logs.
 func (cli log) Info(args ...interface{}) {
-	fmt.Println("Info:", args)
+	blog.Info("%v", args)
 }
 
 // Infof logs to the INFO logs.
 func (cli log) Infof(format string, args ...interface{}) {
-	fmt.Println("Info:", fmt.Sprintf(format, args...))
+	blog.Infof(format, args)
 }
 
 // Warning logs to the WARNING and INFO logs.
 func (cli log) Warning(args ...interface{}) {
-	fmt.Println("Warning:", args)
+	blog.Warn("%v", args)
 }
 
 // Warningf logs to the WARNING and INFO logs.
 func (cli log) Warningf(format string, args ...interface{}) {
-	fmt.Println("Warning:", fmt.Sprintf(format, args...))
+	blog.Warnf(format, args)
 }
 
 // Error logs to the Error、 WARNING and INFO logs.
 func (cli log) Error(args ...interface{}) {
-	fmt.Println("Error:", args)
+	blog.Error("%v", args)
 }
 
 // Errorf logs to the Error、 WARNING and INFO logs.
 func (cli log) Errorf(format string, args ...interface{}) {
-	fmt.Println("Error:", fmt.Sprintf(format, args...))
+	blog.Errorf(format, args)
 }
 
 // Fatal logs to the Fatal, Error, WARNING, and INFO logs.
 func (cli log) Fatal(args ...interface{}) {
-	fmt.Println("Fatal:", args)
+	blog.Fatal(args)
 }
 
 // Fatalf logs to the Fatal, Error, WARNING, and INFO logs.
 func (cli log) Fatalf(format string, args ...interface{}) {
-	fmt.Println("Fatal:", fmt.Sprintf(format, args...))
+	blog.Fatalf(format, args)
 }
 
 // Info logs to the INFO logs.
