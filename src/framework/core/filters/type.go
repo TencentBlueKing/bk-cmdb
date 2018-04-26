1c890dabd71d84a2b5b76ee45b1fefc096b23adf
uriwang <uriwang@tencent.com>
2018-04-26 14:33:07 +0800

    update:修改validator 目录改名字为 filters

---
+++ src/framework/core/filters/type.go
@@ -0,0 +1,6 @@
+package filters
+
+// Filter is the interface that must be implement by ervery filter
+type Filter interface{
+
+}
--- src/framework/core/validator/type.go
+++
@@ -1,6 +0,0 @@
-package validator
-
-// Validator is the interface that must be implement by ervery validator
-type Validator interface{
-
-}
