17e88697688295ee348e982c6e4d2dc53e573ac7
uriwang <uriwang@tencent.com>
2018-05-03 16:08:25 +0800

    update:修改主机测试用例的逻辑代码

--- src/framework/core/output/module/inst/inst_host_test.go
+++ src/framework/core/output/module/inst/inst_host_test.go
@@ -2,6 +2,7 @@ package inst_test
 
 import (
 	"configcenter/src/framework/common"
+	"configcenter/src/framework/core/output/module/inst"
 	"configcenter/src/framework/core/output/module/model"
 	"configcenter/src/framework/core/output/module/v3"
 	//"configcenter/src/framework/core/types"
@@ -15,47 +16,59 @@ func TestHostManager(t *testing.T) {
 	cli.SetUser("build_user")
 	cli.SetAddress("http://test.apiserver:8080")
 
-	items, err := model.FindClassificationsByCondition(common.CreateCondition().Field("bk_classification_id").Eq("bk_host_manage"))
+	clsItem, err := model.FindClassificationsByCondition(common.CreateCondition().Field("bk_classification_id").Eq("bk_host_manage"))
 	if nil != err {
 		t.Errorf("failed to find classifications, %s", err.Error())
 		return
 	}
 
-	for {
-		item, err := items.Next()
-		if nil != err {
-			t.Errorf("failed to get next classification, %s ", err.Error())
-			break
-		}
-		if nil == item {
-			t.Log("exit")
-			break
-		}
-		t.Logf("the classifications:%+v", item)
-
-		modelIterator, err := item.FindModelsByCondition(common.CreateCondition().Field("bk_obj_id").Eq("host"))
-		for {
-
-			item, err := modelIterator.Next()
-			if nil != err {
-				t.Errorf("failed to get model, %s", err.Error())
-				break
-			}
-
-			if nil == item {
-				t.Log("no model")
-				break
-			}
-
-			t.Logf("the model:%+v", item.GetName())
-
-			attrs, _ := item.Attributes()
-			for _, attr := range attrs {
-				t.Logf("the attribute:%+v", attr.GetName())
-			}
-
-		}
-
+	if nil == clsItem {
+		t.Errorf("not found the host classification")
+		return
 	}
+
+	clsItem.ForEach(func(item model.Classification) {
+
+		modelIter, err := item.FindModelsByCondition(common.CreateCondition().Field("bk_obj_id").Eq("host"))
+		if nil != err {
+			t.Errorf("failed to search classification, %s", err.Error())
+			return
+		}
+
+		if nil == modelIter {
+			t.Log("not found the model")
+			return
+		}
+
+		// deal host model
+		modelIter.ForEach(func(modelItem model.Model) {
+
+			// create host
+			hostInst, err := inst.CreateInst(modelItem)
+			if nil != err {
+				t.Errorf("failed to create host ")
+				return
+			}
+
+			// Only test
+			t.Logf("model name:%s", hostInst.GetModel().GetName())
+
+			// set host value
+			err = hostInst.SetValue("test", "test")
+			if nil != err {
+				t.Errorf("failed to set value, %s", err.Error())
+				return
+			}
+
+			// save host info
+			err = hostInst.Save()
+
+			if nil != err {
+				t.Errorf("failed to save ,%s", err.Error())
+			}
+
+		})
+
+	})
 
 }
