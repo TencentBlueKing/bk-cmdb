3cd3abc8cbf1ce0d0637394f64dcd745d2c0f971
uriwang <uriwang@tencent.com>
2018-05-08 15:28:21 +0800

    update:修改方法名

--- src/framework/plugins/example/host_inputer.go
+++ src/framework/plugins/example/host_inputer.go
@@ -1,15 +1,15 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package example
 
 import (
@@ -51,7 +51,7 @@ func (cli *hostInputer) Run() interface{} {
 	host.SetOsType(api.HostOSTypeLinux)
 	host.SetSLA(api.HostSLALevel1)
 	host.SetAssetID("host2122")
-	host.SetMac("1d2-3d-d-d")
+	host.SetInnerMac("1d2-3d-d-d")
 	host.SetOperator("test_user")
 	host.SetBakOperator("test_bak_user")
 	host.SetCPU(5)
