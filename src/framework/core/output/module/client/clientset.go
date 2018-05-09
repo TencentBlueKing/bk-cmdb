a8efa5625d2955bb814ef65eadb1c008da63cce3
wusendong <wusendong@gmail.com>
2018-05-09 17:06:17 +0800

    add service discovery

--- src/framework/core/output/module/client/clientset.go
+++ src/framework/core/output/module/client/clientset.go
@@ -1,15 +1,15 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package client
 
 import (
@@ -37,7 +37,7 @@ func (c *Clientset) CCV3() v3.CCV3Interface {
 
 func NewForConfig(c config.Config, disc discovery.DiscoverInterface) *Clientset {
 	var cs Clientset
-	cs.ccv3 = v3.New(c, nil)
+	cs.ccv3 = v3.New(c, disc)
 	client = &cs
 	return &cs
 }
