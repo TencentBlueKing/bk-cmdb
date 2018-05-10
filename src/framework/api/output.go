9e3688e37e903412fe587831efcb59119549f610
uriwang <uriwang@tencent.com>
2018-05-10 17:11:17 +0800

    update:增加实例扩展 创建plat 的接口

--- src/framework/api/output.go
+++ src/framework/api/output.go
@@ -1,15 +1,15 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package api
 
 import (
@@ -197,6 +197,37 @@ func FindHostByCondition(supplierAccount string, cond common.Condition) (*HostIt
 	}
 	iter, err := mgr.OutputerMgr.FindInstsByCondition(targetModel, cond)
 	return &HostIteratorWrapper{host: iter}, err
+}
+
+// CreatePlat create a new plat object
+func CreatePlat(supplierAccount string) (*PlatWrapper, error) {
+	targetModel, err := GetModel(supplierAccount, "bk_host_manage", "plat")
+	if nil != err {
+		return nil, err
+	}
+
+	platInst, err := mgr.OutputerMgr.CreateInst(targetModel)
+	return &PlatWrapper{plat: platInst}, err
+}
+
+// FindPlatLikeName find all insts by the name
+func FindPlatLikeName(supplierAccount, platName string) (*PlatIteratorWrapper, error) {
+	targetModel, err := GetModel(supplierAccount, "bk_host_manage", "plat")
+	if nil != err {
+		return nil, err
+	}
+	iter, err := mgr.OutputerMgr.FindInstsLikeName(targetModel, platName)
+	return &PlatIteratorWrapper{plat: iter}, err
+}
+
+// FindPlatByCondition find all insts by the condition
+func FindPlatByCondition(supplierAccount string, cond common.Condition) (*PlatIteratorWrapper, error) {
+	targetModel, err := GetModel(supplierAccount, "bk_host_manage", "plat")
+	if nil != err {
+		return nil, err
+	}
+	iter, err := mgr.OutputerMgr.FindInstsByCondition(targetModel, cond)
+	return &PlatIteratorWrapper{plat: iter}, err
 }
 
 // CreateCommonInst create a common inst object
--- src/framework/core/output/module/inst/inst_plat_factory.go
+++ src/framework/core/output/module/inst/inst_plat_factory.go
@@ -1,15 +1,15 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package inst
 
 import (
@@ -19,17 +19,16 @@ import (
 )
 
 func createPlat(target model.Model) (Inst, error) {
-	return &plat{target: target, datas: types.MapStr{}}, nil
+	return &inst{target: target, datas: types.MapStr{}}, nil
 }
 
 // findPlatsLikeName find all insts by inst name
-func findPlatsLikeName(target model.Model, businessName string) (Iterator, error) {
-	// TODO:按照名字读取特定模型的实例集合，实例名字要模糊匹配
-	return nil, nil
+func findPlatsLikeName(target model.Model, platName string) (Iterator, error) {
+	cond := common.CreateCondition().Field(PlatName).Like(platName)
+	return newIteratorInst(target, cond)
 }
 
 // findPlatsByCondition find all insts by condition
-func findPlatsByCondition(target model.Model, condition common.Condition) (Iterator, error) {
-	// TODO:按照条件读取所有实例
-	return nil, nil
+func findPlatsByCondition(target model.Model, cond common.Condition) (Iterator, error) {
+	return newIteratorInst(target, cond)
 }
