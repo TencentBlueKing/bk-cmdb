a785a6c28dc2167d1c56b0ae066131b70e077c53
gmaclinuxer <gmaclinuxer@gmail.com>
2018-05-25 10:36:20 +0800

    move script to scripts

--- scripts/travis_ci.sh
+++ scripts/travis_ci.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+GOVET_ERRORS=$(go tool vet ../src/ 2>&1)
+if [ -n "${GOVET_ERRORS}" ]; then
+  printf >&2 'go vet failed for the following reasons:\n%s\n\nplease run "go tool vet ./src/" on your changes before committing.\n' "${GOVET_ERRORS}"
+  # exit 1
+fi
+
 #GOFMT_FILES=$(gofmt -l .)
 #if [ -n "${GOFMT_FILES}" ]; then
 #  printf >&2 'gofmt failed for the following files:\n%s\n\nplease run "gofmt -w ." on your changes before committing.\n' "${GOFMT_FILES}"
@@ -11,10 +17,4 @@
 #  printf >&2 'golint failed for the following reasons:\n%s\n\nplease run 'golint ./...' on your changes before committing.\n' "${GOLINT_ERRORS}"
 #  exit 1
 #fi
-
-GOVET_ERRORS=$(go tool vet ./src/ 2>&1)
-if [ -n "${GOVET_ERRORS}" ]; then
-  printf >&2 'go vet failed for the following reasons:\n%s\n\nplease run "go tool vet ./src/" on your changes before committing.\n' "${GOVET_ERRORS}"
-  # exit 1
-fi
 
