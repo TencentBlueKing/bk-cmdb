254c71fab1b6f5c8013322cafa4900feab20f701
uriwang <uriwang@tencent.com>
2018-05-31 23:01:34 +0800

    update:修改查询主机返回的数据结构

--- src/framework/core/output/module/client/v3/inst_host.go
+++ src/framework/core/output/module/client/v3/inst_host.go
@@ -1,15 +1,15 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package v3
 
 import (
@@ -176,12 +176,52 @@ func (h *Host) SearchHost(cond common.Condition) ([]types.MapStr, error) {
 		return nil, errors.New(gs.Get("bk_error_msg").String())
 	}
 
-	dataStr := gs.Get("data.info.#.host").String()
+	dataStr := gs.Get("data.info").String()
 	if 0 == len(dataStr) {
 		return nil, errors.New("data is empty")
 	}
 
+	hostMap := make([]types.MapStr, 0)
+	err = json.Unmarshal([]byte(dataStr), &hostMap)
+	if nil != err {
+		return nil, err
+	}
 	resultMap := make([]types.MapStr, 0)
-	err = json.Unmarshal([]byte(dataStr), &resultMap)
+	//fmt.Println("host:", dataStr)
+	for _, item := range hostMap {
+
+		biz, err := item.MapStrArray("biz")
+		if nil != err {
+			return nil, err
+		}
+
+		set, err := item.MapStrArray("set")
+		if nil != err {
+			return nil, err
+		}
+
+		module, err := item.MapStrArray("module")
+		if nil != err {
+			return nil, err
+		}
+
+		host, err := item.MapStr("host")
+		if nil != err {
+			return nil, err
+		}
+
+		host.Set("biz", biz)
+		host.Set("set", set)
+		host.Set("module", module)
+
+		item, err = item.MapStr("host")
+		if nil != err {
+			return nil, err
+		}
+
+		resultMap = append(resultMap, item)
+
+	}
+
 	return resultMap, err
 }
