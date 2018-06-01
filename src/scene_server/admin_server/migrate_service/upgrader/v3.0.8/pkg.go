372ba3a2174482a69a3cda21c3a36a37a80e82f2
wusendong <wusendong@gmail.com>
2018-06-01 11:10:48 +0800

    add upsert functionality #354

---
+++ src/scene_server/admin_server/migrate_service/upgrader/v3.0.8/pkg.go
@@ -0,0 +1,19 @@
+package v3v0v8
+
+import (
+	"configcenter/src/scene_server/admin_server/migrate_service/upgrader"
+	"configcenter/src/storage"
+)
+
+func init() {
+	upgrader.RegistUpgrader("v3.0.8", upgrade)
+}
+
+func upgrade(db storage.DI, conf *upgrader.Config) (err error) {
+	err = createTable(db, conf)
+	if err != nil {
+		return err
+	}
+
+	return
+}
