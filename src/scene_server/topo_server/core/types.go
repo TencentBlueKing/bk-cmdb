e47a1960f3057aeb39011c7b84ff334142aca1ee
uriwang <uriwang@tencent.com>
2018-06-02 17:22:45 +0800

    update:增加topo api 目录

--- src/scene_server/topo_server/core/types.go
+++ src/scene_server/topo_server/core/types.go
@@ -1,27 +1,39 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package core
 
 import (
 	frcommon "configcenter/src/framework/common"
 	frtypes "configcenter/src/framework/core/types"
 
+	"configcenter/src/common/errors"
+	"configcenter/src/common/language"
+
 	"configcenter/src/scene_server/topo_server/core/inst"
 	"configcenter/src/scene_server/topo_server/core/model"
 )
 
-// Manager Provides management interfaces for models and instances
-type Manager interface {
+// LogicParams the logic function params
+type LogicParams struct {
+	Err  errors.CCErrorIf
+	Lang language.CCLanguageIf
+}
+
+// LogicFunc the core logic function definition
+type LogicFunc func(params LogicParams, data frtypes.MapStr) (frtypes.MapStr, error)
+
+// Core Provides management interfaces for models and instances
+type Core interface {
 	CreateClassification(data frtypes.MapStr) (model.Classification, error)
 	CreateObject(data frtypes.MapStr) (model.Object, error)
 	CreateObjectAttribute(data frtypes.MapStr) (model.Attribute, error)
