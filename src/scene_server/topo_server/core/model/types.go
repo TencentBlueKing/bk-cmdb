eaaabd653588cae07d4f363fc641ab99cc074cc3
uriwang <uriwang@tencent.com>
2018-06-02 17:22:45 +0800

    update:增加topo api 目录

--- src/scene_server/topo_server/core/model/types.go
+++ src/scene_server/topo_server/core/model/types.go
@@ -1,18 +1,19 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package model
 
 import (
+	//frcommon "configcenter/src/framework/common"
 	frtypes "configcenter/src/framework/core/types"
 )
 
@@ -36,8 +37,8 @@ type Saver interface {
 // Topo the object topo interface
 type Topo interface {
 	Current() Object
-	Prev() []Object
-	Next() []Object
+	Prev() Object
+	Next() Object
 	ToMapStr() frtypes.MapStr
 }
 
@@ -212,4 +213,5 @@ type Factory interface {
 	CreaetClassification() Classification
 	CreateAttribute() Attribute
 	CreateGroup() Group
+	CreateAssociation() Association
 }
