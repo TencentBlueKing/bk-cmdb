de844ca3b78446f89649c835c976ae4149a6a718
uriwang <uriwang@roberthsiao-pc0.tencent.com>
2018-06-03 22:59:30 +0800

    update:更新框架文档

--- src/framework/core/types/mapstr.go
+++ src/framework/core/types/mapstr.go
@@ -1,15 +1,15 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package types
 
 import (
@@ -182,6 +182,9 @@ func (cli MapStr) MapStr(key string) (MapStr, error) {
 	default:
 		return nil, errors.New("the data is not a map[string]interface{} type")
 	case nil:
+		if _, ok := cli[key]; ok {
+			return MapStr{}, nil
+		}
 		return nil, errors.New("the key is invalid")
 	case map[string]interface{}:
 		return MapStr(t), nil
