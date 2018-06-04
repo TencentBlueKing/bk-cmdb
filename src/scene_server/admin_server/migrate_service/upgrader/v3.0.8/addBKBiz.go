fcf91db7d47d54d05247350cddb81e4ad6d5ad43
wusendong <wusendong@gmail.com>
2018-06-04 09:11:11 +0800

    add default biz #354

---
+++ src/scene_server/admin_server/migrate_service/upgrader/v3.0.8/addBKBiz.go
@@ -0,0 +1,13 @@
+package v3v0v8
+
+import (
+	"configcenter/src/common"
+	"configcenter/src/common/blog"
+	"configcenter/src/scene_server/admin_server/migrate_service/upgrader"
+	"configcenter/src/storage"
+	"time"
+)
+
+func addBKBiz(db storage.DI, conf *upgrader.Config) error {
+	return nil
+}
