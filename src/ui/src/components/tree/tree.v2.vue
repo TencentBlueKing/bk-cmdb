8e53087ade2f350d03cccf1a37ac42f2a12f64ec
peibiaoyang(杨培彪) <peibiaoyang@tencent.com>
2018-06-04 11:39:01 +0800

    minor: delete console

--- src/ui/src/components/tree/tree.v2.vue
+++ src/ui/src/components/tree/tree.v2.vue
@@ -39,7 +39,6 @@
         },
         created () {
             bus.$on('nodeClick', (activeNode, nodeOptions) => {
-                console.log(activeNode, nodeOptions)
                 if (this.treeId === nodeOptions.treeId) {
                     this.activeNodeId = nodeOptions.nodeId
                     this.$emit('nodeClick', activeNode, nodeOptions)
