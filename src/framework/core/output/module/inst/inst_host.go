fe8f868c2fb9befac8671b1c4feceeeaa45e1e69
uriwang <uriwang@holmes.local>
2018-06-08 00:08:33 +0800

    update:实例和模型增加 isexists update create 方法

--- src/framework/core/output/module/inst/inst_host.go
+++ src/framework/core/output/module/inst/inst_host.go
@@ -1,15 +1,15 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package inst
 
 import (
@@ -83,7 +83,18 @@ func (cli *host) SetValue(key string, value interface{}) error {
 	cli.datas.Set(key, value)
 	return nil
 }
-
+func (cli *host) search() ([]model.Attribute, []types.MapStr, error) {
+	return nil, nil, nil
+}
+func (cli *host) IsExists() (bool, error) {
+	return true, nil
+}
+func (cli *host) Create() error {
+	return nil
+}
+func (cli *host) Update() error {
+	return nil
+}
 func (cli *host) Save() error {
 
 	attrs, err := cli.target.Attributes()
--- src/framework/core/output/module/inst/inst_module.go
+++ src/framework/core/output/module/inst/inst_module.go
@@ -1,15 +1,15 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package inst
 
 import (
@@ -19,7 +19,6 @@ import (
 	"configcenter/src/framework/core/output/module/model"
 	"configcenter/src/framework/core/types"
 	"errors"
-	"fmt"
 )
 
 var _ Inst = (*module)(nil)
@@ -89,15 +88,14 @@ func (cli *module) SetValue(key string, value interface{}) error {
 	return nil
 }
 
-func (cli *module) Save() error {
-
+func (cli *module) search() ([]model.Attribute, []types.MapStr, error) {
 	businessID := cli.datas.String(BusinessID)
 	setID := cli.datas.String(SetID)
 
 	// get the attributes
 	attrs, err := cli.target.Attributes()
 	if nil != err {
-		return err
+		return nil, nil, err
 	}
 
 	// construct the condition which is used to check the if it is exists
@@ -109,30 +107,46 @@ func (cli *module) Save() error {
 
 			attrVal := cli.datas.String(attrItem.GetID())
 			if 0 == len(attrVal) {
-				return errors.New("the key field(" + attrItem.GetID() + ") is not set")
+				return attrs, nil, errors.New("the key field(" + attrItem.GetID() + ") is not set")
 			}
 
 			cond.Field(attrItem.GetID()).Eq(attrVal)
 		}
 	}
 
-	fmt.Println("cond:", cond.ToMapStr())
-
 	// search by condition
 	existItems, err := client.GetClient().CCV3().Module().SearchModules(cond)
+
+	return attrs, existItems, err
+
+}
+
+func (cli *module) IsExists() (bool, error) {
+
+	_, items, err := cli.search()
+	if nil != err {
+		return false, err
+	}
+	return 0 != len(items), nil
+}
+func (cli *module) Create() error {
+	moduleID, err := client.GetClient().CCV3().Module().CreateModule(cli.datas)
+	if nil != err {
+		return err
+	}
+	cli.datas.Set(ModuleID, moduleID)
+	return nil
+}
+func (cli *module) Update() error {
+
+	businessID := cli.datas.String(BusinessID)
+	setID := cli.datas.String(SetID)
+
+	attrs, existItems, err := cli.search()
 	if nil != err {
 		return err
 	}
 
-	//fmt.Println("the exists:", existItems)
-
-	// create a new
-	if 0 == len(existItems) {
-		_, err = client.GetClient().CCV3().Module().CreateModule(cli.datas)
-		return err
-	}
-
-	// update the exists
 	for _, existItem := range existItems {
 
 		cli.datas.ForEach(func(key string, val interface{}) {
@@ -162,4 +176,14 @@ func (cli *module) Save() error {
 
 	}
 	return nil
+}
+func (cli *module) Save() error {
+
+	if exists, err := cli.IsExists(); nil != err {
+		return err
+	} else if exists {
+		return cli.Update()
+	}
+
+	return cli.Create()
 }
