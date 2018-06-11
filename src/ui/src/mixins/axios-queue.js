9871d5b4d96a53323a8ab4b842837c7e763989c0
peibiaoyang(杨培彪) <peibiaoyang@tencent.com>
2018-06-11 19:34:26 +0800

    update:axios queue mixin

--- src/ui/src/mixins/axios-queue.js
+++ src/ui/src/mixins/axios-queue.js
@@ -1,14 +1,9 @@
 import { $AxiosQueue } from '@/api/axios'
 
 export default {
-    computed: {
-        $AxiosQueue () {
-            return $AxiosQueue
-        }
-    },
     methods: {
         $loading () {
-            const queue = this.$AxiosQueue
+            const queue = $AxiosQueue
             const axiosIds = [].slice.call(arguments)
             if (axiosIds.length) {
                 return axiosIds.some(id => queue.includes(id))
