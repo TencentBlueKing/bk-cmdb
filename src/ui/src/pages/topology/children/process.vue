94cfa4e1811068088ff3aa0bd552f72d6cbc5a46
ProtosL <644074553@qq.com>
2018-06-12 12:20:53 +0800

    update: 1、进程管理显示字段调整 2、模块查看进程新增跳转 3、批量编辑属性未清空上次选中的内容

--- src/ui/src/pages/topology/children/process.vue
+++ src/ui/src/pages/topology/children/process.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="process-wrapper">
+    <div class="process-wrapper" ref="wrapper">
         <v-table class="process-table" 
             :header="table.header"
             :list="table.list"
             :defaultSort="table.defaultSort"
             :pagination="table.pagination"
             :loading="table.isLoading"
+            :height="table.height"
             @handlePageChange="setCurrentPage"
             @handleSizeChange="setCurrentSize"
             @handleSortChange="setCurrentSort">
@@ -19,6 +20,12 @@
                 <template v-else>{{item[id]}}</template>
             </template>
         </v-table>
+        <p class="footer-info">
+            <span>{{$t('ProcessManagement["在进程管理中绑定进程到模块，"]')}}</span>
+            <router-link to="process">
+                {{$t('ProcessManagement["点击此跳转"]')}}
+            </router-link>
+        </p>
     </div>
 </template>
 
@@ -42,6 +49,7 @@
             return {
                 attribute: [],
                 table: {
+                    height: 200,
                     header: [],
                     list: [],
                     pagination: {
@@ -78,6 +86,7 @@
             isShow (isShow) {
                 if (isShow) {
                     this.table.isLoading = true
+                    this.setTableHeight()
                     Promise.all([
                         this.getProcessAttribute(),
                         this.getProcessList()
@@ -97,9 +106,6 @@
                 let headerTail = []
                 attribute.map(property => {
                     let {
-                        isonly,
-                        isrequired,
-                        'bk_isapi': bkIsapi,
                         'bk_property_id': bkPropertyId,
                         'bk_property_name': bkPropertyName
                     } = property
@@ -108,20 +114,39 @@
                         name: bkPropertyName,
                         property: property
                     }
-                    if (!bkIsapi) {
-                        if (isonly && isrequired) {
-                            headerLead.push(headerItem)
-                        } else if (isonly || isonly) {
-                            headerMiddle.push(headerItem)
-                        } else {
-                            headerTail.push(headerItem)
-                        }
+                    switch (bkPropertyId) {
+                        case 'bk_process_name':
+                            headerMiddle[0] = headerItem
+                            break
+                        case 'bk_func_id':
+                            headerMiddle[1] = headerItem
+                            break
+                        case 'bind_ip':
+                            headerMiddle[2] = headerItem
+                            break
+                        case 'port':
+                            headerMiddle[3] = headerItem
+                            break
+                        case 'protocol':
+                            headerMiddle[4] = headerItem
+                            break
+                        case 'bk_func_name':
+                            headerMiddle[5] = headerItem
+                            break
                     }
+                })
+                headerMiddle = headerMiddle.filter(header => {
+                    return header !== undefined
                 })
                 this.table.header = headerLead.concat(headerMiddle, headerTail).slice(0, 6)
             }
         },
         methods: {
+            setTableHeight () {
+                const wrapper = this.$refs.wrapper.getBoundingClientRect()
+                const body = document.body.getBoundingClientRect()
+                this.table.height = Number(`${body.height - wrapper.top - 41 - 30}`)
+            },
             getEnumCell (data, property) {
                 let obj = property.option.find(({id}) => {
                     return id === data
@@ -191,6 +216,22 @@
 <style lang="scss" scoped>
     .process-wrapper {
         padding: 20px;
+        .footer-info {
+            margin-top: 10px;
+            font-size: 0;
+            span,
+            a {
+                font-size: 14px;
+                line-height: 20px;
+                height: 20px;
+            }
+            a {
+                color: #3c96ff;
+                &:hover {
+                    color: #0082ff;
+                }
+            }
+        }
     }
 </style>
 
