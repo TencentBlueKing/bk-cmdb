ef0af521f011fc187c91c35c4f99ca6ebf62cbea
ProtosL <644074553@qq.com>
2018-06-14 20:01:33 +0800

    bugfix:  业务拓扑有关联主机时，ui显示错误

--- src/ui/src/components/common/hostAssociation/host.vue
+++ src/ui/src/components/common/hostAssociation/host.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <input class="bk-form-input selected-host" type="text" readonly :value="localSelected.join(',')" @click="isSelectBoxShow = !isSelectBoxShow">
+        <input class="bk-form-input selected-host" type="text" readonly :value="localSelected.join(',')" @click="showSelectBox">
         <i class="bk-icon icon-close bk-selector-icon clear-icon" @click.stop="clear" v-show="localSelected.length"></i>
         <div class="selectbox-wrapper" v-show="isSelectBoxShow" @click.self="handleCancel">
             <div class="selectbox-box">
@@ -74,13 +74,13 @@
                     return Array.isArray(selected) || typeof selected === 'string' || typeof selected === 'undefined' || selected === null
                 }
             },
-            multiple: Boolean
+            multiple: Boolean,
+            isSelectBoxShow: Boolean
         },
         data () {
             return {
                 ready: false,
                 localSelected: [],
-                isSelectBoxShow: false,
                 table: {
                     header: [],
                     list: [],
@@ -167,6 +167,9 @@
             this.initLocalSelected()
         },
         methods: {
+            showSelectBox () {
+                this.$emit('update:isSelectBoxShow', true)
+            },
             initLocalSelected () {
                 if (Array.isArray(this.selected)) {
                     let availableSelected = this.selected.filter(({id}) => id !== '')
@@ -373,7 +376,7 @@
                 })
             },
             handleConfirm () {
-                this.isSelectBoxShow = false
+                this.$emit('update:isSelectBoxShow', false)
                 this.setLocalSelected()
                 let availableId = this.table.chooseId.filter(id => {
                     return !!this.table.allHost.find(({bk_host_id: bkHostId}) => bkHostId === id)
@@ -385,7 +388,7 @@
                 this.localSelected = selectedHost.map(({bk_host_innerip: bkHostInnerip}) => bkHostInnerip)
             },
             handleCancel () {
-                this.isSelectBoxShow = false
+                this.$emit('update:isSelectBoxShow', false)
                 this.initLocalSelected()
             },
             clear () {
