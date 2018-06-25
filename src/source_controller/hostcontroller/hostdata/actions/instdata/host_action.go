de242bd92af3d38c52a176670e983615af9e0013
wusendong <wusendong@gmail.com>
2018-06-25 15:30:26 +0800

    update request change

--- src/source_controller/hostcontroller/hostdata/actions/instdata/host_action.go
+++ src/source_controller/hostcontroller/hostdata/actions/instdata/host_action.go
@@ -13,29 +13,28 @@
 package instdata
 
 import (
-	"configcenter/src/common"
-	"configcenter/src/common/base"
-	"configcenter/src/common/blog"
-	"configcenter/src/common/core/cc/actions"
-	"configcenter/src/common/core/cc/api"
-	"configcenter/src/common/util"
-	eventtypes "configcenter/src/scene_server/event_server/types"
-	"configcenter/src/source_controller/common/commondata"
-	"configcenter/src/source_controller/common/eventdata"
-	"configcenter/src/source_controller/common/instdata"
-	"encoding/json"
-	redis "gopkg.in/redis.v5"
 	"io/ioutil"
 	"net/http"
 	"strconv"
 	"strings"
 	"time"
 
-	dcCommon "configcenter/src/scene_server/datacollection/common"
-
+	"encoding/json"
 	simplejson "github.com/bitly/go-simplejson"
-
 	"github.com/emicklei/go-restful"
+	redis "gopkg.in/redis.v5"
+
+	"configcenter/src/common"
+	"configcenter/src/common/base"
+	"configcenter/src/common/blog"
+	"configcenter/src/common/core/cc/actions"
+	"configcenter/src/common/core/cc/api"
+	"configcenter/src/common/util"
+	dcCommon "configcenter/src/scene_server/datacollection/common"
+	eventtypes "configcenter/src/scene_server/event_server/types"
+	"configcenter/src/source_controller/common/commondata"
+	"configcenter/src/source_controller/common/eventdata"
+	"configcenter/src/source_controller/common/instdata"
 )
 
 var host *hostAction = &hostAction{}
@@ -163,9 +162,8 @@ func (cli *hostAction) GetHostSnap(req *restful.Request, resp *restful.Response)
 	cli.CallResponseEx(func() (int, interface{}, error) {
 		hostID := req.PathParameter("bk_host_id")
 
-		data := common.KvMap{"key": dcCommon.RedisSnapKeyPrefix + hostID}
-		var result interface{}
-		err := cli.CC.CacheCli.GetOneByCondition("Get", nil, data, &result)
+		var result string
+		err := redisCli.Get(dcCommon.RedisSnapKeyPrefix + hostID).Scan(&result)
 		if err != nil {
 			statuscode := 0
 			err := redisCli.Get(dcCommon.RedisSnapKeyChannelStatus).Scan(&statuscode)
