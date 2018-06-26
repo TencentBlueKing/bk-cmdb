c10577eeba07efb1b68d8b7ab6bb8900856d8aef
ProtosL <644074553@qq.com>
2018-06-26 16:07:06 +0800

    minor：跳转agent改为跳转节点管理

--- src/ui/src/pages/resource/resource.vue
+++ src/ui/src/pages/resource/resource.vue
@@ -69,7 +69,7 @@
                         <p>{{$t("HostResourcePool['agent安装说明']")}}</p>
                         <div class="back-contain">
                             <i class="icon-cc-skip"></i>
-                            <a href="javascript:void(0)" @click="openAgentApp">{{$t("HostResourcePool['点此进入Agent安装APP']")}}</a>
+                            <a href="javascript:void(0)" @click="openAgentApp">{{$t("HostResourcePool['点此进入节点管理']")}}</a>
                         </div>
                     </div>
                 </bk-tabpanel>
