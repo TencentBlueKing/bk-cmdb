d97277f10919662497d46071d5eefbf6b4321e7b
wusendong <wusendong@gmail.com>
2018-06-27 12:23:15 +0800

    update pr

--- src/web_server/application/middleware/user/owner.go
+++ src/web_server/application/middleware/user/owner.go
@@ -1,4 +1,16 @@
-package middleware
+/*
+ * Tencent is pleased to support the open source community by making 蓝鲸 available.
+ * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package user
 
 import (
 	"configcenter/src/common"
@@ -6,6 +18,7 @@ import (
 	"configcenter/src/common/core/cc/api"
 	"configcenter/src/common/http/httpclient"
 	"configcenter/src/scene_server/validator"
+	"configcenter/src/web_server/application/middleware/types"
 	webCommon "configcenter/src/web_server/common"
 	"encoding/json"
 	"fmt"
@@ -113,7 +126,7 @@ func (m *OwnerManager) defaultAppIsExist() (bool, error) {
 		return false, err
 	}
 
-	result := SearchAppResult{}
+	result := types.SearchAppResult{}
 	err = json.Unmarshal(reply, &result)
 	if nil != err {
 		return false, err
