d2a9d66037060402134da371da834382e57f2b91
rentiansheng <rentiansheng@163.com>
2018-07-12 14:34:45 +0800

    change log info

--- src/scene_server/host_server/service/usercustom.go
+++ src/scene_server/host_server/service/usercustom.go
@@ -68,7 +68,7 @@ func (s *Service) GetUserCustom(req *restful.Request, resp *restful.Response) {
 	user := util.GetUser(pheader)
 	result, err := s.CoreAPI.HostController().User().GetUserCustomByUser(context.Background(), user, pheader)
 	if err != nil || (err == nil && !result.Result) {
-		blog.Error("save user custom, but get user custom failed, err: %v, %v", err, result.ErrMsg)
+		blog.Error("get user custom, but get user custom failed, err: %v, %v", err, result.ErrMsg)
 		resp.WriteError(http.StatusBadRequest, &metadata.RespError{Msg: defErr.Error(common.CC_Err_Comm_USER_CUSTOM_SAVE_FAIL)})
 		return
 	}
