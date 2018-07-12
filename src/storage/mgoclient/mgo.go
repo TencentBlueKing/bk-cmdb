e6a69aacfc2cbd30a0dc73af9342ce4a130186ee
owenlxu <owenlxu@tencent.com>
2018-07-12 16:08:01 +0800

    Support cluster mode

--- src/storage/mgoclient/mgo.go
+++ src/storage/mgoclient/mgo.go
@@ -67,11 +67,9 @@ func NewMgoCli(host, port, usr, pwd, mechanism, database string) (*MgoCli, error
 
 // Open open the connection
 func (m *MgoCli) Open() error {
-	// mgo.SetDebug(true)
-	// mgo.SetLogger(log.New(os.Stderr, "", log.LstdFlags))
 
 	dialInfo := &mgo.DialInfo{
-		Addrs:     []string{m.host + ":" + m.port},
+		Addrs:     strings.Split(m.host, ","),
 		Direct:    false,
 		Timeout:   time.Second * 5,
 		Database:  m.dbName,
