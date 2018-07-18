6a5cee6345679ee8c7303d369f9c8dd03aaf79d6
wusendong <wusendong@gmail.com>
2018-07-18 10:52:02 +0800

    update datatacollection

--- src/scene_server/datacollection/app/server.go
+++ src/scene_server/datacollection/app/server.go
@@ -1,15 +1,3 @@
-/*
- * Tencent is pleased to support the open source community by making 蓝鲸 available.
- * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- * http://opensource.org/licenses/MIT
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package app
 
 import (
@@ -17,8 +5,6 @@ import (
 	"fmt"
 	"os"
 	"time"
-
-	"configcenter/src/scene_server/datacollection/datacollection"
 
 	"github.com/emicklei/go-restful"
 
@@ -30,6 +16,7 @@ import (
 	"configcenter/src/common/types"
 	"configcenter/src/common/version"
 	"configcenter/src/scene_server/datacollection/app/options"
+	"configcenter/src/scene_server/datacollection/datacollection"
 	svc "configcenter/src/scene_server/datacollection/service"
 	"configcenter/src/storage/mgoclient"
 	"configcenter/src/storage/redisclient"
@@ -69,26 +56,27 @@ func Run(ctx context.Context, op *options.ServerOption) error {
 		Server:       server,
 	}
 
-	dcSvr := new(DCServer)
+	process := new(DCServer)
 	engine, err := backbone.NewBackbone(ctx, op.ServConf.RegDiscover,
 		types.CC_MODULE_DATACOLLECTION,
 		op.ServConf.ExConfig,
-		dcSvr.onHostConfigUpdate,
+		process.onHostConfigUpdate,
 		bonC)
 	if err != nil {
 		return fmt.Errorf("new backbone failed, err: %v", err)
 	}
 
 	service.Engine = engine
-	dcSvr.Core = engine
-	dcSvr.Service = service
+	process.Core = engine
+	process.Service = service
 	for {
-		if dcSvr.Config == nil {
+		if process.Config == nil {
 			time.Sleep(time.Second * 2)
 			blog.V(3).Info("config not found, retry 2s later")
+			continue
 		}
 
-		db, err := mgoclient.NewFromConfig(dcSvr.Config.MongoDB)
+		db, err := mgoclient.NewFromConfig(process.Config.MongoDB)
 		if err != nil {
 			return fmt.Errorf("connect mongo server failed %s", err.Error())
 		}
@@ -96,16 +84,18 @@ func Run(ctx context.Context, op *options.ServerOption) error {
 		if err != nil {
 			return fmt.Errorf("connect mongo server failed %s", err.Error())
 		}
-		dcSvr.Service.SetDB(db)
+		process.Service.SetDB(db)
 
-		cache, err := redisclient.NewFromConfig(dcSvr.Config.Redis)
+		cache, err := redisclient.NewFromConfig(process.Config.CCRedis)
 		if err != nil {
 			return fmt.Errorf("connect redis server failed %s", err.Error())
 		}
-		dcSvr.Service.SetCache(cache)
+		process.Service.SetCache(cache)
+		break
 	}
 
-	datacollection.Run()
+	datacollection.NewDataCollection(process.Config, process.Core)
+
 	select {}
 	return nil
 }
