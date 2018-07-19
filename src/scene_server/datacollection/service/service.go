82359f474afb53d6f9577a46dd16fff0b1cfd1ad
wusendong <wusendong@gmail.com>
2018-07-19 20:31:18 +0800

    update healthz

--- src/scene_server/datacollection/service/service.go
+++ src/scene_server/datacollection/service/service.go
@@ -45,51 +45,54 @@ func (s *Service) WebService() *restful.WebService {
 		return s.CCErr
 	}
 	ws.Path("/collector/v3").Filter(rdapi.AllGlobalFilter(getErrFun)).Produces(restful.MIME_JSON).Consumes(restful.MIME_JSON)
-	ws.GET("/healthz").To(s.Healthz)
-
 	return ws
 }
 
-func (s *Service) Healthz(req *restful.Request, resp *restful.Response) {
-	meta := metric.HealthMeta{IsHealthy: true}
+func (s *Service) Healthz() *restful.WebService {
+	ws := new(restful.WebService)
+	ws.Path("/").Produces(restful.MIME_JSON)
+	ws.Route(ws.GET("/healthz").To(func(req *restful.Request, resp *restful.Response) {
+		meta := metric.HealthMeta{IsHealthy: true}
 
-	// zk health status
-	zkItem := metric.HealthItem{IsHealthy: true, Name: types.CCFunctionalityServicediscover}
-	if err := s.Engine.Ping(); err != nil {
-		zkItem.IsHealthy = false
-		zkItem.Message = err.Error()
-	}
-	meta.Items = append(meta.Items, zkItem)
-
-	// topo server
-	objCtr := metric.HealthItem{IsHealthy: true, Name: types.CC_MODULE_TOPO}
-	if _, err := s.Engine.CoreAPI.Healthz().HealthCheck(types.CC_MODULE_TOPO); err != nil {
-		objCtr.IsHealthy = false
-		objCtr.Message = err.Error()
-	}
-	meta.Items = append(meta.Items, objCtr)
-
-	for _, item := range meta.Items {
-		if item.IsHealthy == false {
-			meta.IsHealthy = false
-			meta.Message = "datacolection server is unhealthy"
-			break
+		// zk health status
+		zkItem := metric.HealthItem{IsHealthy: true, Name: types.CCFunctionalityServicediscover}
+		if err := s.Engine.Ping(); err != nil {
+			zkItem.IsHealthy = false
+			zkItem.Message = err.Error()
 		}
-	}
+		meta.Items = append(meta.Items, zkItem)
 
-	info := metric.HealthInfo{
-		Module:     types.CC_MODULE_DATACOLLECTION,
-		HealthMeta: meta,
-		AtTime:     types.Now(),
-	}
+		// topo server
+		objCtr := metric.HealthItem{IsHealthy: true, Name: types.CC_MODULE_TOPO}
+		if _, err := s.Engine.CoreAPI.Healthz().HealthCheck(types.CC_MODULE_TOPO); err != nil {
+			objCtr.IsHealthy = false
+			objCtr.Message = err.Error()
+		}
+		meta.Items = append(meta.Items, objCtr)
 
-	answer := metric.HealthResponse{
-		Code:    common.CCSuccess,
-		Data:    info,
-		OK:      meta.IsHealthy,
-		Result:  meta.IsHealthy,
-		Message: meta.Message,
-	}
-	resp.Header().Set("Content-Type", "application/json")
-	resp.WriteEntity(answer)
+		for _, item := range meta.Items {
+			if item.IsHealthy == false {
+				meta.IsHealthy = false
+				meta.Message = "datacolection server is unhealthy"
+				break
+			}
+		}
+
+		info := metric.HealthInfo{
+			Module:     types.CC_MODULE_DATACOLLECTION,
+			HealthMeta: meta,
+			AtTime:     types.Now(),
+		}
+
+		answer := metric.HealthResponse{
+			Code:    common.CCSuccess,
+			Data:    info,
+			OK:      meta.IsHealthy,
+			Result:  meta.IsHealthy,
+			Message: meta.Message,
+		}
+		resp.Header().Set("Content-Type", "application/json")
+		resp.WriteEntity(answer)
+	}))
+	return ws
 }
