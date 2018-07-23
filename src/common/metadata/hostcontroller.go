d85d93d64aeb276479f50cbc1f92a40cdc5a0b63
wusendong <wusendong@gmail.com>
2018-07-23 19:53:15 +0800

    update identifier

--- src/common/metadata/hostcontroller.go
+++ src/common/metadata/hostcontroller.go
@@ -112,10 +112,11 @@ type AssignHostToAppParams struct {
 }
 
 type ModuleHost struct {
-	AppID    int64 `json:"bk_biz_id" bson:"bk_biz_id"`
-	HostID   int64 `json:"bk_host_id" bson:"bk_host_id"`
-	ModuleID int64 `json:"bk_module_id" bson:"bk_module_id"`
-	SetID    int64 `json:"bk_set_id" bson:"bk_set_id"`
+	AppID    int64  `json:"bk_biz_id" bson:"bk_biz_id"`
+	HostID   int64  `json:"bk_host_id" bson:"bk_host_id"`
+	ModuleID int64  `json:"bk_module_id" bson:"bk_module_id"`
+	SetID    int64  `json:"bk_set_id" bson:"bk_set_id"`
+	OwnerID  string `json:"bk_supplier_account" bson:"bk_supplier_account"`
 }
 
 type HostConfig struct {
