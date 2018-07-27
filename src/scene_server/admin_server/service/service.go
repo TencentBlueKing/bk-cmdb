9756ca336783552ce3d37a3a9ee2fa7e4181e9e9
wusendong <wusendong@gmail.com>
2018-07-27 11:39:56 +0800

    解决 多关联业务搜索寄结果不准确

--- src/scene_server/admin_server/service/service.go
+++ src/scene_server/admin_server/service/service.go
@@ -38,7 +38,7 @@ func (s *Service) WebService() *restful.WebService {
 	getErrFun := func() errors.CCErrorIf {
 		return s.CCErr
 	}
-	ws.Path("/migrate/v3").Filter(rdapi.AllGlobalFilter(getErrFun)).Produces(restful.MIME_JSON).Consumes(restful.MIME_JSON)
+	ws.Path("/migrate/v3").Filter(rdapi.AllGlobalFilter(getErrFun)).Produces(restful.MIME_JSON)
 
 	ws.Route(ws.POST("/migrate/{distribution}/{ownerID}").To(s.migrate))
 	ws.Route(ws.POST("/migrate/system/hostcrossbiz/{ownerID}").To(s.Set))
