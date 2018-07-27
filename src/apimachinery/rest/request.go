cefcfa145d1ef43479d5d271800232146fc4ba90
rentiansheng <rentiansheng@163.com>
2018-07-27 14:34:57 +0800

    add log info

--- src/apimachinery/rest/request.go
+++ src/apimachinery/rest/request.go
@@ -288,7 +288,7 @@ func (r *Result) Into(obj interface{}) error {
 	if 0 != len(r.Body) {
 		err := json.Unmarshal(r.Body, obj)
 		if nil != err {
-			blog.Errorf("reply:%s", string(r.Body))
+			blog.Errorf("http reply not json, reply:%s", string(r.Body))
 			return err
 		}
 	}
