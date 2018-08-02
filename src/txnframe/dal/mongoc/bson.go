d284a8954af620314aaaec68f66243813bec6ad9
uriwang <uriwang@tencent.com>
2018-08-02 23:31:59 +0800

    完成mongoc 接口声明，完成mongoc 的find 和insertone 接口封装
    Former-commit-id: a23df706c2d3a9ca491d318178d69b7debbe7b70

---
+++ src/txnframe/dal/mongoc/bson.go
@@ -0,0 +1,39 @@
+/*
+ * Tencent is pleased to support the open source community by making čé˛¸ available.
+ * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mongoc
+
+// #cgo CFLAGS: -I/usr/local/include/libbson-1.0
+// #cgo CFLAGS: -I/usr/local/include/libmongoc-1.0
+// #cgo LDFLAGS: -lmongoc-1.0
+// #cgo LDFLAGS: -lbson-1.0
+// #include <stdlib.h>
+// #include "mongo.h"
+import "C"
+
+import "unsafe"
+
+type bson struct {
+	data string
+}
+
+func (b *bson) ToDocument() (*C.bson_t, error) {
+
+	var err C.bson_error_t
+	innerData := C.CString(b.data)
+	defer C.free(unsafe.Pointer(innerData))
+	bson := C.bson_new_from_json((*C.uchar)(unsafe.Pointer(innerData)), -1, &err)
+	if nil == bson {
+		return nil, TransformError(err)
+	}
+	return bson, nil
+}
