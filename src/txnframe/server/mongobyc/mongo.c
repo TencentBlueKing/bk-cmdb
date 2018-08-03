74cfcac420f2e5a3c7520267efdcb308676d12e0
uriwang <uriwang@tencent.com>
2018-08-03 17:29:42 +0800

    修改license

--- src/txnframe/server/mongobyc/mongo.c
+++ src/txnframe/server/mongobyc/mongo.c
@@ -1,5 +1,5 @@
 /*
- * Tencent is pleased to support the open source community by making čé˛¸ available.
+ * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
  * Licensed under the MIT License (the "License"); you may not use this file except 
  * in compliance with the License. You may obtain a copy of the License at
@@ -9,6 +9,7 @@
  * either express or implied. See the License for the specific language governing permissions and 
  * limitations under the License.
  */
+ 
 
 #include "mongo.h"
 
