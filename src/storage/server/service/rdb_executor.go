4ce1cb59645e3dd60a40a755016c41f36459dc24
wusendong <wusendong@gmail.com>
2018-08-09 16:23:55 +0800

    rename packages

---
+++ src/storage/server/service/rdb_executor.go
@@ -0,0 +1,112 @@
+/*
+ * Tencent is pleased to support the open source community by making 蓝鲸 available.
+ * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package service
+
+import (
+	"configcenter/src/common/blog"
+	"configcenter/src/common/util"
+	"configcenter/src/storage/mongobyc"
+	"configcenter/src/storage/mongobyc/findopt"
+	"configcenter/src/storage/rpc"
+	"configcenter/src/storage/types"
+	"context"
+)
+
+type CollectionFunc func(collName string) mongobyc.CollectionInterface
+
+// ExecuteCollection execute collection operation to db
+func ExecuteCollection(ctx context.Context, collFunc CollectionFunc, opcode types.OPCode, decoder rpc.Decoder) (*types.OPREPLY, error) {
+	executor := &collectionExecutor{ctx: ctx, collection: collFunc, opcode: opcode, msg: decoder}
+	executor.execute()
+	return &executor.reply, executor.execerr
+}
+
+type collectionExecutor struct {
+	ctx context.Context
+
+	opcode types.OPCode
+	msg    rpc.Decoder
+
+	collection CollectionFunc
+
+	reply   types.OPREPLY
+	execerr error
+}
+
+func (e *collectionExecutor) execute() {
+	switch e.opcode {
+	case types.OPInsert:
+		e.insert()
+	case types.OPUpdate:
+		e.update()
+	case types.OPDelete:
+		e.delete()
+	case types.OPFind:
+		e.find()
+	case types.OPFindAndModify:
+		e.findAndModify()
+	case types.OPCount:
+		e.count()
+	}
+	if e.execerr != nil {
+		e.reply.OK = false
+		e.reply.Message = e.execerr.Error()
+	} else {
+		e.reply.OK = true
+	}
+}
+
+func (e collectionExecutor) insert() {
+	msg := types.OPINSERT{}
+	e.msg.Decode(&msg)
+	e.execerr = e.collection(msg.Collection).InsertMany(e.ctx, util.ConverToInterfaceSlice(msg.DOCS), nil)
+}
+func (e collectionExecutor) update() {
+	msg := types.OPUPDATE{}
+	e.msg.Decode(&msg)
+	_, e.execerr = e.collection(msg.Collection).UpdateMany(e.ctx, msg.Selector, msg.DOC, nil)
+}
+func (e collectionExecutor) delete() {
+	msg := types.OPDELETE{}
+	e.msg.Decode(&msg)
+	_, e.execerr = e.collection(msg.Collection).DeleteMany(e.ctx, msg.Selector, nil)
+}
+
+func (e collectionExecutor) find() {
+	msg := types.OPFIND{}
+	e.msg.Decode(&msg)
+
+	opt := findopt.Many{}
+	opt.Skip = int64(msg.Start)
+	opt.Limit = int64(msg.Limit)
+	opt.Sort = msg.Sort
+
+	blog.Infof("[collectionExecutor] execute %+v", msg)
+	e.execerr = e.collection(msg.Collection).Find(e.ctx, msg.Selector, &opt, &e.reply.Docs)
+	blog.Infof("[collectionExecutor] find result: %+v, err: [%v]", e.reply.Docs, e.execerr)
+}
+
+func (e collectionExecutor) findAndModify() {
+	msg := types.OPFINDANDMODIFY{}
+	e.msg.Decode(&msg)
+	opt := findopt.FindAndModify{}
+	opt.Upsert = msg.Upsert
+	opt.Remove = msg.Remove
+	opt.New = msg.ReturnNew
+	e.execerr = e.collection(msg.Collection).FindAndModify(e.ctx, msg.Selector, msg.DOC, nil, &e.reply.Docs)
+}
+func (e collectionExecutor) count() {
+	msg := types.OPDELETE{}
+	e.msg.Decode(&msg)
+	e.reply.Count, e.execerr = e.collection(msg.Collection).Count(e.ctx, msg.Selector)
+}
---
+++ src/storage/server/service/txn.go
@@ -0,0 +1,135 @@
+/*
+ * Tencent is pleased to support the open source community by making 蓝鲸 available.
+ * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package service
+
+import (
+	"configcenter/src/common/backbone"
+	"configcenter/src/common/blog"
+	"configcenter/src/storage/mongobyc"
+	"configcenter/src/storage/rpc"
+	"configcenter/src/storage/server/manager"
+	"configcenter/src/storage/types"
+	"context"
+	"fmt"
+)
+
+type TXRPC struct {
+	*backbone.Engine
+	ctx    context.Context
+	rpcsrv *rpc.Server
+	man    *manager.TxnManager
+	db     mongobyc.Client
+}
+
+func (t *TXRPC) SetEngine(engine *backbone.Engine) {
+	t.Engine = engine
+}
+
+func (t *TXRPC) SetDB(db mongobyc.Client) {
+	t.db = db
+}
+
+func (t *TXRPC) SetMan(man *manager.TxnManager) {
+	t.man = man
+}
+
+func NewTXRPC(rpcsrv *rpc.Server) *TXRPC {
+	txrpc := new(TXRPC)
+	txrpc.rpcsrv = rpcsrv
+
+	rpcsrv.Handle(types.CommandRDBOperation, txrpc.RDBOperation)
+	return txrpc
+}
+
+func (t *TXRPC) RDBOperation(input *rpc.Message) (interface{}, error) {
+
+	reply := new(types.OPREPLY)
+
+	header := types.MsgHeader{}
+	err := input.Decode(&header)
+	if nil != err {
+		reply.Message = err.Error()
+		return reply, nil
+	}
+
+	blog.V(3).Infof("RDBOperation %+v", header)
+
+	var transaction mongobyc.Session
+	if header.TxnID != "" {
+		session := t.man.GetSession(header.TxnID)
+		if nil == session {
+			reply.Message = "session not found"
+			return reply, nil
+		}
+		transaction = session.Session
+	}
+
+	switch header.OPCode {
+	case types.OPStartTransaction:
+		session, err := t.man.CreateTransaction(header.RequestID, buildProcessor(t.ServerInfo.IP, t.ServerInfo.Port, t.ServerInfo.Pid))
+		if nil != err {
+			reply.Message = err.Error()
+			return reply, nil
+		}
+		reply.OK = true
+		reply.TxnID = session.Txninst.TxnID
+		reply.Processor = session.Txninst.Processor
+		return reply, nil
+	case types.OPCommit:
+		err := t.man.Commit(header.TxnID)
+		if nil != err {
+			reply.Message = err.Error()
+			return reply, nil
+		}
+		reply.OK = true
+		return reply, nil
+	case types.OPAbort:
+		err := t.man.Abort(header.TxnID)
+		if nil != err {
+			reply.Message = err.Error()
+			return reply, nil
+		}
+		reply.OK = true
+		return reply, nil
+	case types.OPInsert, types.OPUpdate, types.OPDelete, types.OPFind, types.OPFindAndModify, types.OPCount:
+		var collectionFunc = t.db.Collection
+		if header.TxnID != "" {
+			collectionFunc = transaction.Collection
+		}
+		return ExecuteCollection(t.ctx, collectionFunc, header.OPCode, input)
+	default:
+		reply.Message = "unknow operation"
+		return reply, nil
+	}
+}
+
+func (*TXRPC) Watch(input interface{}, output string) error {
+	blog.V(3).Infof("Watch %#v", input)
+	return nil
+}
+func (*TXRPC) Search(input interface{}, output string) error {
+	blog.V(3).Infof("Search %#v", input)
+	return nil
+}
+func (*TXRPC) Healthz(input interface{}, output string) error {
+	blog.V(3).Infof("Healthz %#v", input)
+	return nil
+}
+func (*TXRPC) Metrics(input interface{}, output string) error {
+	blog.V(3).Infof("Metrics %#v", input)
+	return nil
+}
+
+func buildProcessor(ip string, port uint, pid int) string {
+	return fmt.Sprintf("%s:%d-%d", ip, port, pid)
+}
