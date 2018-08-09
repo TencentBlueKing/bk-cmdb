db4527ff1ff0b8463b9feed1b3b9317cdaee8433
owenlxu <owenlxu@tencent.com>
2018-08-09 10:08:52 +0800

    commmit cross privilege error
    Former-commit-id: 28feba2031d1160db56c4a90009f5e3de381c9ee

--- src/source_controller/objectcontroller/service/system_action.go
+++ src/source_controller/objectcontroller/service/system_action.go
@@ -45,7 +45,7 @@ func (cli *Service) GetSystemFlag(req *restful.Request, resp *restful.Response) 
 	cond[flag] = hex.EncodeToString(cipherStr) + ownerID
 
 	err := cli.Instance.GetOneByCondition(common.BKTableNameSystem, []string{}, cond, &result)
-	if nil != err && !cli.Instance.IsNotFoundErr(err) {
+	if nil != err {
 		blog.Errorf("get system config error :%v, cond:%#v", err, cond)
 		resp.WriteError(http.StatusBadRequest, &meta.RespError{Msg: defErr.New(common.CCErrObjectSelectInstFailed, err.Error())})
 		return
