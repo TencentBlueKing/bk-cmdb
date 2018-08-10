3e557951fc331a70d644e24a6ac0a2cbd4cbced3
wusendong <wusendong@gmail.com>
2018-08-10 10:20:37 +0800

    sort imports and add license
    Former-commit-id: 533037aa746d52a913dd4b82e44cfc4c8e7b63be

--- src/storage/server/service/service.go
+++ src/storage/server/service/service.go
@@ -1,18 +1,24 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except
+ * Licensed under the MIT License (the "License"); you may not use this file except 
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and
+ * either express or implied. See the License for the specific language governing permissions and 
  * limitations under the License.
  */
-
+ 
 package service
 
 import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/emicklei/go-restful"
+
 	"configcenter/src/common"
 	"configcenter/src/common/backbone"
 	"configcenter/src/common/metadata"
@@ -21,10 +27,6 @@ import (
 	"configcenter/src/storage/mongobyc"
 	"configcenter/src/storage/rpc"
 	"configcenter/src/storage/server/manager"
-	"github.com/emicklei/go-restful"
-	"log"
-	"net/http"
-	"os"
 )
 
 type Service struct {
