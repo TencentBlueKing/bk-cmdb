8b39b5f48e1e1fb52b7632b0481b36d97116f8c3
wusendong <wusendong@gmail.com>
2018-08-10 10:45:51 +0800

    mv types.Filter to dal.Filter

--- src/storage/dal/rdb.go
+++ src/storage/dal/rdb.go
@@ -1,15 +1,15 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package dal
 
 import (
@@ -49,15 +49,15 @@ type RDB interface {
 // Collection collection operation interface
 type Collection interface {
 	// Find 查询多个并反序列化到 Result
-	Find(ctx context.Context, filter types.Filter) Find
+	Find(ctx context.Context, filter Filter) Find
 	// Insert 插入数据, docs 可以为 单个数据 或者 多个数据
 	Insert(ctx context.Context, docs interface{}) error
 	// Update 更新数据
-	Update(ctx context.Context, filter types.Filter, doc interface{}) error
+	Update(ctx context.Context, filter Filter, doc interface{}) error
 	// Delete 删除数据
-	Delete(ctx context.Context, filter types.Filter) error
+	Delete(ctx context.Context, filter Filter) error
 	// Count 统计数量(非事务)
-	Count(ctx context.Context, filter types.Filter) (uint64, error)
+	Count(ctx context.Context, filter Filter) (uint64, error)
 	// CreateIndex 创建索引
 	CreateIndex(ctx context.Context, index Index) error
 	// DropIndex 移除索引
