9f08f0cdace216692718a622d65592c2718b08b1
breezeli <breezeli@tencent.com>
2018-08-27 17:16:19 +0800

    add mock, unit test in apimachinery
    Former-commit-id: 4877c171849f4f7937f6229560aff9a9ee67cdeb

---
+++ src/apimachinery/clientset_mock.go
@@ -0,0 +1,26 @@
+package apimachinery
+
+type MockClientSetInterface interface {
+	ClientSetInterface
+	MockDo(output interface{}) ClientSetInterface
+}
+
+func NewMockApiMachinery() MockClientSetInterface {
+	return &MockClientSet{
+		ClientSet: NewMockClientSet(),
+	}
+}
+
+type MockClientSet struct {
+	*ClientSet
+}
+
+func (mc *MockClientSet) MockDo(output interface{}) ClientSetInterface {
+	mc.ClientSet.Mock.SetMockData = true
+	mc.ClientSet.Mock.MockData = output
+	// = util.MockInfo{
+	//     SetMockData: true,
+	//     MockData: output,
+	// }
+	return mc.ClientSet
+}
