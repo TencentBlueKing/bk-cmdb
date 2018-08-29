cb719269d91a93a302a39769f139ed01c4607bd6
ProtosL <644074553@qq.com>
2018-08-30 00:56:10 +0800

    feature: 模型管理

--- src/ui/src/views/model/details/model-field/char.vue
+++ src/ui/src/views/model/details/model-field/char.vue
@@ -1,6 +1,36 @@
 <template>
     <div class="form-item">
         <label class="form-label">{{$t('Common["正则验证"]')}}</label>
-        <input type="text" class="cmdb-form-input" name="" placeholder="">
+        <input type="text" class="cmdb-form-input" name="" placeholder="" 
+            v-model="localValue"
+            @input="handleInput">
     </div>
 </template>
+
+<script>
+    export default {
+        props: {
+            value: {
+                default: ''
+            }
+        },
+        data () {
+            return {
+                localValue: ''
+            }
+        },
+        watch: {
+            value () {
+                this.localValue = this.value === '' ? '' : this.value
+            }
+        },
+        created () {
+            this.localValue = this.value === '' ? '' : this.value
+        },
+        methods: {
+            handleInput () {
+                this.$emit('input', this.localValue)
+            }
+        }
+    }
+</script>
