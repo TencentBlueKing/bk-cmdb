2278e366a4e1952708b64cda9c0e9b81ef6f9baa
ProtosL <644074553@qq.com>
2018-08-29 21:54:24 +0800

    feature: 模型管理
    Former-commit-id: ebc41b4ca750e7eadfe562bdf3d8f92e947e7615

--- src/ui/src/components/model-field/int.vue
+++ src/ui/src/components/model-field/int.vue
@@ -2,11 +2,41 @@
     <div>
         <div class="form-item has-right-content">
             <label class="form-label">{{$t('ModelManagement["最小值"]')}}</label>
-            <input type="text" class="cmdb-form-input" name="" placeholder="">
+            <div class="input-box">
+                <input type="text" class="cmdb-form-input" placeholder=""
+                    v-model="min"
+                    v-validate="`number`"
+                    maxlength="11"
+                    :name="'min'">
+                <span v-show="errors.has('min')" class="error-msg color-danger">{{ errors.first('min') }}</span>
+            </div>
         </div>
         <div class="form-item">
             <label class="form-label">{{$t('ModelManagement["最大值"]')}}</label>
-            <input type="text" class="cmdb-form-input" name="" placeholder="">
+            <div class="input-box">
+                <input type="text" class="cmdb-form-input" placeholder="" v-model="max"
+                    name="max"
+                    v-validate="`number|isBigger:${min}`">
+                <span v-show="errors.has('max')" class="error-msg color-danger">{{ errors.first('max') }}</span>
+            </div>
         </div>
     </div>
 </template>
+
+<script>
+    export default {
+        data () {
+            return {
+                max: '',
+                min: ''
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .form-label {
+        vertical-align: top;
+        line-height: 30px;
+    }
+</style>
