ea00c84b21802ffc713721e95fd5379fa2e4f6da
peibiaoyang(杨培彪) <peibiaoyang@tencent.com>
2018-08-31 18:44:01 +0800

    update: magicbox i18n
    Former-commit-id: b1061b2f73479f9b2c7457918578c0219793b1a8

--- src/ui/src/i18n/lang/messages.js
+++ src/ui/src/i18n/lang/messages.js
@@ -1,8 +1,6 @@
 import en from './en.json'
 import cn from './cn.json'
-import magicboxEn from '@/magicbox/src/locale/lang/en-US'
-import magicboxCn from '@/magicbox/src/locale/lang/zh-CN'
 export default {
-    en: Object.assign(magicboxEn, en),
-    'zh_CN': Object.assign(magicboxCn, cn)
+    en,
+    'zh_CN': cn
 }
