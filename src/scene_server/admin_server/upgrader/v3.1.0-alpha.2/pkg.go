733dfc5505e804087babd14ac771255b8c5b8f70
uriwang <uriwang@tencent.com>
2018-09-03 10:53:51 +0800

    解决代码合并后的编译问题

--- src/scene_server/admin_server/upgrader/v3.1.0-alpha.2/pkg.go
+++ src/scene_server/admin_server/upgrader/v3.1.0-alpha.2/pkg.go
@@ -13,20 +13,22 @@
 package v3v0v1alpha2
 
 import (
+	"context"
+
 	"configcenter/src/scene_server/admin_server/upgrader"
-	"configcenter/src/storage"
+	"configcenter/src/storage/dal"
 )
 
 func init() {
 	upgrader.RegistUpgrader("v3.1.0-alpha.2", upgrade)
 }
 
-func upgrade(db storage.DI, conf *upgrader.Config) (err error) {
-	err = addBkStartParamRegex(db, conf)
+func upgrade(ctx context.Context, db dal.RDB, conf *upgrader.Config) (err error) {
+	err = addBkStartParamRegex(ctx, db, conf)
 	if err != nil {
 		return err
 	}
-	err = updateLanguageField(db, conf)
+	err = updateLanguageField(ctx, db, conf)
 	if err != nil {
 		return err
 	}
