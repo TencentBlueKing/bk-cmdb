bd51aa468345508d2d085550416240d87790c556
uriwang <uriwang@tencent.com>
2018-09-05 10:08:56 +0800

    delete example directory

--- src/framework/core/output/module/client/v3/model_classification_test.go
+++ src/framework/core/output/module/client/v3/model_classification_test.go
@@ -1,31 +1,32 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package v3_test
 
 import (
+	"fmt"
+	"testing"
+
 	"configcenter/src/framework/common"
 	"configcenter/src/framework/core/config"
 	"configcenter/src/framework/core/output/module/client"
 	"configcenter/src/framework/core/types"
-	"fmt"
-	"testing"
 )
 
 func TestCreateClassification(t *testing.T) {
 
 	cli := client.NewForConfig(config.Config{"core.supplierAccount": "0", "core.user": "build_user", "core.ccaddress": "http://test.apiserver:8080"}, nil)
 
-	id, err := cli.CCV3().Classification().CreateClassification(types.MapStr{
+	id, err := cli.CCV3(client.Params{}).Classification().CreateClassification(types.MapStr{
 		"bk_classification_id":   common.UUID(),
 		"bk_classification_name": fmt.Sprintf("test_%s", common.UUID()),
 	})
@@ -42,7 +43,7 @@ func TestDeleteClassification(t *testing.T) {
 
 	cond := common.CreateCondition().Field("id").Eq(9)
 
-	err := cli.CCV3().Classification().DeleteClassification(cond)
+	err := cli.CCV3(client.Params{}).Classification().DeleteClassification(cond)
 
 	if nil != err {
 		t.Errorf("failed to create, error info is %s", err.Error())
@@ -56,7 +57,7 @@ func TestUpdateClassification(t *testing.T) {
 
 	cond := common.CreateCondition().Field("id").Eq(10)
 
-	err := cli.CCV3().Classification().UpdateClassification(types.MapStr{"bk_classification_name": "test_update"}, cond)
+	err := cli.CCV3(client.Params{}).Classification().UpdateClassification(types.MapStr{"bk_classification_name": "test_update"}, cond)
 
 	if nil != err {
 		t.Errorf("failed to update, error info is %s", err.Error())
@@ -69,7 +70,7 @@ func TestSearchClassification(t *testing.T) {
 
 	cond := common.CreateCondition().Field("bk_classification_name").Like("test_")
 
-	dataMap, err := cli.CCV3().Classification().SearchClassifications(cond)
+	dataMap, err := cli.CCV3(client.Params{}).Classification().SearchClassifications(cond)
 
 	if nil != err {
 		t.Errorf("failed to create, error info is %s", err.Error())
@@ -86,7 +87,7 @@ func TestSearchClassificationWithObjects(t *testing.T) {
 
 	cond := common.CreateCondition().Field("bk_classification_name").Like("业务")
 
-	dataMap, err := cli.CCV3().Classification().SearchClassificationWithObjects(cond)
+	dataMap, err := cli.CCV3(client.Params{}).Classification().SearchClassificationWithObjects(cond)
 
 	if nil != err {
 		t.Errorf("failed to create, error info is %s", err.Error())
--- src/framework/core/output/module/client/v3/model_test.go
+++ src/framework/core/output/module/client/v3/model_test.go
@@ -1,31 +1,32 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package v3_test
 
 import (
+	"fmt"
+	"testing"
+
 	"configcenter/src/framework/common"
 	"configcenter/src/framework/core/config"
 	"configcenter/src/framework/core/output/module/client"
 	"configcenter/src/framework/core/types"
-	"fmt"
-	"testing"
 )
 
 func TestCreateObject(t *testing.T) {
 
 	cli := client.NewForConfig(config.Config{"core.supplierAccount": "0", "core.user": "build_user", "core.ccaddress": "http://test.apiserver:8080"}, nil)
 
-	id, err := cli.CCV3().Model().CreateObject(types.MapStr{
+	id, err := cli.CCV3(client.Params{}).Model().CreateObject(types.MapStr{
 		"bk_supplier_account":  "0",
 		"bk_obj_id":            common.UUID(),
 		"bk_classification_id": "bk_biz_topo",
@@ -43,7 +44,7 @@ func TestDeleteObject(t *testing.T) {
 	cli := client.NewForConfig(config.Config{"core.supplierAccount": "0", "core.user": "build_user", "core.ccaddress": "http://test.apiserver:8080"}, nil)
 	cond := common.CreateCondition().Field("id").Eq(16)
 
-	err := cli.CCV3().Model().DeleteObject(cond)
+	err := cli.CCV3(client.Params{}).Model().DeleteObject(cond)
 
 	if nil != err {
 		t.Errorf("failed to delete, error info is %s", err.Error())
@@ -57,7 +58,7 @@ func TestUpdateObject(t *testing.T) {
 
 	cond := common.CreateCondition().Field("id").Eq(16)
 
-	err := cli.CCV3().Model().UpdateObject(types.MapStr{"bk_obj_name": "test_update"}, cond)
+	err := cli.CCV3(client.Params{}).Model().UpdateObject(types.MapStr{"bk_obj_name": "test_update"}, cond)
 
 	if nil != err {
 		t.Errorf("failed to update, error info is %s", err.Error())
@@ -70,7 +71,7 @@ func TestSearchObject(t *testing.T) {
 
 	cond := common.CreateCondition().Field("bk_obj_id").Like("host")
 
-	dataMap, err := cli.CCV3().Model().SearchObjects(cond)
+	dataMap, err := cli.CCV3(client.Params{}).Model().SearchObjects(cond)
 
 	if nil != err {
 		t.Errorf("failed to search, error info is %s", err.Error())
