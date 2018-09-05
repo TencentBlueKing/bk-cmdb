d6dd0cdba49087006ec6f7fd95cc2ec457732904
wusendong <wusendong@gmail.com>
2018-09-05 15:53:57 +0800

    fix subsctiption not refresh bug
    Former-commit-id: 6666a45c7231c6600aeb0a341bd6984302b23130

--- src/common/metadata/eventserver.go
+++ src/common/metadata/eventserver.go
@@ -16,6 +16,8 @@ import (
 	"database/sql/driver"
 	"encoding/json"
 	"errors"
+	"sort"
+	"strings"
 	"time"
 
 	"configcenter/src/common/types"
@@ -79,6 +81,9 @@ func (Subscription) TableName() string {
 }
 
 func (s Subscription) GetCacheKey() string {
+	eventnames := strings.Split(s.SubscriptionForm, ",")
+	sort.Strings(eventnames)
+	s.SubscriptionForm = strings.Join(eventnames, ",")
 	ns := &Subscription{
 		SubscriptionID:   s.SubscriptionID,
 		CallbackURL:      s.CallbackURL,
