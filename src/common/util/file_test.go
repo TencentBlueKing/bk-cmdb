b2fc487483f4047bf3209f40f227c43f7d122f26
chuyinfeng <chuyinfeng@gmail.com>
2018-09-11 20:09:51 +0800

    add testcase
    Former-commit-id: 3c457b619d6becd748ddfdc54e3cb8fe2064086d

--- src/common/util/file_test.go
+++ src/common/util/file_test.go
@@ -14,27 +14,21 @@ package util
 
 import "testing"
 
-func TestGetDailAddress(t *testing.T) {
+func TestFileExists(t *testing.T) {
 	type args struct {
-		URL string
+		name string
 	}
 	tests := []struct {
-		name    string
-		args    args
-		want    string
-		wantErr bool
+		name string
+		args args
+		want bool
 	}{
-		{"", args{"http://localhost:80/path?q=a"}, "localhost:80", false},
+		{"", args{"file_not_found"}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := GetDailAddress(tt.args.URL)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetDailAddress() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("GetDailAddress() = %v, want %v", got, tt.want)
+			if got := FileExists(tt.args.name); got != tt.want {
+				t.Errorf("FileExists() = %v, want %v", got, tt.want)
 			}
 		})
 	}
