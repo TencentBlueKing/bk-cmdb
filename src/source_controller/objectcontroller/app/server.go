8be3828f9b62f004001ef750714771bf158dc7d5
wusendong <wusendong@gmail.com>
2018-09-12 22:16:16 +0800

    fix bug
    Former-commit-id: 93a636e1e4d476feb97fa7062ed5afefbfa75ce2

--- src/source_controller/objectcontroller/app/server.go
+++ src/source_controller/objectcontroller/app/server.go
@@ -72,7 +72,7 @@ func Run(ctx context.Context, op *options.ServerOption) error {
 	objCtr := new(ObjectController)
 	objCtr.Service = coreService
 	objCtr.Service.Core, err = backbone.NewBackbone(ctx, op.ServConf.RegDiscover,
-		types.CC_MODULE_HOSTCONTROLLER,
+		types.CC_MODULE_OBJECTCONTROLLER,
 		op.ServConf.ExConfig,
 		objCtr.onObjectConfigUpdate,
 		bonC)
