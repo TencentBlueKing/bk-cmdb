c8368693a7f12b6203ec22420af6f845824a3419
freakkid <904811062@qq.com>
2018-09-20 17:39:51 +0800

    rewrite check net device function #1078

--- src/scene_server/datacollection/logics/common.go
+++ src/scene_server/datacollection/logics/common.go
@@ -11,58 +11,42 @@ import (
 
 // by checking if bk_obj_id and bk_obj_name function parameter are valid net device object or not
 // one of bk_obj_id and bk_obj_name can be empty and will return both value if no error
-func (lgc *Logics) checkNetObject(objID string, objName string, pheader http.Header) (string, string, bool, error) {
+func (lgc *Logics) checkNetObject(objID string, objName string, pheader http.Header) (string, string, error) {
 	defErr := lgc.Engine.CCErr.CreateDefaultCCErrorIf(util.GetLanguage(pheader))
 
 	if "" == objName && "" == objID {
 		blog.Errorf("check net device object, empty bk_obj_id and bk_obj_name")
-		return "", "", false, defErr.Errorf(common.CCErrCommParamsLostField, common.BKObjIDField)
+		return "", "", defErr.Errorf(common.CCErrCommParamsLostField, common.BKObjIDField)
 	}
 
-	// netDeviceObjID is a net device objID from objName
-	var (
-		netDeviceObjID string
-		err            error
-	)
+	objCond := map[string]interface{}{
+		common.BKOwnerIDField:          util.GetOwnerID(pheader),
+		common.BKClassificationIDField: common.BKNetwork}
 
-	// one of objectName and objectID must not be empty
-	// if objectName is not empty, get net device objectID from objName
 	if "" != objName {
-		netDeviceObjID, err = lgc.getNetDeviceObjIDFromObjName(objName, pheader)
-		if nil != err {
-			blog.Errorf("check net device object, error: %v", err)
-			return "", "", false, err
-		}
-
-		// if objName is not empty, netDeviceObjID must not be empty
-		if "" == netDeviceObjID {
-			blog.Errorf("check net device object, get empty objID from object name [%s]", objName)
-			return "", "", false, defErr.Errorf(common.CCErrCollectObjNameNotNetDevice)
-		}
-
-		// return true if objectID is empty or netDeviceObjID and objectID are the same
-		switch {
-		case "" == objID:
-			objID = netDeviceObjID
-			return objID, objName, true, nil
-		case netDeviceObjID == objID:
-			return objID, objName, true, nil
-		}
-
-		// objectID are not empty, netDeviceObjID and objectID are the same
-		blog.Errorf("check net device object, get objID [%s] from object name [%s], different between object ID [%s]",
-			netDeviceObjID, objID, objName)
-		return "", "", false, defErr.Errorf(common.CCErrCollectDiffObjIDAndName)
+		objCond[common.BKObjNameField] = objName
+	}
+	if "" != objID {
+		objCond[common.BKObjIDField] = objID
 	}
 
-	// objectName from function parameter is empty and objectID must not be empty
-	// check if objectID is one of net collect device, such as bk_router, bk_switch...
-	isNetDeviceObjID, err := lgc.checkIfObjIDIsNetDevice(objID, pheader)
+	objResult, err := lgc.CoreAPI.ObjectController().Meta().SelectObjects(context.Background(), pheader, objCond)
 	if nil != err {
-		return "", "", false, err
+		blog.Errorf("check net device object, get net device object fail, error: %v, condition [%#v]", err, objCond)
+		return "", "", defErr.Errorf(common.CCErrObjectSelectInstFailed)
 	}
 
-	return objID, objName, isNetDeviceObjID, nil
+	if !objResult.Result {
+		blog.Errorf("check net device object, errors: %s, condition [%#v]", objResult.ErrMsg, objCond)
+		return "", "", defErr.Errorf(objResult.Code)
+	}
+
+	if nil == objResult.Data || 0 == len(objResult.Data) {
+		blog.Errorf("check net device object, device object is not exist, condition [%#v]", objCond)
+		return "", "", defErr.Errorf(common.CCErrCollectObjNameNotNetDevice)
+	}
+
+	return objResult.Data[0].ObjectID, objResult.Data[0].ObjectName, nil
 }
 
 // by checking if bk_obj_id and bk_obj_name function parameter are valid net device object or not
