66d7551e4270e7402ccee25fea2bb7295ac798e3
freakkid <904811062@qq.com>
2018-09-20 17:15:03 +0800

    add check property and device function #1078
    Former-commit-id: c7d3d4ff6aaee22f53c972986582097eee10d1ad

--- src/scene_server/datacollection/logics/common.go
+++ src/scene_server/datacollection/logics/common.go
@@ -1,6 +1,7 @@
 package logics
 
 import (
+	"context"
 	"net/http"
 
 	"configcenter/src/common"
@@ -10,7 +11,7 @@ import (
 
 // by checking if bk_obj_id and bk_obj_name function parameter are valid net device object or not
 // one of bk_obj_id and bk_obj_name can be empty and will return both value if no error
-func (lgc *Logics) checkIfNetObject(objID string, objName string, pheader http.Header) (string, string, bool, error) {
+func (lgc *Logics) checkNetObject(objID string, objName string, pheader http.Header) (string, string, bool, error) {
 	defErr := lgc.Engine.CCErr.CreateDefaultCCErrorIf(util.GetLanguage(pheader))
 
 	if "" == objName && "" == objID {
@@ -62,4 +63,78 @@ func (lgc *Logics) checkIfNetObject(objID string, objName string, pheader http.H
 	}
 
 	return objID, objName, isNetDeviceObjID, nil
+}
+
+// by checking if bk_obj_id and bk_obj_name function parameter are valid net device object or not
+// one of bk_obj_id and bk_obj_name can be empty and will return both value if no error
+func (lgc *Logics) checkNetObjectProperty(netDeviceObjID, propertyName, propertyID string, pheader http.Header) (string, string, error) {
+	defErr := lgc.Engine.CCErr.CreateDefaultCCErrorIf(util.GetLanguage(pheader))
+
+	if "" == netDeviceObjID {
+		blog.Errorf("check net device object, empty bk_obj_id")
+		return "", "", defErr.Errorf(common.CCErrCommParamsLostField, common.BKObjIDField)
+	}
+
+	if "" == propertyName && "" == propertyID {
+		blog.Errorf("check net device object, empty bk_property_id and bk_property_name")
+		return "", "", defErr.Errorf(common.CCErrCommParamsLostField, common.BKPropertyIDField)
+	}
+
+	propertyCond := map[string]interface{}{
+		common.BKOwnerIDField: util.GetOwnerID(pheader),
+		common.BKObjIDField:   netDeviceObjID}
+
+	if "" == propertyName {
+		propertyCond[common.BKPropertyNameField] = propertyName
+	}
+	if "" == propertyID {
+		propertyCond[common.BKPropertyIDField] = propertyID
+	}
+
+	attrResult, err := lgc.CoreAPI.ObjectController().Meta().SelectObjectAttWithParams(context.Background(), pheader, propertyCond)
+	if nil != err {
+		blog.Errorf("get object attribute fail, error: %v, condition [%#v]", err, propertyCond)
+		return "", "", err
+	}
+	if !attrResult.Result {
+		blog.Errorf("check net device object property, errors: %s", attrResult.ErrMsg)
+		return "", "", defErr.Errorf(attrResult.Code)
+	}
+
+	if nil == attrResult.Data || 0 == len(attrResult.Data) {
+		blog.Errorf("check net device object property, property is not exist, condition [%#v]", propertyCond)
+		return "", "", defErr.Errorf(common.CCErrCollectNetDeviceObjPropertyNotExist)
+	}
+
+	return attrResult.Data[0].PropertyID, attrResult.Data[0].PropertyName, nil
+}
+
+// by checking if bk_obj_id and bk_obj_name function parameter are valid net device object or not
+// one of bk_obj_id and bk_obj_name can be empty and will return both value if no error
+func (lgc *Logics) checkNetDeviceExist(deviceName, deviceID string, pheader http.Header) (string, string, error) {
+	defErr := lgc.Engine.CCErr.CreateDefaultCCErrorIf(util.GetLanguage(pheader))
+
+	if "" == deviceName && "" == deviceID {
+		blog.Errorf("check net device exist fail, empty device_id and device_name")
+		return "", "", defErr.Errorf(common.CCErrCommParamsLostField, common.BKDeviceIDField)
+	}
+
+	deviceCond := map[string]interface{}{common.BKOwnerIDField: util.GetOwnerID(pheader)}
+
+	if "" == deviceName {
+		deviceCond[common.BKDeviceNameField] = deviceName
+	}
+	if "" == deviceID {
+		deviceCond[common.BKDeviceIDField] = deviceID
+	}
+
+	attrResult := map[string]interface{}{}
+	err := lgc.Instance.GetOneByCondition(
+		common.BKTableNameNetcollectDevice, []string{common.BKDeviceIDField, common.BKDeviceNameField}, deviceCond, &attrResult)
+	if nil != err {
+		blog.Errorf("get object attribute fail, error: %v, condition [%#v]", err, deviceCond)
+		return "", "", err
+	}
+
+	return attrResult[common.BKDeviceIDField].(string), attrResult[common.BKDeviceNameField].(string), nil
 }
