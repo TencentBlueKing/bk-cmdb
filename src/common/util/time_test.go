fb07e5232ccdaf5a7b0a1508cee498f8c63db2dd
freakkid <904811062@qq.com>
2018-09-22 18:54:54 +0800

    move formatperiod function to common and fix pointer error #1078

--- src/common/util/time_test.go
+++ src/common/util/time_test.go
@@ -1,23 +1,24 @@
 /*
  * Tencent is pleased to support the open source community by making 蓝鲸 available.
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except 
+ * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and 
+ * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package util
 
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetCurrentTimeStr(t *testing.T) {
@@ -40,4 +41,17 @@ func TestConvParamsTime(t *testing.T) {
 	a = ConvParamsTime(a)
 	fmt.Println(a)
 
+}
+
+func TestFormatPeriod(t *testing.T) {
+	period := "000290S"
+	periodFormated, err := FormatPeriod(period)
+	if nil != err {
+		t.Error(err.Error())
+		return
+	}
+	if periodFormated != "290S" {
+		t.Errorf("error formated period %s", periodFormated)
+	}
+	fmt.Println(periodFormated)
 }
