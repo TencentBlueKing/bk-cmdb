f3437d5af95657b0147db1ebda6eb3de28b94bae
rentiansheng <rentiansheng@163.com>
2018-09-30 16:34:16 +0800

    add hsot hard action issue #1145
    Former-commit-id: 160db51de59f80fb786af963978faa515d3fde6f

--- src/api_server/service/service.go
+++ src/api_server/service/service.go
@@ -92,6 +92,7 @@ func (s *Service) V2WebService() *restful.WebService {
 	ws.Route(ws.POST("host/cloneHostProperty").To(s.cloneHostProperty))
 	ws.Route(ws.POST("host/delHostInApp").To(s.delHostInApp))
 	ws.Route(ws.POST("host/getgitServerIp").To(s.getGitServerIp))
+	ws.Route(ws.POST("host/hardinfo").To(s.GetHostHardInfo))
 
 	ws.Route(ws.POST("/CustomerGroup/getContentByCustomerGroupID").To(s.getContentByCustomerGroupID))
 	ws.Route(ws.POST("CustomerGroup/getContentByCustomerGroupId").To(s.getContentByCustomerGroupID))
