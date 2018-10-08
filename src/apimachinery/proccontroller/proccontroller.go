fa27d05d8858c704b576664d34dc531b155f38d6
rentiansheng <rentiansheng@163.com>
2018-10-08 16:19:10 +0800

    add proccontroller api

--- src/apimachinery/proccontroller/proccontroller.go
+++ src/apimachinery/proccontroller/proccontroller.go
@@ -13,26 +13,32 @@
 package proccontroller
 
 import (
+	"context"
 	"fmt"
-    "context"
-    "net/http"
-	
+	"net/http"
+
 	"configcenter/src/apimachinery/rest"
 	"configcenter/src/apimachinery/util"
-    "configcenter/src/common/metadata"
+	"configcenter/src/common/metadata"
 )
 
 type ProcCtrlClientInterface interface {
-    CreateProc2Module(ctx context.Context, h http.Header, dat interface{}) (resp *metadata.Response, err error)
-    GetProc2Module(ctx context.Context, h http.Header, dat interface{}) (resp *metadata.ProcModuleResult, err error)
-    DeleteProc2Module(ctx context.Context, h http.Header, dat interface{}) (resp *metadata.Response, err error)
-    CreateConfTemp(ctx context.Context, h http.Header, dat interface{}) (resp *metadata.Response, err error)
-    UpdateConfTemp(ctx context.Context, h http.Header, dat interface{}) (resp *metadata.Response, err error)
-    DeleteConfTemp(ctx context.Context, h http.Header, dat interface{}) (resp *metadata.Response, err error)
-    QueryConfTemp(ctx context.Context, h http.Header, dat interface{}) (resp *metadata.Response, err error)
-    CreateProcInstanceModel(ctx context.Context, h http.Header, dat interface{}) (resp *metadata.Response, err error)
-    GetProcInstanceModel(ctx context.Context, h http.Header, dat interface{}) (resp *metadata.ProcInstModelResult, err error)
-    DeleteProcInstanceModel(ctx context.Context, h http.Header, dat interface{}) (resp *metadata.Response, err error)
+	CreateProc2Module(ctx context.Context, h http.Header, dat interface{}) (resp *metadata.Response, err error)
+	GetProc2Module(ctx context.Context, h http.Header, dat interface{}) (resp *metadata.ProcModuleResult, err error)
+	DeleteProc2Module(ctx context.Context, h http.Header, dat interface{}) (resp *metadata.Response, err error)
+	CreateProc2Template(ctx context.Context, h http.Header, dat interface{}) (resp *metadata.Response, err error)
+	SearchProc2Template(ctx context.Context, h http.Header, dat interface{}) (resp *metadata.MapArrayResponse, err error)
+	DeleteProc2Template(ctx context.Context, h http.Header, dat interface{}) (resp *metadata.Response, err error)
+	CreateProcInstanceModel(ctx context.Context, h http.Header, dat []*metadata.ProcInstanceModel) (resp *metadata.Response, err error)
+	GetProcInstanceModel(ctx context.Context, h http.Header, dat *metadata.QueryInput) (resp *metadata.ProcInstModelResult, err error)
+	DeleteProcInstanceModel(ctx context.Context, h http.Header, dat map[string]interface{}) (resp *metadata.Response, err error)
+	RegisterProcInstanceDetail(ctx context.Context, h http.Header, dat *metadata.GseProcRequest) (resp *metadata.Response, err error)
+	ModifyProcInstanceDetail(ctx context.Context, h http.Header, dat *metadata.ModifyProcInstanceDetail) (resp *metadata.Response, err error)
+	GetProcInstanceDetail(ctx context.Context, h http.Header, dat *metadata.QueryInput) (resp *metadata.ProcInstanceDetailResult, err error)
+	DeleteProcInstanceDetail(ctx context.Context, h http.Header, dat map[string]interface{}) (resp *metadata.Response, err error)
+	AddOperateTaskInfo(ctx context.Context, h http.Header, dat []*metadata.ProcessOperateTask) (resp *metadata.Response, err error)
+	UpdateOperateTaskInfo(ctx context.Context, h http.Header, dat *metadata.UpdateParams) (resp *metadata.Response, err error)
+	SearchOperateTaskInfo(ctx context.Context, h http.Header, dat *metadata.QueryInput) (resp *metadata.ProcessOperateTaskResult, err error)
 }
 
 func NewProcCtrlClientInterface(c *util.Capability, version string) ProcCtrlClientInterface {
