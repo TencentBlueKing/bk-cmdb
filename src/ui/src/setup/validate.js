f559e62af750b9b4fe17c18563a72384ddb28ae6
ProtosL <644074553@qq.com>
2018-10-17 10:38:19 +0800

    minor: oid校验规则调整

--- src/ui/src/setup/validate.js
+++ src/ui/src/setup/validate.js
@@ -71,7 +71,7 @@ const customRules = {
     },
     oid: {
         validate: (value) => {
-            return /^(\d+\.)+\d+$/.test(value)
+            return /^(\d+)?(\.\d+)+$/.test(value)
         }
     }
 }
