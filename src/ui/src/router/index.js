ba6017c4f3adcfe2c7adfffda3dbd95f04e67022
ProtosL <644074553@qq.com>
2018-10-22 14:46:39 +0800

    feature: 模型重构

--- src/ui/src/router/index.js
+++ src/ui/src/router/index.js
@@ -7,6 +7,7 @@ import $http from '@/api'
 const index = () => import(/* webpackChunkName: index */ '@/views/index')
 const model = () => import(/* webpackChunkName: model */ '@/views/model')
 const modelManage = () => import(/* webpackChunkName: model */ '@/views/model-manage')
+const modelDetail = () => import(/* webpackChunkName: model */ '@/views/model-manage/children')
 const modelTopo = () => import(/* webpackChunkName: model */ '@/views/model/model-topo')
 const business = () => import(/* webpackChunkName: business */ '@/views/business')
 const businessArchived = () => import(/* webpackChunkName: businessArchived */ '@/views/business/archived')
@@ -41,6 +42,12 @@ const router = new Router({
     }, {
         path: '/model-manage',
         component: modelManage
+    }, {
+        path: '/model-manage/:modelId',
+        component: modelDetail,
+        meta: {
+            ignoreAuthorize: true
+        }
     }, {
         path: '/model',
         component: model,
