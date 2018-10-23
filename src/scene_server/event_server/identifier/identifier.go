a8f30ff1a798863bad59f1a7e3ffd71cc65ff9da
wusendong <wusendong@gmail.com>
2018-10-23 11:57:50 +0800

    解决推送主机身份缺少进程信息问题
    Former-commit-id: 066122ddd1974d3a053cc16c0216ad81cc15ffeb

--- src/scene_server/event_server/identifier/identifier.go
+++ src/scene_server/event_server/identifier/identifier.go
@@ -218,6 +218,7 @@ func (ih *IdentifierHandler) handleRelatedInst(hostIdentify metadata.EventInst, 
 				hostIDKeys = append(hostIDKeys, getInstCacheKey(common.BKInnerObjIDHost, hostID))
 			}
 			idens := ih.cache.MGet(hostIDKeys[index:leftIndex]...).Val()
+			// TODO fetch from db if not cached
 			for identIndex := range idens {
 				iden := HostIdentifier{}
 				if err = json.Unmarshal([]byte(getString(idens[identIndex])), &iden); err != nil {
@@ -541,23 +542,74 @@ func (ih *IdentifierHandler) fetchHostCache() {
 
 	relations := []metadata.ModuleHost{}
 	hosts := []*HostIdentifier{}
+	modules := []metadata.ModuleInst{}
+	proc2modules := []metadata.ProcessModule{}
 
-	ih.db.Table(common.BKTableNameModuleHostConfig).Find(map[string]interface{}{}).All(ih.ctx, &relations)
-	ih.db.Table(common.BKTableNameBaseHost).Find(map[string]interface{}{}).All(ih.ctx, &hosts)
+	err := ih.db.Table(common.BKTableNameModuleHostConfig).Find(map[string]interface{}{}).All(ih.ctx, &relations)
+	if err != nil {
+		blog.Errorf("[identifier][fetchHostCache] get cc_ModuleHostConfig error: %v", err)
+		return
+	}
+	err = ih.db.Table(common.BKTableNameBaseHost).Find(map[string]interface{}{}).All(ih.ctx, &hosts)
+	if err != nil {
+		blog.Errorf("[identifier][fetchHostCache] get cc_HostBase error: %v", err)
+		return
+	}
+	err = ih.db.Table(common.BKTableNameProcModule).Find(map[string]interface{}{}).All(ih.ctx, &proc2modules)
+	if err != nil {
+		blog.Errorf("[identifier][fetchHostCache] get cc_Proc2Module error: %v", err)
+		return
+	}
+	err = ih.db.Table(common.BKTableNameBaseModule).Find(map[string]interface{}{}).All(ih.ctx, &modules)
+	if err != nil {
+		blog.Errorf("[identifier][fetchHostCache] get cc_ModuleBase error: %v", err)
+		return
+	}
 
 	relationMap := map[int64][]metadata.ModuleHost{}
 	for _, relate := range relations {
 		relationMap[relate.HostID] = append(relationMap[relate.HostID], relate)
 	}
 
+	proc2modulesMap := map[string][]int64{}
+	bindModulesMap := map[int64][]int64{}
+	for _, proc2module := range proc2modules {
+		proc2modulesMap[proc2module.ModuleName] = append(proc2modulesMap[proc2module.ModuleName], proc2module.ProcessID)
+		for _, module := range modules {
+			if module.BizID == proc2module.AppID && module.ModuleName == proc2module.ModuleName {
+				bindModulesMap[proc2module.ProcessID] = append(bindModulesMap[proc2module.ProcessID], module.ModuleID)
+			}
+		}
+	}
+
+	modulesMap := map[int64]metadata.ModuleInst{}
+	for _, module := range modules {
+		modulesMap[module.ModuleID] = module
+	}
+
 	for _, ident := range hosts {
 		ident.Module = map[string]*Module{}
+		hostprocs := map[int64]bool{}
 		for _, rela := range relationMap[ident.HostID] {
 			ident.Module[strconv.FormatInt(rela.ModuleID, 10)] = &Module{
 				SetID:    rela.SetID,
 				ModuleID: rela.ModuleID,
 				BizID:    rela.AppID,
 			}
+			if module, ok := modulesMap[rela.ModuleID]; ok {
+				for _, procid := range proc2modulesMap[module.ModuleName] {
+					hostprocs[procid] = true
+				}
+			}
+		}
+
+		ident.Process = []Process{}
+		for procid := range hostprocs {
+			bindModules := bindModulesMap[procid]
+			if len(bindModules) == 0 {
+				bindModules = []int64{}
+			}
+			ident.Process = append(ident.Process, Process{ProcessID: procid, BindModules: bindModules})
 		}
 
 		if err := ih.cache.Set(getInstCacheKey(common.BKInnerObjIDHost, ident.HostID), ident, 0).Err(); err != nil {
