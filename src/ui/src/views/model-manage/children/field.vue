947bbf56e71a644464c51ba0bd12c5c4ff339a3c
ProtosL <644074553@qq.com>
2018-10-23 21:55:19 +0800

    feature: 模型重构
    Former-commit-id: 5e7454d539dcb9402cb663abe7d5787b4d14baf8

--- src/ui/src/views/model-manage/children/field.vue
+++ src/ui/src/views/model-manage/children/field.vue
@@ -10,77 +10,24 @@
             :list="table.list"
             :pagination.sync="table.pagination"
             :wrapperMinusHeight="220"
-            @handlePageChange="handlePageChange"
-            @handleSizeChange="handleSizeChange"
             @handleSortChange="handleSortChange">
         </cmdb-table>
         <cmdb-slider
             :width="514"
-            :isShow="slider.isShow"
+            :isShow.sync="slider.isShow"
         >
-            <div slot="content" class="slider-content">
-                <label class="form-label">
-                    <span class="label-text">
-                        {{$t('ModelManagement["唯一标识"]')}}
-                        <span class="color-danger">*</span>
-                    </span>
-                    <input type="text" class="cmdb-form-input">
-                    <i class="bk-icon icon-info-circle"></i>
-                </label>
-                <label class="form-label">
-                    <span class="label-text">
-                        {{$t('ModelManagement["名称"]')}}
-                        <span class="color-danger">*</span>
-                    </span>
-                    <input type="text" class="cmdb-form-input">
-                    <i class="bk-icon icon-info-circle"></i>
-                </label>
-                <div class="form-label">
-                    <span class="label-text">
-                        {{$t('ModelManagement["字段类型"]')}}
-                        <span class="color-danger">*</span>
-                    </span>
-                    <bk-selector
-                        :list="fieldTypeList"
-                        :selected.sync="fieldInfo['bk_property_type']"
-                    ></bk-selector>
-                    <i class="bk-icon icon-info-circle"></i>
-                </div>
-                <div class="field-detail">
-                    <div class="form-label">
-                        <span class="label-text">{{$t('ModelManagement["字段设置"]')}}</span>
-                        <label class="cmdb-form-checkbox cmdb-checkbox-small">
-                            <input type="checkbox">
-                            <span class="cmdb-form-text">{{$t('ModelManagement["可编辑"]')}}</span>
-                        </label>
-                        <label class="cmdb-form-checkbox cmdb-checkbox-small">
-                            <input type="checkbox">
-                            <span class="cmdb-form-text">{{$t('ModelManagement["必填"]')}}</span>
-                        </label>
-                    </div>
-                    <div class="form-label">
-                        <span class="label-text">{{$t('ModelManagement["正则校验"]')}}</span>
-                        <textarea name="" id="" cols="30" rows="10"></textarea>
-                    </div>
-                </div>
-                <label class="form-label">
-                    <span class="label-text">
-                        {{$t('ModelManagement["单位"]')}}
-                    </span>
-                    <input type="text" class="cmdb-form-input">
-                    <i class="bk-icon icon-info-circle"></i>
-                </label>
-                <div class="form-label">
-                    <span class="label-text">{{$t('ModelManagement["用户提示"]')}}</span>
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
-                </div>
-            </div>
+            <the-field-detail class="slider-content" slot="content"></the-field-detail>
         </cmdb-slider>
     </div>
 </template>
 
 <script>
+    import theFieldDetail from './field-detail'
+    import { mapActions } from 'vuex'
     export default {
+        components: {
+            theFieldDetail
+        },
         data () {
             return {
                 slider: {
@@ -117,50 +64,13 @@
                     },
                     defaultSort: '-op_time',
                     sort: '-op_time'
-                },
-                fieldTypeList: [{
-                    id: 'singlechar',
-                    name: this.$t('ModelManagement["短字符"]')
-                }, {
-                    id: 'int',
-                    name: this.$t('ModelManagement["数字"]')
-                }, {
-                    id: 'enum',
-                    name: this.$t('ModelManagement["枚举"]')
-                }, {
-                    id: 'date',
-                    name: this.$t('ModelManagement["日期"]')
-                }, {
-                    id: 'time',
-                    name: this.$t('ModelManagement["时间"]')
-                }, {
-                    id: 'longchar',
-                    name: this.$t('ModelManagement["长字符"]')
-                }, {
-                    id: 'objuser',
-                    name: this.$t('ModelManagement["用户"]')
-                }, {
-                    id: 'timezone',
-                    name: this.$t('ModelManagement["时区"]')
-                }, {
-                    id: 'bool',
-                    name: 'bool'
-                }],
-                fieldInfo: {
-                    bk_property_name: '',
-                    bk_property_id: '',
-                    unit: '',
-                    placeholder: '',
-                    bk_property_type: 'singlechar',
-                    editable: true,
-                    isrequired: false,
-                    isonly: false,
-                    option: '',
-                    bk_asst_obj_id: ''
                 }
             }
         },
         methods: {
+            ...mapActions('objectModelProperty', [
+                'searchObjectAttribute'
+            ]),
             showSlider () {
                 this.slider.isShow = true
             },
@@ -186,43 +96,5 @@
     }
     .slider-content {
         padding: 20px;
-        .form-label {
-            .cmdb-form-input {
-                width: calc(100% - 145px);
-            }
-            textarea {
-                padding: 10px;
-                width: 329px;
-                height: 84px;
-                font-size: 14px;
-                border: 1px solid $cmdbBorderColor;
-                border-radius: 2px;
-                outline: none;
-                resize: none;
-            }
-        }
-        .label-text {
-            width: 110px;
-        }
-        .icon-info-circle {
-            font-size: 18px;
-            color: $cmdbBorderColor;
-            padding-left: 5px;
-        }
-        .field-detail {
-            margin-bottom: 20px;
-            padding: 20px 0;
-            background: #f3f8ff;
-            .form-label:last-child {
-                margin: 0;
-            }
-            .label-text {
-                vertical-align: top;
-            }
-            .cmdb-form-checkbox {
-                width: 90px;
-                vertical-align: middle;
-            }
-        }
     }
 </style>
