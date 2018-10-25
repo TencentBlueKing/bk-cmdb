25d70951a6298ef81d9ae7d1dae6ce822264e682
ProtosL <644074553@qq.com>
2018-10-25 20:17:04 +0800

    feature: 关联接口录入
    Former-commit-id: 13dbdce839506ebebc2c0400a5dc27245d9ec31f

---
+++ src/ui/src/store/modules/api/object-association.js
@@ -0,0 +1,159 @@
+/*
+ * Tencent is pleased to support the open source community by making 蓝鲸 available.
+ * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+import $http from '@/api'
+
+const state = {
+
+}
+
+const getters = {
+
+}
+
+const actions = {
+    /**
+     * 查询关联类型
+     * @param {Function} commit store commit mutation hander
+     * @param {Object} state store state
+     * @param {String} dispatch store dispatch action hander
+     * @param {Object} params 参数
+     * @return {promises} promises 对象
+     */
+    searchAssociationType ({ commit, state, dispatch }, params) {
+        return $http.post(`topo/association/type/action/search`, params)
+    },
+    /**
+     * 添加关联类型
+     * @param {Function} commit store commit mutation hander
+     * @param {Object} state store state
+     * @param {String} dispatch store dispatch action hander
+     * @param {Object} params 参数
+     * @return {promises} promises 对象
+     */
+    createAssociationType ({ commit, state, dispatch }, params) {
+        return $http.post(`topo/association/type/action/create`, params)
+    },
+    /**
+     * 编辑关联类型
+     * @param {Function} commit store commit mutation hander
+     * @param {Object} state store state
+     * @param {String} dispatch store dispatch action hander
+     * @param {Number} id 自增id
+     * @param {Object} params 参数
+     * @return {promises} promises 对象
+     */
+    updateAssociationType ({ commit, state, dispatch }, id, params) {
+        return $http.post(`topo/association/type/${id}/action/update`, params)
+    },
+    /**
+     * 删除关联类型
+     * @param {Function} commit store commit mutation hander
+     * @param {Object} state store state
+     * @param {String} dispatch store dispatch action hander
+     * @param {Number} id 自增id
+     * @param {Object} params 参数
+     * @return {promises} promises 对象
+     */
+    deleteAssociationType ({ commit, state, dispatch }, id, params) {
+        return $http.delete(`topo/association/type/${id}/action/delete`, params)
+    },
+    /**
+     * 查询模型关联
+     * @param {Function} commit store commit mutation hander
+     * @param {Object} state store state
+     * @param {String} dispatch store dispatch action hander
+     * @param {Object} params 参数
+     * @return {promises} promises 对象
+     */
+    searchObjectAssociation ({ commit, state, dispatch }, params) {
+        return $http.post(`object/association/action/search`, params)
+    },
+    /**
+     * 添加模型关联
+     * @param {Function} commit store commit mutation hander
+     * @param {Object} state store state
+     * @param {String} dispatch store dispatch action hander
+     * @param {Object} params 参数
+     * @return {promises} promises 对象
+     */
+    createObjectAssociation ({ commit, state, dispatch }, params) {
+        return $http.post(`object/association/action/create`, params)
+    },
+    /**
+     * 编辑模型关联
+     * @param {Function} commit store commit mutation hander
+     * @param {Object} state store state
+     * @param {String} dispatch store dispatch action hander
+     * @param {Number} id 自增id
+     * @param {Object} params 参数
+     * @return {promises} promises 对象
+     */
+    updateObjectAssociation ({ commit, state, dispatch }, id, params) {
+        return $http.post(`object/association/${id}/action/update`, params)
+    },
+    /**
+     * 删除关联类型
+     * @param {Function} commit store commit mutation hander
+     * @param {Object} state store state
+     * @param {String} dispatch store dispatch action hander
+     * @param {Number} id 自增id
+     * @param {Object} params 参数
+     * @return {promises} promises 对象
+     */
+    deleteObjectAssociation ({ commit, state, dispatch }, id, params) {
+        return $http.delete(`object/association/${id}/action/delete`, params)
+    },
+    /**
+     * 查询实例关联
+     * @param {Function} commit store commit mutation hander
+     * @param {Object} state store state
+     * @param {String} dispatch store dispatch action hander
+     * @param {Object} params 参数
+     * @return {promises} promises 对象
+     */
+    searchInstAssociation ({ commit, state, dispatch }, params) {
+        return $http.post(`inst/association/action/search`, params)
+    },
+    /**
+     * 添加实例关联
+     * @param {Function} commit store commit mutation hander
+     * @param {Object} state store state
+     * @param {String} dispatch store dispatch action hander
+     * @param {Object} params 参数
+     * @return {promises} promises 对象
+     */
+    createInstAssociation ({ commit, state, dispatch }, params) {
+        return $http.post(`inst/association/action/create`, params)
+    },
+    /**
+     * 删除实例关联
+     * @param {Function} commit store commit mutation hander
+     * @param {Object} state store state
+     * @param {String} dispatch store dispatch action hander
+     * @param {Object} params 参数
+     * @return {promises} promises 对象
+     */
+    deleteInstAssociation ({ commit, state, dispatch }, params) {
+        return $http.post(`inst/association/action/delete`, params)
+    }
+}
+
+const mutations = {
+
+}
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+}
