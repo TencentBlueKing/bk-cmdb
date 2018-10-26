31fc23ec28bffcfc8f1b3cf8a32ca2c92b1e659f
ProtosL <644074553@qq.com>
2018-10-26 11:44:37 +0800

    feature: 关联类型
    Former-commit-id: b826a56e39224b8e855fa2652b36eb7059a37f07

--- src/ui/src/views/model-manage/relation.vue
+++ src/ui/src/views/model-manage/relation.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="relation-wrapper">
         <p class="operation-box">
-            <bk-button type="primary" @click="slider.isShow = true">
+            <bk-button type="primary" @click="createRelation">
                 {{$t('ModelManagement["新增关联类型"]')}}
             </bk-button>
             <label class="search-input">
@@ -10,24 +10,40 @@
             </label>
         </p>
         <cmdb-table
-            :loading="$loading('getOperationLog')"
+            :loading="$loading('searchAssociationType')"
             :header="table.header"
             :list="table.list"
-            :pagination.sync="table.pagination">
+            :pagination.sync="table.pagination"
+            @handlePageChange="handlePageChange"
+            @handleSizeChange="handleSizeChange"
+            @handleSortChange="handleSortChange">
+            <template slot="operation" slot-scope="{ item }">
+                <span class="text-primary mr10" @click.stop="editRelation(item)">
+                    {{$t('Common["编辑"]')}}
+                </span>
+                <span class="text-primary" v-if="!item.ispre && !isReadOnly" @click.stop="deleteRelation(item)">
+                    {{$t('Common["删除"]')}}
+                </span>
+            </template>
         </cmdb-table>
         <cmdb-slider
             class="relation-slider"
             :width="514"
             :title="slider.title"
             :isShow.sync="slider.isShow">
-            <the-relation slot="content" class="slider-content"></the-relation>
+            <the-relation
+                slot="content"
+                class="slider-content"
+                :isEdit="slider.isEdit"
+                :relation="slider.relation"
+            ></the-relation>
         </cmdb-slider>
     </div>
 </template>
 
 <script>
     import theRelation from './relation-type'
-    import { mapActions } from 'vuex'
+    import { mapGetters, mapActions } from 'vuex'
     export default {
         components: {
             theRelation
@@ -36,7 +52,9 @@
             return {
                 slider: {
                     isShow: false,
-                    title: this.$t('ModelManagement["新增关联类型"]')
+                    isEdit: false,
+                    title: this.$t('ModelManagement["新增关联类型"]'),
+                    relation: {}
                 },
                 searchText: '',
                 table: {
@@ -57,7 +75,8 @@
                         name: this.$t('ModelManagement["使用数"]')
                     }, {
                         id: 'operation',
-                        name: this.$t('Common["操作"]')
+                        name: this.$t('Common["操作"]'),
+                        sortable: false
                     }],
                     list: [],
                     pagination: {
@@ -65,21 +84,94 @@
                         current: 1,
                         size: 10
                     },
-                    defaultSort: '-op_time',
-                    sort: '-op_time'
+                    defaultSort: '-bk_asst_id',
+                    sort: '-bk_asst_id'
                 }
             }
         },
         created () {
-            this.searchAssociationType()
+            this.searchRelation()
+        },
+        computed: {
+            ...mapGetters('objectModel', [
+                'activeModel'
+            ]),
+            isReadOnly () {
+                if (this.activeModel) {
+                    return this.activeModel['bk_ispaused']
+                }
+                return false
+            },
+            searchParams () {
+                let params = {
+                    page: {
+                        start: (this.table.pagination.current - 1) * this.table.pagination.size,
+                        limit: this.table.pagination.size,
+                        sort: this.table.sort
+                    }
+                }
+                if (this.searchText.length) {
+                    Object.assign(params, {
+                        condition: {
+                            bk_asst_name: {
+                                '$like': this.searchText
+                            }
+                        }
+                    })
+                }
+                return params
+            }
         },
         methods: {
             ...mapActions('objectAssociation', [
                 'searchAssociationType',
-                'createAssociationType',
-                'updateAssociationType',
                 'deleteAssociationType'
-            ])
+            ]),
+            searchRelation () {
+                this.searchAssociationType({
+                    params: this.searchParams,
+                    config: {
+                        requestId: 'searchAssociationType'
+                    }
+                })
+            },
+            createRelation () {
+                this.slider.title = this.$t('ModelManagement["新增关联类型"]')
+                this.slider.isEdit = false
+                this.slider.isShow = true
+            },
+            editRelation (relation) {
+                this.slider.title = this.$t('ModelManagement["编辑关联类型"]')
+                this.slider.relation = relation
+                this.slider.isEdit = true
+                this.slider.isShow = true
+            },
+            deleteRelation (relation) {
+                this.$bkInfo({
+                    title: this.$tc('ModelManagement["确定删除关联类型？"]', relation['bk_asst_name'], {name: relation['bk_asst_name']}),
+                    confirmFn: async () => {
+                        await this.deleteAssociationType({
+                            id: relation.id,
+                            config: {
+                                requestId: 'deleteAssociationType'
+                            }
+                        })
+                        this.searchRelation()
+                    }
+                })
+            },
+            handlePageChange (current) {
+                this.pagination.current = current
+                this.searchRelation()
+            },
+            handleSizeChange (size) {
+                this.pagination.size = size
+                this.handlePageChange(1)
+            },
+            handleSortChange (sort) {
+                this.sort = sort
+                this.searchRelation()
+            }
         }
     }
 </script>
