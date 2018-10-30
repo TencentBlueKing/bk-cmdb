95d2e17e851060c85cb9b72d0aed8c5a7961cbe3
owenlxu <lowenitt@sina.com>
2018-10-30 11:30:22 +0800

    fix  Compatible with supplier account #1288

--- src/common/definitions.go
+++ src/common/definitions.go
@@ -699,6 +699,8 @@ const (
 	BKHTTPHeaderUser = "BK_User"
 	// BKHTTPLanguage the language key word
 	BKHTTPLanguage = "HTTP_BLUEKING_LANGUAGE"
+	// BKHTTPOwnerID the owner
+	BKHTTPOwner = "HTTP_BLUEKING_SUPPLIER_ACCOUNT"
 	// BKHTTPOwnerID the owner id
 	BKHTTPOwnerID = "HTTP_BLUEKING_SUPPLIER_ID"
 	//BKHTTPOwnerID = "HTTP_BLUEKING_OWNERID"
