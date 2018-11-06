7e3f52b0ce8a4173a25cf8f7e975b3d1480dab6f
wusendong <wusendong@gmail.com>
2018-11-06 15:52:00 +0800

    fix: 解决拓扑查询慢问题
    Former-commit-id: 699e3b78a13bcbadb9577e0ecf3b946406252b99

--- src/common/metadata/association.go
+++ src/common/metadata/association.go
@@ -134,7 +134,7 @@ type TopoInst struct {
 // TopoInstRst 拓扑实例
 type TopoInstRst struct {
 	TopoInst `json:",inline"`
-	Child    []TopoInstRst `json:"child"`
+	Child    []*TopoInstRst `json:"child"`
 }
 
 // ConditionItem subcondition
