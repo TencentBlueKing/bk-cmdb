0c20bdb243e38ba07b6d766e0f4836bd8371482b
wusendong <wusendong@gmail.com>
2018-11-06 17:27:51 +0800

    fix: 解决导入拓扑时没删掉不含default字段的模块问题

--- src/scene_server/admin_server/command/getter.go
+++ src/scene_server/admin_server/command/getter.go
@@ -116,7 +116,11 @@ func getTree(ctx context.Context, db dal.RDB, root *Node, pcmap map[string]*meta
 
 	switch asst.ObjectID {
 	case common.BKInnerObjIDApp, common.BKInnerObjIDSet, common.BKInnerObjIDModule:
-		condition["default"] = map[string]interface{}{common.BKDBEQ: 0}
+		condition[common.BKDefaultField] = map[string]interface{}{
+			common.BKDBNot: map[string]interface{}{
+				common.BKDBGT: 0,
+			},
+		}
 	default:
 		condition[common.BKObjIDField] = asst.ObjectID
 	}
