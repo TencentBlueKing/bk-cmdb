28b7b369fecd99e99f61e48815e19708fa86e45b
wusendong <wusendong@gmail.com>
2018-11-12 21:07:29 +0800

    feature: add SearchObjectUnique #1358
    Former-commit-id: 5a9c0ce194fea22d8a3270b14c532d64fae75861

--- src/common/metadata/unique.go
+++ src/common/metadata/unique.go
@@ -43,6 +43,7 @@ type UpdateUniqueRequest struct {
 	ObjID     string       `json:"bk_obj_id" bson:"bk_obj_id"`
 	MustCheck bool         `json:"must_check" bson:"must_check"`
 	Keys      []UinqueKeys `json:"keys" bson:"keys"`
+	LastTime  Time         `json:"last_time" bson:"last_time"`
 }
 
 type UpdateUniqueResult struct {
