9eab7234cc126608a24287234f0d35a53c38dd85
ProtosL <644074553@qq.com>
2018-11-13 22:26:27 +0800

    feature: 模型拓扑
    Former-commit-id: c7e1e244b665634f31603bfc2d550919d362a168

---
+++ src/ui/src/views/model-manage/topo-detail/relation.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <label class="form-label">
+            <span class="label-text">
+                {{$t('ModelManagement["唯一标识"]')}}
+                <span class="color-danger">*</span>
+            </span>
+            <div class="cmdb-form-item" :class="{'is-error': errors.has('asstId')}">
+                <input type="text" class="cmdb-form-input"
+                name="asstId"
+                :disabled="isEdit"
+                v-model.trim="relationInfo['bk_asst_id']"
+                v-validate="'required|singlechar'"
+                :placeholder="$t('ModelManagement[\'请输入英文标识\']')">
+                <p class="form-error">{{errors.first('asstId')}}</p>
+            </div>
+        </label>
+    </div>
+</template>
