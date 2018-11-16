aa86a6bc47f4a8ff9261376636830cdcb8e5e36f
wusendong <wusendong@gmail.com>
2018-11-16 17:20:22 +0800

    feature: update validator #1358

--- src/scene_server/validator/valid_unique.go
+++ src/scene_server/validator/valid_unique.go
@@ -15,56 +15,86 @@ package validator
 import (
 	"configcenter/src/common"
 	"configcenter/src/common/blog"
+	"configcenter/src/common/condition"
 	"configcenter/src/common/metadata"
 )
 
 // validCreateUnique  valid create unique
 func (valid *ValidMap) validCreateUnique(valData map[string]interface{}) error {
-	if 0 >= len(valid.isOnly) {
+	uniqueresp, err := valid.CoreAPI.ObjectController().Unique().Search(valid.ctx, valid.pheader, valid.objID)
+	if nil != err {
+		return err
+	}
+	if !uniqueresp.Result {
+		return valid.errif.New(uniqueresp.Code, uniqueresp.ErrMsg)
+	}
+
+	if 0 >= len(uniqueresp.Data) {
 		blog.V(3).Infof("is only array is zero for %s", valid.objID)
 		return nil
 	}
 
-	searchCond := make(map[string]interface{})
-	// only search data not in diable status
-	searchCond[common.BKDataStatusField] = map[string]interface{}{common.BKDBNE: common.DataStatusDisabled}
-
-	// retrive isonly value
-	for key, val := range valData {
-		if valid.isOnly[key] {
-			searchCond[key] = val
+	for _, unique := range uniqueresp.Data {
+		// retrive unique value
+		uniquekeys := map[string]bool{}
+		for _, key := range unique.Keys {
+			switch key.Kind {
+			case metadata.UinqueKeyKindProperty:
+				property, ok := valid.idToProperty[int64(key.ID)]
+				if !ok {
+					return valid.errif.Errorf(common.CCErrTopoObjectPropertyNotFound, property.ID)
+				}
+				uniquekeys[property.PropertyID] = true
+			default:
+				return valid.errif.Errorf(common.CCErrTopoObjectUniqueKeyKindInvalid, key.Kind)
+			}
 		}
-	}
 
-	if common.GetObjByType(valid.objID) == common.BKInnerObjIDObject {
-		searchCond[common.BKObjIDField] = valid.objID
-	}
+		cond := condition.CreateCondition()
+		allEmpty := true
+		for key, val := range valData {
+			if uniquekeys[key] {
+				cond.Field(key).Eq(val)
+				if isEmpty(val) {
+					allEmpty = false
+				}
+			}
+		}
 
-	if 0 == len(searchCond) {
-		return nil
-	}
+		if allEmpty && !unique.MustCheck {
+			return nil
+		}
 
-	result, err := valid.CoreAPI.ObjectController().Instance().SearchObjects(valid.ctx, common.GetObjByType(valid.objID), valid.pheader, &metadata.QueryInput{Condition: searchCond})
-	if nil != err {
-		return err
-	}
-	if !result.Result {
-		return valid.errif.Error(result.Code)
-	}
+		// only search data not in diable status
+		cond.Field(common.BKDataStatusField).NotEq(common.DataStatusDisabled)
+		if common.GetObjByType(valid.objID) == common.BKInnerObjIDObject {
+			cond.Field(common.BKObjIDField).Eq(valid.objID)
+		}
 
-	if 0 < result.Data.Count {
-		blog.Errorf("[validUpdateUnique] duplicate data condition: %#v, isonly: %#v, objID %s", searchCond, valid.isOnly, valid.objID)
-		return valid.errif.Error(common.CCErrCommDuplicateItem)
+		result, err := valid.CoreAPI.ObjectController().Instance().SearchObjects(valid.ctx, common.GetObjByType(valid.objID), valid.pheader, &metadata.QueryInput{Condition: cond.ToMapStr()})
+		if nil != err {
+			return err
+		}
+		if !result.Result {
+			return valid.errif.New(result.Code, result.ErrMsg)
+		}
+
+		if 0 < result.Data.Count {
+			blog.Errorf("[validUpdateUnique] duplicate data condition: %#v, unique keys: %#v, objID %s", cond.ToMapStr(), uniquekeys, valid.objID)
+			return valid.errif.Error(common.CCErrCommDuplicateItem)
+		}
+
 	}
 
 	return nil
 }
 
+func isEmpty(value interface{}) bool {
+	return value == nil || value == ""
+}
+
 // validUpdateUnique valid update unique
 func (valid *ValidMap) validUpdateUnique(valData map[string]interface{}, instID int64) error {
-	if 0 >= len(valid.isOnly) {
-		return nil
-	}
 
 	objID := valid.objID
 	searchCond := make(map[string]interface{})
@@ -74,37 +104,73 @@ func (valid *ValidMap) validUpdateUnique(valData map[string]interface{}, instID 
 	}
 
 	// retrive isonly value
-	for key, val := range mapData {
-		if valid.isOnly[key] {
-			searchCond[key] = val
-		}
-	}
 	for key, val := range valData {
-		if valid.isOnly[key] {
-			searchCond[key] = val
-		}
+		mapData[key] = val
 	}
 
-	objIDName := common.GetInstIDField(objID)
-	searchCond[objIDName] = map[string]interface{}{common.BKDBNE: instID}
-	// only search data not in diable status
-	searchCond[common.BKDataStatusField] = map[string]interface{}{common.BKDBNE: common.DataStatusDisabled}
-	if common.GetInstTableName(objID) == common.BKTableNameBaseInst {
-		objID = common.BKInnerObjIDObject
-		searchCond[common.BKObjIDField] = valid.objID
-	}
-
-	result, err := valid.CoreAPI.ObjectController().Instance().SearchObjects(valid.ctx, objID, valid.pheader, &metadata.QueryInput{Condition: searchCond})
+	uniqueresp, err := valid.CoreAPI.ObjectController().Unique().Search(valid.ctx, valid.pheader, valid.objID)
 	if nil != err {
 		return err
 	}
-	if !result.Result {
-		return valid.errif.Error(result.Code)
+	if !uniqueresp.Result {
+		return valid.errif.New(uniqueresp.Code, uniqueresp.ErrMsg)
 	}
 
-	if 0 < result.Data.Count {
-		blog.Errorf("[validUpdateUnique] duplicate data condition: %#v, origin: %#v, isonly: %#v, objID: %s, instID %v count %d", searchCond, mapData, valid.isOnly, valid.objID, instID, result.Data.Count)
-		return valid.errif.Error(common.CCErrCommDuplicateItem)
+	if 0 >= len(uniqueresp.Data) {
+		blog.V(3).Infof("is only array is zero for %s", valid.objID)
+		return nil
+	}
+
+	for _, unique := range uniqueresp.Data {
+		// retrive unique value
+		uniquekeys := map[string]bool{}
+		for _, key := range unique.Keys {
+			switch key.Kind {
+			case metadata.UinqueKeyKindProperty:
+				property, ok := valid.idToProperty[int64(key.ID)]
+				if !ok {
+					return valid.errif.Errorf(common.CCErrTopoObjectPropertyNotFound, property.ID)
+				}
+				uniquekeys[property.PropertyID] = true
+			default:
+				return valid.errif.Errorf(common.CCErrTopoObjectUniqueKeyKindInvalid, key.Kind)
+			}
+		}
+
+		cond := condition.CreateCondition()
+		allEmpty := true
+		for key, val := range mapData {
+			if uniquekeys[key] {
+				cond.Field(key).Eq(val)
+				if isEmpty(val) {
+					allEmpty = false
+				}
+			}
+		}
+
+		if allEmpty && !unique.MustCheck {
+			return nil
+		}
+
+		// only search data not in diable status
+		cond.Field(common.BKDataStatusField).NotEq(common.DataStatusDisabled)
+		if common.GetObjByType(valid.objID) == common.BKInnerObjIDObject {
+			cond.Field(common.BKObjIDField).Eq(valid.objID)
+		}
+		cond.Field(common.GetInstIDField(objID)).NotEq(instID)
+
+		result, err := valid.CoreAPI.ObjectController().Instance().SearchObjects(valid.ctx, common.GetObjByType(valid.objID), valid.pheader, &metadata.QueryInput{Condition: searchCond})
+		if nil != err {
+			return err
+		}
+		if !result.Result {
+			return valid.errif.Error(result.Code)
+		}
+
+		if 0 < result.Data.Count {
+			blog.Errorf("[validUpdateUnique] duplicate data condition: %#v, origin: %#v, unique keys: %v, objID: %s, instID %v count %d", searchCond, mapData, uniquekeys, valid.objID, instID, result.Data.Count)
+			return valid.errif.Error(common.CCErrCommDuplicateItem)
+		}
 	}
 	return nil
 }
