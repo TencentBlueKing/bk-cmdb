ea13758dd45cead9e8c42979c3a15702a3c8f052
ProtosL <644074553@qq.com>
2018-11-20 15:06:30 +0800

    minor: 样式调整

--- src/ui/src/views/model-manage/children/field-detail/config.vue
+++ src/ui/src/views/model-manage/children/field-detail/config.vue
@@ -3,11 +3,11 @@
         <span class="label-text">{{$t('ModelManagement["字段设置"]')}}</span>
         <label class="cmdb-form-checkbox cmdb-checkbox-small" v-if="isEditableShow">
             <input type="checkbox" v-model="localValue.editable" :disabled="isReadOnly">
-            <span class="cmdb-form-text">{{$t('ModelManagement["可编辑"]')}}</span>
+            <span class="cmdb-checkbox-text">{{$t('ModelManagement["可编辑"]')}}</span>
         </label>
         <label class="cmdb-form-checkbox cmdb-checkbox-small" v-if="isRequiredShow">
             <input type="checkbox" v-model="localValue.isrequired" :disabled="isReadOnly">
-            <span class="cmdb-form-text">{{$t('ModelManagement["必填"]')}}</span>
+            <span class="cmdb-checkbox-text">{{$t('ModelManagement["必填"]')}}</span>
         </label>
     </div>
 </template>
