6100aa92303e7237a8bd6338ece55ba7caeef1ef
rentiansheng <rentiansheng@163.com>
2018-11-25 14:12:34 +0800

    fix: v2 api instance ID is large integer, return id val is scientific notation. issue #1454

--- src/api_server/logics/v2/common/converter/converter.go
+++ src/api_server/logics/v2/common/converter/converter.go
@@ -692,7 +692,7 @@ func GeneralV2Data(data interface{}) interface{} {
 		return ""
 	}
 
-	return fmt.Sprintf("%v", data)
+	return convToV2ValStr(data) //fmt.Sprintf("%v", data)
 
 }
 
@@ -1033,7 +1033,7 @@ func convertFieldsNilToString(itemMap map[string]interface{}, fields []string) m
 		if !ok || nil == val {
 			itemMap[field] = ""
 		} else {
-			itemMap[field] = fmt.Sprintf("%v", val)
+			itemMap[field] = convToV2ValStr(val) //fmt.Sprintf("%v", val)
 		}
 	}
 
@@ -1169,11 +1169,11 @@ func convertToString(itemMap map[string]interface{}) map[string]interface{} {
 	tempMap := make(map[string]interface{})
 	blog.Debug(" itemMap: %v", itemMap)
 	for key, val := range itemMap {
-		filedInt, err := util.GetIntByInterface(val)
+		filedInt, err := util.GetInt64ByInterface(val)
 		if nil != err {
 			blog.Errorf("convert field %s to number fail!value:%v", key, val)
 		}
-		tempMap[key] = strconv.Itoa(filedInt)
+		tempMap[key] = strconv.FormatInt(filedInt, 10)
 	}
 
 	return tempMap
@@ -1198,12 +1198,12 @@ func convertFieldsIntToStr(itemMap map[string]interface{}, fields []string) (map
 		case string:
 		case nil:
 		default:
-			filedInt, err := util.GetIntByInterface(item)
+			filedInt, err := util.GetInt64ByInterface(item)
 			if nil != err {
 				blog.Debug("convert field %s to number fail!", field)
 				return nil, err
 			}
-			tempMap[field] = strconv.Itoa(filedInt)
+			tempMap[field] = strconv.FormatInt(filedInt, 10)
 		}
 
 	}
@@ -1259,10 +1259,50 @@ func getV2KeyVal(key string, val interface{}) (string, string) {
 			} else {
 				v2Val = ""
 			}
+		} else {
+			return v2Key, convToV2ValStr(val)
 		}
 	}
 
 	return v2Key, v2Val
+}
+
+func convToV2ValStr(val interface{}) string {
+	switch realVal := val.(type) {
+	case int:
+		return strconv.FormatInt(int64(realVal), 10)
+	case int8:
+		return strconv.FormatInt(int64(realVal), 10)
+	case int16:
+		return strconv.FormatInt(int64(realVal), 10)
+	case int32:
+		return strconv.FormatInt(int64(realVal), 10)
+	case int64:
+		return strconv.FormatInt(realVal, 10)
+	case uint:
+		return strconv.FormatInt(int64(realVal), 10)
+	case uint8:
+		return strconv.FormatInt(int64(realVal), 10)
+	case uint16:
+		return strconv.FormatInt(int64(realVal), 10)
+	case uint32:
+		return strconv.FormatInt(int64(realVal), 10)
+	case uint64:
+		return strconv.FormatInt(int64(realVal), 10)
+	case float32:
+		return strconv.FormatInt(int64(realVal), 10)
+	case float64:
+		return strconv.FormatInt(int64(realVal), 10)
+	case json.Number:
+		jsVal, err := realVal.Int64()
+		if err != nil {
+			return realVal.String()
+		}
+		return strconv.FormatInt(jsVal, 10)
+	case string:
+		return realVal
+	}
+	return fmt.Sprintf("%v", val)
 }
 
 func getFieldsMap(objType string) map[string]string {
