eb96c2b3f57e7fee917abb293b4bb912e33dabbc
ProtosL <644074553@qq.com>
2018-12-03 21:20:56 +0800

    minor: 长短字符支持输入@和#号

--- src/ui/src/setup/validate.js
+++ src/ui/src/setup/validate.js
@@ -6,7 +6,7 @@ const customRules = {
     singlechar: {
         validate: value => {
             /* eslint-disable */
-            return /^([a-zA-Z0-9]|[\u4e00-\u9fa5]|[\(\)\+\-《》_,，；:;“”‘’。\."'\\\/\s]){0,256}$/.test(value)
+            return /^([a-zA-Z0-9]|[\u4e00-\u9fa5]|[\(\)\+\-《》_,，；:;“”‘’。@#\."'\\\/\s]){0,256}$/.test(value)
             /* eslint-enable */
         }
     },
