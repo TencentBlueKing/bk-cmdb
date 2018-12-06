466532e051b4b227dba8637d51de3ca4b6c7e032
peibiaoyang(杨培彪) <itsfantacy@hotmail.com>
2018-12-06 15:06:54 +0800

    bugfix:修复主机编辑时会提交云区域数据
    Former-commit-id: a735108522d9a8bd985acf018951656e8049841d

--- src/ui/src/utils/tools.js
+++ src/ui/src/utils/tools.js
@@ -115,7 +115,7 @@ export function getInstFormValues (properties, inst = {}) {
     properties.forEach(property => {
         const propertyId = property['bk_property_id']
         const propertyType = property['bk_property_type']
-        if (['singleasst', 'multiasst'].includes(propertyType)) {
+        if (['singleasst', 'multiasst', 'foreignkey'].includes(propertyType)) {
             // const validAsst = (inst[propertyId] || []).filter(asstInst => asstInst.id !== '')
             // values[propertyId] = validAsst.map(asstInst => asstInst['bk_inst_id']).join(',')
         } else if (['date', 'time'].includes(propertyType)) {
