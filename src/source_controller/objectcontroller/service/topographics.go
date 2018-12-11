3fd15a17992ae706dd5320686206bffec0fdbda4
breezeli <breezeli@tencent.com>
2018-12-11 22:17:47 +0800

    optimize: change topo graphics positioon from pointer to normal type.

--- src/source_controller/objectcontroller/service/topographics.go
+++ src/source_controller/objectcontroller/service/topographics.go
@@ -96,6 +96,7 @@ func (cli *Service) UpdateTopoGraphics(req *restful.Request, resp *restful.Respo
 			condition.SetObjID(datas[index].ObjID)
 			condition.SetInstID(datas[index].InstID)
 			condition.SetSupplierAccount(ownerID)
+			condition.SetPosition(datas[index].Position)
 			if err = cli.Instance.Table(common.BKTableNameTopoGraphics).Update(context.Background(), condition, datas[index]); err != nil {
 				blog.Errorf("update data failed, error information is %s", err.Error())
 				resp.WriteError(http.StatusBadRequest, &meta.RespError{Msg: defErr.New(common.CCErrCommDBUpdateFailed, err.Error())})
