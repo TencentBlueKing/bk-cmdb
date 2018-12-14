50dec57d6ffa280ffaafd8636febdb9a0a4aa0cf
peibiaoyang(杨培彪) <itsfantacy@hotmail.com>
2018-12-14 15:17:51 +0800

    fix:ui:修复拓扑节点属性编辑保存后数据显示不正确的问题
    Former-commit-id: b7cb35bd437674238320e304b008e7b67ffc0ef8

--- src/ui/src/views/topology/index.vue
+++ src/ui/src/views/topology/index.vue
@@ -667,7 +667,7 @@
                     const instNameKey = ['set', 'module'].includes(objId) ? `bk_${objId}_name` : 'bk_inst_name'
                     selectedNode['bk_inst_name'] = formData[instNameKey]
                     this.tree.selectedNodeInst = value
-                    this.tree.flatternedSelectedNodeInst = this.$tools.flatternList(this.tab.properties, value)
+                    this.tree.flatternedSelectedNodeInst = this.$tools.flatternItem(this.tab.properties, value)
                     this.tab.type = 'details'
                     this.$success(this.$t('Common[\'修改成功\']'))
                 })
