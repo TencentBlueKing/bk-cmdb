0f2cfe98cc0f352d5641cc60e8f442a0d07d4adb
lowenitt <lowenitt@sina.com>
2019-01-02 15:24:06 +0800

    feature: add coreservice config file  issue #1649
    Former-commit-id: 8e80760ea1a30ffc45205b0b0c398d70a3c80abb

--- src/common/definitions.go
+++ src/common/definitions.go
@@ -788,14 +788,14 @@ const (
 
 // integer const
 const (
-	MaxUint64 = ^uint64(0)
-	MinUint64 = 0
-	MaxInt64  = int64(MaxUint64 >> 1)
-	MinInt64  = -MaxInt64 - 1
-	MaxUint   = ^uint(0)
-	MinUint   = 0
-	MaxInt    = int(MaxUint >> 1)
-	MinInt    = -MaxInt - 1
+	MaxUint64  = ^uint64(0)
+	MinUint64  = 0
+	MaxInt64   = int64(MaxUint64 >> 1)
+	MinInt64   = -MaxInt64 - 1
+	MaxUint    = ^uint(0)
+	MinUint    = 0
+	MaxInt     = int(MaxUint >> 1)
+	MinInt     = -MaxInt - 1
 	MaxFloat64 = math.MaxFloat64
 	MinFloat64 = -math.MaxFloat64
 )
@@ -824,8 +824,8 @@ const (
 type DataStatusFlag string
 
 const (
-	DataStatusDisabled string = "disabled"
-	DataStatusEnable   string = "enable"
+	DataStatusDisabled DataStatusFlag = "disabled"
+	DataStatusEnable   DataStatusFlag = "enable"
 )
 
 const (
