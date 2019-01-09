6c231760ab1ae964b021d3dc4f5433f16f469515
peibiaoyang(杨培彪) <itsfantacy@hotmail.com>
2019-01-09 16:02:44 +0800

    feature:ui: injectMetadata mixin添加配置指定是否克隆参数后返回
    Former-commit-id: 9d6c520ab95f33ef757065ea131f3fbaf1a1a940

--- src/ui/src/mixins/inject.js
+++ src/ui/src/mixins/inject.js
@@ -1,7 +1,12 @@
 export default {
     methods: {
-        $injectMetadata (params) {
-            const injectedParams = this.$tools.clone(params)
+        $injectMetadata (params, shouldClone = true) {
+            let injectedParams
+            if (shouldClone) {
+                injectedParams = this.$tools.clone(params)
+            } else {
+                injectedParams = params
+            }
             if (!this.$store.getters.isAdminView) {
                 Object.assign(injectedParams, {
                     metadata: {
