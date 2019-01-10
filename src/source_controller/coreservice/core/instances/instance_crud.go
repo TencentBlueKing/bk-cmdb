877066d8869e37051c4dd6cc0e50038c8d049e09
wusendong <wusendong@gmail.com>
2019-01-10 14:29:43 +0800

    refactor: update coreservice
    Former-commit-id: 3f8d30bd9b2aad93eb66a7a453383041c43400f0

--- src/source_controller/coreservice/core/instances/instance_crud.go
+++ src/source_controller/coreservice/core/instances/instance_crud.go
@@ -85,7 +85,9 @@ func (m *instanceManager) searchInstance(ctx core.ContextParams, objID string, i
 	if nil != err {
 		return results, err
 	}
-	condition.And(&mongo.Eq{Key: common.BKObjIDField, Val: objID})
+	if tableName == common.BKTableNameBaseInst {
+		condition.And(&mongo.Eq{Key: common.BKObjIDField, Val: objID})
+	}
 	instHandler := m.dbProxy.Table(tableName).Find(condition.ToMapStr())
 	for _, sort := range inputParam.SortArr {
 		fileld := sort.Field
@@ -105,7 +107,9 @@ func (m *instanceManager) countInstance(ctx core.ContextParams, objID string, co
 	if nil != err {
 		return 0, err
 	}
-	condition.And(&mongo.Eq{Key: common.BKObjIDField, Val: objID})
+	if tableName == common.BKTableNameBaseInst {
+		condition.And(&mongo.Eq{Key: common.BKObjIDField, Val: objID})
+	}
 	count, err = m.dbProxy.Table(tableName).Find(condition.ToMapStr()).Count(ctx)
 
 	return count, err
