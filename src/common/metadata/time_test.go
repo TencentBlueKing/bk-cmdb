c33fd7974f33d697f768b9141095ec5945b75b6c
holmeswang <uriwang1986@gmail.com>
2019-01-11 23:37:23 +0800

    test:delete unused test file and add time testcase, issue #1720
    Former-commit-id: 81fcab1df66b499fc450a675581a12347bafab1d

---
+++ src/common/metadata/time_test.go
@@ -0,0 +1,39 @@
+/*
+ * Tencent is pleased to support the open source community by making 蓝鲸 available.
+ * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package metadata
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIssue1720(t *testing.T) {
+	type testData struct {
+		TestName   string `json:"test_name"`
+		CreateTime *Time  `json:"create_time"`
+	}
+
+	inputData, err := json.Marshal(&testData{TestName: "hello-name", CreateTime: &Time{Time: time.Now()}})
+
+	require.NoError(t, err)
+	t.Logf("input data:%s", inputData)
+
+	out := &testData{}
+	err = json.Unmarshal(inputData, out)
+	require.NoError(t, err)
+	t.Logf("out:%#v %s", out, out.CreateTime)
+
+}
