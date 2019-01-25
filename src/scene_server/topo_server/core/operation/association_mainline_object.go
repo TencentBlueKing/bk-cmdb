77f0a6bd9c1299feb6bccc52614df085bcd8a350
hoffermei(梅海峰) <hoffermei@tencent.com>
2019-01-25 20:09:12 +0800

    create mainline测试用例运行失败处理：新增 bk_classification_id 为空的校验
    Former-commit-id: e856077aa847d8ef9a93bdd2d64261a9d78e528e

--- src/scene_server/topo_server/core/operation/association_mainline_object.go
+++ src/scene_server/topo_server/core/operation/association_mainline_object.go
@@ -127,6 +127,11 @@ func (a *association) CreateMainlineAssociation(params types.ContextParams, data
 		return nil, params.Err.Errorf(common.CCErrCommParamsNeedSet, common.BKAsstObjIDField)
 	}
 
+	if data.ClassificationID == "" {
+		blog.Errorf("[operation-asst] bk_classification_id empty,rid:%s", util.GetHTTPCCRequestID(params.Header))
+		return nil, params.Err.Errorf(common.CCErrCommParamsNeedSet, common.BKClassificationIDField)
+	}
+
 	items, err := a.SearchMainlineAssociationTopo(params, bizObj)
 	if nil != err {
 		blog.Errorf("[operation-asst] failed to check the mainline topo level, error info is %s", err.Error())
