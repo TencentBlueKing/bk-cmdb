f811b3bdea312f49bd196c4cc9d0baeac33cd73b
wusendong <wusendong@gmail.com>
2019-02-15 10:38:53 +0800

    fix: don't return error if index not found
    Former-commit-id: dd900726961463d3f9b9ad6377b01c10b24e3f8a

--- src/scene_server/admin_server/upgrader/x19.01.18.01/drop_index.go
+++ src/scene_server/admin_server/upgrader/x19.01.18.01/drop_index.go
@@ -14,14 +14,17 @@ package x19_01_18_01
 
 import (
 	"context"
+	"strings"
 
 	"configcenter/src/scene_server/admin_server/upgrader"
 	"configcenter/src/storage/dal"
 )
 
 func dropIndex(ctx context.Context, db dal.RDB, conf *upgrader.Config) (err error) {
-	if err = db.Table("cc_TopoGraphics").DropIndex(ctx, "scope_id_1_node_type_1_bk_obj_id_1_bk_inst_id_1_scope_type_1"); err != nil {
+	if err = db.Table("cc_TopoGraphics").
+		DropIndex(ctx, "scope_id_1_node_type_1_bk_obj_id_1_bk_inst_id_1_scope_type_1"); err != nil &&
+		!strings.Contains(err.Error(), "not found") {
 		return err
 	}
-	return
+	return nil
 }
