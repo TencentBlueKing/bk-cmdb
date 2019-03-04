96d16fed15ae5b6594d5a2b635fffc10b4826309
hoffermei(梅海峰) <hoffer@vip.qq.com>
2019-03-04 11:10:01 +0800

    worker queue

---
+++ src/scene_server/auth_synchronizer/pkg/synchronizer/worker.go
@@ -0,0 +1,76 @@
+/*
+ * Tencent is pleased to support the open source community by making 蓝鲸 available.
+ * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package synchronizer
+
+import (
+	"fmt"
+	"time"
+)
+
+// NewWorker creates, and returns a new Worker object. Its only argument
+// is a channel that the worker can add itself to whenever it is done its
+// work.
+func NewWorker(id int, workerQueue chan WorkRequest, handler *SyncHandler) Worker {
+	// Create, and return the worker.
+	worker := Worker{
+		ID:          id,
+		Work:        make(chan WorkRequest),
+		WorkerQueue: workerQueue,
+		QuitChan:    make(chan bool),
+		SyncHandler: handler,
+	}
+
+	return worker
+}
+
+type Worker struct {
+	ID          int
+	Work        chan WorkRequest
+	WorkerQueue chan chan WorkRequest
+	QuitChan    chan bool
+	SyncHandler SyncHandler
+}
+
+// This function "starts" the worker by starting a goroutine, that is
+// an infinite "for-select" loop.
+func (w *Worker) Start() {
+	go func() {
+		for {
+			// Add ourselves into the worker queue.
+			w.WorkerQueue <- w.Work
+
+			select {
+			case work := <-w.Work:
+				// Receive a work request.
+				fmt.Printf("worker%d: Received work request, delaying for %f seconds\n", w.ID, work.Delay.Seconds())
+
+				time.Sleep(work.Delay)
+				fmt.Printf("worker%d: Hello, %s!\n", w.ID, work.Name)
+
+			case <-w.QuitChan:
+				// We have been asked to stop.
+				fmt.Printf("worker%d stopping\n", w.ID)
+				return
+			}
+		}
+	}()
+}
+
+// Stop tells the worker to stop listening for work requests.
+//
+// Note that the worker will only stop *after* it has finished its work.
+func (w *Worker) Stop() {
+	go func() {
+		w.QuitChan <- true
+	}()
+}
