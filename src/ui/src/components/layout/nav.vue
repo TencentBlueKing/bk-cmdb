4e034e579922492267f5ee0469c8d297ccb06f3d
v_dgzheng(郑定国) <v_dgzheng@tencent.com>
2019-03-18 11:05:58 +0800

    update:ui:管理后台导航样式改动

--- src/ui/src/components/layout/nav.vue
+++ src/ui/src/components/layout/nav.vue
@@ -252,11 +252,11 @@ $color: #979ba5;
         margin: 0 0 0 4px;
         vertical-align: middle;
         border-radius: 2px;
-        color: #fff;
+        color: #282B41;
         font-size: 20px;
         font-weight: normal;
         line-height: 32px;
-        background: #e3a547;
+        background: #18B48A;
         transform: scale(0.5);
         transform-origin: left center;
     }
