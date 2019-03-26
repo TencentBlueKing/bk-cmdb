a7d36776086555df4443c5f71beb6f0db8a1d441
Daryl0819 <zhdgmax@qq.com>
2019-03-26 16:46:33 +0800

    update:ui: 调整前端文字

--- src/ui/src/views/model-manage/index.vue
+++ src/ui/src/views/model-manage/index.vue
@@ -103,7 +103,7 @@
             :quick-close="false"
             :is-show.sync="modelDialog.isShow">
             <div slot="content" class="dialog-content">
-                <p class="title">{{$t('ModelManagement["新增模型"]')}}</p>
+                <p class="title">{{$t('ModelManagement["新建模型"]')}}</p>
                 <div class="content clearfix">
                     <div class="content-left" @click="modelDialog.isIconListShow = true">
                         <div class="icon-wrapper">
