1ab230b225720457f4059437f97e81aa8cc9ea4f
rentiansheng <rentiansheng@163.com>
2019-03-28 14:36:39 +0800

    style:delete get gseagent status api issue  #1949
    Former-commit-id: 5455a1f13d356cccd36db4fd7db2dd5c28bc39a0

--- src/scene_server/host_server/service/phpapi.go
+++ src/scene_server/host_server/service/phpapi.go
@@ -880,31 +880,6 @@ func (s *Service) DelPlat(req *restful.Request, resp *restful.Response) {
 	}
 }
 
-func (s *Service) GetAgentStatus(req *restful.Request, resp *restful.Response) {
-	defErr := s.CCErr.CreateDefaultCCErrorIf(util.GetActionLanguage(req))
-
-	// 获取AppID
-	pathParams := req.PathParameters()
-	appID, err := util.GetInt64ByInterface(pathParams["appid"])
-	if nil != err {
-		blog.Errorf("GetAgentStatus error :%s", err)
-		resp.WriteError(http.StatusBadRequest, &meta.RespError{Msg: defErr.Errorf(common.CCErrCommParamsInvalid, err.Error())})
-		return
-	}
-
-	res, err := s.Logics.GetAgentStatus(appID, &s.Config.Gse, req.Request.Header)
-	if nil != err {
-		blog.Errorf("GetAgentStatus error :%v", err)
-		resp.WriteError(http.StatusInternalServerError, &meta.RespError{Msg: defErr.Errorf(common.CCErrCommParamsInvalid, err.Error())})
-		return
-	}
-	resp.WriteEntity(meta.Response{
-		BaseResp: meta.SuccessBaseResp,
-		Data:     res,
-	})
-
-}
-
 func (s *Service) getHostListByAppidAndField(req *restful.Request, resp *restful.Response) {
 
 	defErr := s.CCErr.CreateDefaultCCErrorIf(util.GetActionLanguage(req))
