85bdf153bce5d96d370c8ebb2d145a9c618e8de5
Daryl0819 <zhdgmax@qq.com>
2019-04-04 14:53:27 +0800

    feature:ui: 添加权限tips界面

--- src/ui/src/views/status/require-business.vue
+++ src/ui/src/views/status/require-business.vue
@@ -1,17 +1,44 @@
 <template>
-    <div>
-        <span v-if="$i18n.locale === 'en'">
-            You don't have any business permission, please contact with administrator.
-        </span>
-        <span v-else>
-            当前并无任何业务权限，请联系管理员添加。
-        </span>
+    <div class="tips-wrapper">
+        <div class="content-wrapper">
+            <div class="title">
+                <i class="bk-icon icon-cc-no-authority"></i>
+                <h2>{{$t("Common['无业务权限']")}}</h2>
+            </div>
+            <p>{{$t("Common['点击下方按钮申请']")}}</p>
+            <div class="btns">
+                <bk-button type="default">
+                    {{$t("Common['申请业务权限']")}}
+                </bk-button>
+            </div>
+        </div>
     </div>
 </template>
 <script>
     export default {
-        created () {
-            this.$store.commit('setHeaderTitle', '')
-        }
     }
 </script>
+
+<style lang="scss" scoped>
+    .content-wrapper {
+        margin-top: 100px;
+        text-align: center;
+        color: #63656E;
+        font-size: 14px;
+        .title h2 {
+            margin-top: 10px;
+            margin-bottom: 10px;
+            font-size: 22px;
+            color: #313238;
+            font-weight: normal;
+        }
+        .btns {
+            margin-top: 24px;
+            .bk-button {
+                border-radius: 3px;
+                padding-left: 10px;
+                padding-right: 10px;
+            }
+        }
+    }
+</style>
