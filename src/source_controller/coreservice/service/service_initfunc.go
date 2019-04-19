457ce1c17177f4cd48eb3ab31fa4d295b1c90a86
rentiansheng <rentiansheng@163.com>
2019-04-19 11:30:08 +0800

    feature: add delete host api action  issue #2086
    Former-commit-id: a59af5e618a307ffd29a1edf9abb7cc14af7122a

--- src/source_controller/coreservice/service/service_initfunc.go
+++ src/source_controller/coreservice/service/service_initfunc.go
@@ -119,6 +119,7 @@ func (s *coreService) host() {
 	s.addAction(http.MethodPost, "/set/module/host/relation/module", s.TransferHostModule, nil)
 	s.addAction(http.MethodPost, "/set/module/host/relation/cross/business", s.TransferHostCrossBusiness, nil)
 	s.addAction(http.MethodPost, "/read/module/host/relation", s.GetHostModuleRelation, nil)
+	s.addAction(http.MethodDelete, "/delete/host", s.DeleteHost, nil)
 
 }
 
