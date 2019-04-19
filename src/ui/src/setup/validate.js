2e597fe2285bf8bc8a664c538f61d146cad06be2
Daryl0819 <zhdgmax@qq.com>
2019-04-19 16:36:34 +0800

    fix:ui: 修改关联类型正则

--- src/ui/src/setup/validate.js
+++ src/ui/src/setup/validate.js
@@ -17,6 +17,11 @@ const customRules = {
             /* eslint-enable */
         }
     },
+    lowercaseChar: {
+        validate: (value) => {
+            return /^[a-z]+$/.test(value)
+        }
+    },
     classifyName: {
         validate: value => {
             return /^([a-zA-Z0-9_ ]|[\u4e00-\u9fa5]|[\uac00-\ud7ff]|[\u0800-\u4e00]){1,20}$/.test(value)
@@ -97,6 +102,7 @@ const dictionary = {
             regex: () => '请输入符合自定义正则的内容',
             longchar: () => '请输入正确的长字符内容',
             singlechar: () => '请输入正确的短字符内容',
+            lowercaseChar: () => '请输入小写字母',
             classifyName: () => '请输入正确的内容',
             classifyId: () => '请输入正确的内容',
             required: () => '该字段是必填项',
@@ -124,6 +130,7 @@ const dictionary = {
             regex: () => 'Please enter the correct content that conform custom regex',
             longchar: () => 'Please enter the correct content',
             singlechar: () => 'Please enter the correct content',
+            lowercaseChar: () => 'Please enter lowercase letter',
             classifyName: () => 'Please enter the correct content',
             classifyId: () => 'Please enter the correct content',
             required: () => 'This field is required',
