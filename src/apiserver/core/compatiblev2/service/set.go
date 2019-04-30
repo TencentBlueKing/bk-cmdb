bd5ce7970ff94a1ca551493063afff53a9262d82
breezeli <breezelee6@163.com>
2019-04-30 09:54:02 +0800

    optimize: rollback json package to encoding/json.
    Former-commit-id: ec398e1588109248f4024bf16618970ddab3a56d

--- src/apiserver/core/compatiblev2/service/set.go
+++ src/apiserver/core/compatiblev2/service/set.go
@@ -13,6 +13,7 @@
 package service
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/url"
 	"strconv"
@@ -22,7 +23,6 @@ import (
 	"configcenter/src/apiserver/core/compatiblev2/common/utils"
 	"configcenter/src/common"
 	"configcenter/src/common/blog"
-	"configcenter/src/common/json"
 	"configcenter/src/common/mapstr"
 	"configcenter/src/common/paraparse"
 	"configcenter/src/common/util"
