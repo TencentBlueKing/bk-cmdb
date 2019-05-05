b5a0797091e4fdc7137cb1605b76a2627c7f7afd
wusendong <wusendong@gmail.com>
2019-05-05 11:00:09 +0800

    replace auditlog for procserver
    Former-commit-id: 87d9cb0d0a62eacd5bf22cfe92c5777478722ed8

--- src/scene_server/proc_server/proc_service/service/proc2template.go
+++ src/scene_server/proc_server/proc_service/service/proc2template.go
@@ -147,8 +147,15 @@ func (ps *ProcServer) BindProc2Template(req *restful.Request, resp *restful.Resp
 	}
 
 	// save operation log
-	log := common.KvMap{common.BKOpDescField: fmt.Sprintf("bind template [%d] to process [%d]", tempID, procID), common.BKOpTypeField: auditoplog.AuditOpTypeAdd}
-	ps.CoreAPI.AuditController().AddProcLog(srvData.ctx, srvData.ownerID, appIDStr, srvData.user, srvData.header, log)
+	log := meta.SaveAuditLogParams{
+		ID:      int64(procID),
+		Model:   common.BKInnerObjIDProc,
+		Content: meta.Content{},
+		OpDesc:  fmt.Sprintf("bind template [%d] to process [%d]", tempID, procID),
+		OpType:  auditoplog.AuditOpTypeAdd,
+		BizID:   int64(appID),
+	}
+	ps.CoreAPI.CoreService().Audit().SaveAuditLog(srvData.ctx, srvData.header, log)
 
 	resp.WriteEntity(meta.NewSuccessResp(nil))
 }
@@ -198,8 +205,15 @@ func (ps *ProcServer) DeleteProc2Template(req *restful.Request, resp *restful.Re
 	}
 
 	// save operation log
-	log := common.KvMap{common.BKOpDescField: fmt.Sprintf("unbind template [%d] to process [%d]", tempID, procID), common.BKOpTypeField: auditoplog.AuditOpTypeDel}
-	ps.CoreAPI.AuditController().AddProcLog(srvData.ctx, srvData.ownerID, appIDStr, srvData.user, req.Request.Header, log)
+	log := meta.SaveAuditLogParams{
+		ID:      int64(procID),
+		Model:   common.BKInnerObjIDProc,
+		Content: meta.Content{},
+		OpDesc:  fmt.Sprintf("unbind template [%d] to process [%d]", tempID, procID),
+		OpType:  auditoplog.AuditOpTypeDel,
+		BizID:   int64(appID),
+	}
+	ps.CoreAPI.CoreService().Audit().SaveAuditLog(srvData.ctx, srvData.header, log)
 
 	resp.WriteEntity(meta.NewSuccessResp(nil))
 }
