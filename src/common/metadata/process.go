45f0f3faa8b2e0ad83f9aab8669a858fe3dd416e
hoffermei(梅海峰) <hoffer@vip.qq.com>
2019-05-14 15:30:08 +0800

    define enum type for socket bind type and protocol type
    Former-commit-id: 500da661354cc46ae9727226058a920c7399cecc

--- src/common/metadata/process.go
+++ src/common/metadata/process.go
@@ -13,32 +13,74 @@ package metadata
 
 import "time"
 
+type SocketBindType string
+
+const (
+	BindLocalHost SocketBindType = "1"
+	BindAll       SocketBindType = "2"
+	BindInnerIP   SocketBindType = "3"
+	BindOtterIP   SocketBindType = "4"
+)
+
+func (p SocketBindType) String() string {
+	switch p {
+	case BindLocalHost:
+		return "127.0.0.1"
+	case BindAll:
+		return "0.0.0.0"
+	case BindInnerIP:
+		return "第一内网IP"
+	case BindOtterIP:
+		return "第一外网IP"
+	default:
+		return ""
+	}
+}
+
+type ProtocolType string
+
+const (
+	ProtocolType_TCP ProtocolType = "1"
+	ProtocolType_UDP ProtocolType = "2"
+)
+
+func (p ProtocolType) String() string {
+	switch p {
+	case ProtocolType_TCP:
+		return "TCP"
+	case ProtocolType_UDP:
+		return "UDP"
+	default:
+		return ""
+	}
+}
+
 type Process struct {
-	ProcNum         int64     `field:"proc_num" json:"proc_num" bson:"proc_num"`
-	StopCmd         string    `field:"stop_cmd" json:"stop_cmd" bson:"stop_cmd"`
-	RestartCmd      string    `field:"restart_cmd" json:"restart_cmd" bson:"restart_cmd"`
-	ForceStopCmd    string    `field:"face_stop_cmd" json:"face_stop_cmd" bson:"face_stop_cmd"`
-	ProcessID       int       `field:"bk_process_id" json:"bk_process_id" bson:"bk_process_id"`
-	FuncName        string    `field:"bk_func_name" json:"bk_func_name" bson:"bk_func_name"`
-	WorkPath        string    `field:"work_path" json:"work_path" bson:"work_path"`
-	BindIP          string    `field:"bind_ip" json:"bind_ip" bson:"bind_ip"`
-	Priority        int64     `field:"priority" json:"priority" bson:"priority"`
-	ReloadCmd       string    `field:"reload_cmd" json:"reload_cmd" bson:"reload_cmd"`
-	ProcessName     string    `field:"bk_process_name" json:"bk_process_name" bson:"bk_process_name"`
-	Port            string    `field:"port" json:"port" bson:"port"`
-	PidFile         string    `field:"pid_file" json:"pid_file" bson:"pid_file"`
-	AutoStart       bool      `field:"auto_start" json:"auto_start" bson:"auto_start"`
-	AutoTimeGap     int64     `field:"auto_time_gap" json:"auto_time_gap" bson:"auto_time_gap"`
-	LastTime        time.Time `field:"last_time" json:"last_time" bson:"last_time"`
-	CreateTime      time.Time `field:"create_time" json:"create_time" bson:"create_time"`
-	BizID           int64     `field:"bk_biz_id" json:"bk_biz_id" bson:"bk_biz_id"`
-	StartCmd        string    `field:"start_cmd" json:"start_cmd" bson:"start_cmd"`
-	FuncID          string    `field:"bk_func_id" json:"bk_func_id" bson:"bk_func_id"`
-	User            string    `field:"user" json:"user" bson:"user"`
-	Timeout         int64     `field:"timeout" json:"timeout" bson:"timeout"`
-	Protocol        string    `field:"protocol" json:"protocol" bson:"protocol"`
-	Description     string    `field:"description" json:"description" bson:"description"`
-	SupplierAccount string    `field:"bk_supplier_account" json:"bk_supplier_account" bson:"bk_supplier_account"`
+	ProcNum         int64          `field:"proc_num" json:"proc_num" bson:"proc_num"`
+	StopCmd         string         `field:"stop_cmd" json:"stop_cmd" bson:"stop_cmd"`
+	RestartCmd      string         `field:"restart_cmd" json:"restart_cmd" bson:"restart_cmd"`
+	ForceStopCmd    string         `field:"face_stop_cmd" json:"face_stop_cmd" bson:"face_stop_cmd"`
+	ProcessID       int64          `field:"bk_process_id" json:"bk_process_id" bson:"bk_process_id"`
+	FuncName        string         `field:"bk_func_name" json:"bk_func_name" bson:"bk_func_name"`
+	WorkPath        string         `field:"work_path" json:"work_path" bson:"work_path"`
+	BindIP          SocketBindType `field:"bind_ip" json:"bind_ip" bson:"bind_ip"`
+	Priority        int64          `field:"priority" json:"priority" bson:"priority"`
+	ReloadCmd       string         `field:"reload_cmd" json:"reload_cmd" bson:"reload_cmd"`
+	ProcessName     string         `field:"bk_process_name" json:"bk_process_name" bson:"bk_process_name"`
+	Port            string         `field:"port" json:"port" bson:"port"`
+	PidFile         string         `field:"pid_file" json:"pid_file" bson:"pid_file"`
+	AutoStart       bool           `field:"auto_start" json:"auto_start" bson:"auto_start"`
+	AutoTimeGap     int64          `field:"auto_time_gap" json:"auto_time_gap" bson:"auto_time_gap"`
+	LastTime        time.Time      `field:"last_time" json:"last_time" bson:"last_time"`
+	CreateTime      time.Time      `field:"create_time" json:"create_time" bson:"create_time"`
+	BusinessID      int64          `field:"bk_biz_id" json:"bk_biz_id" bson:"bk_biz_id"`
+	StartCmd        string         `field:"start_cmd" json:"start_cmd" bson:"start_cmd"`
+	FuncID          string         `field:"bk_func_id" json:"bk_func_id" bson:"bk_func_id"`
+	User            string         `field:"user" json:"user" bson:"user"`
+	TimeoutSeconds  int64          `field:"timeout" json:"timeout" bson:"timeout"`
+	Protocol        ProtocolType   `field:"protocol" json:"protocol" bson:"protocol"`
+	Description     string         `field:"description" json:"description" bson:"description"`
+	SupplierAccount string         `field:"bk_supplier_account" json:"bk_supplier_account" bson:"bk_supplier_account"`
 }
 
 type ServiceCategory struct {
