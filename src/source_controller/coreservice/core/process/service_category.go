5655e84531da6103eb5afd4cd8968e58716ef9d7
hoffermei(梅海峰) <hoffer@vip.qq.com>
2019-05-16 11:41:23 +0800

    1. servicecategory新增字段标记内置类型；2. 删除category时检查引用次数和子节点书 #2361

--- src/source_controller/coreservice/core/process/service_category.go
+++ src/source_controller/coreservice/core/process/service_category.go
@@ -59,6 +59,7 @@ func (p *processOperation) CreateServiceCategory(ctx core.ContextParams, categor
 		category.RootID = category.ID
 	}
 	
+	category.IsBuiltIn = false
 	category.SupplierAccount = ctx.SupplierAccount
 	
 	if err := p.dbProxy.Table(common.BKTableNameServiceCategory).Insert(ctx.Context, &category); nil != err {
@@ -78,7 +79,7 @@ func (p *processOperation) GetServiceCategory(ctx core.ContextParams, categoryID
 	if err := p.dbProxy.Table(common.BKTableNameServiceCategory).Find(filter.ToMapStr()).One(ctx.Context, &category); nil != err {
 		blog.Errorf("GetServiceCategory failed, mongodb failed, table: %s, err: %+v, rid: %s", common.BKTableNameServiceCategory, err, ctx.ReqID)
 		if err.Error() == "document not found" {
-			return nil, ctx.Error.CCError(common.CCErrCoreServiceServiceCategoryNotFound)
+			return nil, ctx.Error.CCError(common.CCErrCommNotFound)
 		}
 		return  nil, err
 	}
@@ -94,6 +95,17 @@ func (p *processOperation) UpdateServiceCategory(ctx core.ContextParams, categor
 
 	// update fields to local object
 	category.Name = input.Name
+	if field, err := category.Validate(); err != nil {
+		blog.Errorf("UpdateServiceCategory failed, validation failed, code: %d, err: %+v, rid: %s", common.CCErrCommParamsInvalid, err, ctx.ReqID)
+		err := ctx.Error.New(common.CCErrCommParamsInvalid, field)
+		return nil, err
+	}
+	
+	if category.IsBuiltIn == true {
+		blog.Errorf("UpdateServiceCategory failed, forbidden update built-in category, code: %d, rid: %s", common.CCErrCommOperateBuiltInItemForbidden, ctx.ReqID)
+		err := ctx.Error.CCError(common.CCErrCommOperateBuiltInItemForbidden)
+		return nil, err
+	}
 	
 	// do update
 	filter := condition.CreateCondition()
@@ -126,17 +138,46 @@ func (p *processOperation) ListServiceCategories(ctx core.ContextParams, bizID i
 }
 
 func (p *processOperation) DeleteServiceCategory(ctx core.ContextParams, categoryID int64) error {
-	_, err := p.GetServiceCategory(ctx, categoryID)
+	category, err := p.GetServiceCategory(ctx, categoryID)
 	if err != nil {
 		blog.Errorf("DeleteServiceCategory failed, GetServiceCategory failed, categoryID: %d, err: %+v, rid: %s", categoryID, err, ctx.ReqID)
 		return err
 	}
-	
-	// TODO: support delete sub tree at same time 
 
-	filter := condition.CreateCondition()
-	filter.Field(common.BKFieldID).Eq(categoryID)
-	if err := p.dbProxy.Table(common.BKTableNameServiceCategory).Delete(ctx, filter.ToMapStr()); nil != err {
+	if category.IsBuiltIn == true {
+		blog.Errorf("DeleteServiceCategory failed, forbidden delete built-in category, code: %d, rid: %s", common.CCErrCommOperateBuiltInItemForbidden, ctx.ReqID)
+		err := ctx.Error.CCError(common.CCErrCommOperateBuiltInItemForbidden)
+		return err
+	}
+
+	// category that has sub category shouldn't be removed
+	childrenFilter := map[string]int64{"parent_id": category.ID}
+	childrenCount, err := p.dbProxy.Table(common.BKTableNameServiceCategory).Find(childrenFilter).Count(ctx.Context)
+	if nil != err {
+		blog.Errorf("DeleteServiceCategory failed, mongodb failed, table: %s, err: %+v, rid: %s", common.BKTableNameServiceCategory, err, ctx.ReqID)
+		return  err
+	}
+	if childrenCount > 0 {
+		blog.Errorf("DeleteServiceCategory failed, forbidden delete category has children node, childrenCoundt: %d, code: %d, rid: %s", childrenCount, common.CCErrCommOperateBuiltInItemForbidden, ctx.ReqID)
+		err := ctx.Error.CCError(common.CCErrCommRemoveRecordHasChildrenForbidden)
+		return err
+	}
+
+	// category that referenced by service template shouldn't be removed
+	usageFilter := map[string]int64{"service_category_id": category.ID}
+	usageCount, err := p.dbProxy.Table(common.BKTableNameServiceTemplate).Find(usageFilter).Count(ctx.Context)
+	if nil != err {
+		blog.Errorf("DeleteServiceCategory failed, mongodb failed, table: %s, err: %+v, rid: %s", common.BKTableNameServiceTemplate, err, ctx.ReqID)
+		return  err
+	}
+	if usageCount > 0 {
+		blog.Errorf("DeleteServiceCategory failed, forbidden delete category be referenced, code: %d, rid: %s", common.CCErrCommRemoveRecordHasChildrenForbidden, ctx.ReqID)
+		err := ctx.Error.CCError(common.CCErrCommRemoveRecordHasChildrenForbidden)
+		return err
+	}
+
+	deleteFilter := map[string]int64{common.BKFieldID: category.ID}
+	if err := p.dbProxy.Table(common.BKTableNameServiceCategory).Delete(ctx, deleteFilter); nil != err {
 		blog.Errorf("DeleteServiceCategory failed, mongodb failed, table: %s, err: %+v, rid: %s", common.BKTableNameServiceCategory, err, ctx.ReqID)
 		return  err
 	}
