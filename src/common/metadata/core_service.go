171c0b3d2b26fbf54accdbed7fbaee9c3c807d21
hoffermei(梅海峰) <hoffer@vip.qq.com>
2019-05-17 12:48:58 +0800

    新增ProcessInstanceRelation api machinery #2361

--- src/common/metadata/core_service.go
+++ src/common/metadata/core_service.go
@@ -325,17 +325,17 @@ type MultipleServiceInstanceResult struct {
 	Data     MultipleServiceInstance `json:"data"`
 }
 
-type OneProcessInstanceResult struct {
+type OneProcessInstanceRelationResult struct {
 	BaseResp `json:",inline"`
-	Data     Process `json:"data"`
+	Data     ProcessInstanceRelation `json:"data"`
 }
 
-type MultipleProcessInstance struct {
-	Count int64     `json:"count"`
-	Info  []Process `json:"info"`
+type MultipleProcessInstanceRelation struct {
+	Count int64                     `json:"count"`
+	Info  []ProcessInstanceRelation `json:"info"`
 }
 
-type MultipleProcessInstanceResult struct {
+type MultipleProcessInstanceRelationResult struct {
 	BaseResp `json:",inline"`
-	Data     MultipleProcessInstance `json:"data"`
+	Data     MultipleProcessInstanceRelation `json:"data"`
 }
