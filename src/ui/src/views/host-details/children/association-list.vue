026ddb2a1d433a27ff7f4dcb01d05809c4ff4cf3
joe511230 <itsfantacy@hotmail.com>
2019-05-19 23:11:59 +0800

    temp:ui: 主机详情-关联
    Former-commit-id: 83fa97eacff468e429226bc969193c8f278c0527

---
+++ src/ui/src/views/host-details/children/association-list.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="association-list"></div>
+</template>
+
+<script>
+    export default {
+        name: 'cmdb-host-association-list',
+        data () {
+            return {}
+        }
+    }
+</script>
