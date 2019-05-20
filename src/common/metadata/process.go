a8532bc04de162132c746dbf6be356a332f92433
hoffermei(梅海峰) <hoffer@vip.qq.com>
2019-05-20 15:36:01 +0800

    FuncID这种实际上是整型但是存储成字符串的类型，单独用PropertyInt64String类型表示及校验  #2361
    Former-commit-id: ad39e3df02c186d8c0b8c0706f2c617e10f05c00

--- src/common/metadata/process.go
+++ src/common/metadata/process.go
@@ -15,6 +15,7 @@ import (
 	"errors"
 	"fmt"
 	"regexp"
+	"strconv"
 	"time"
 
 	"configcenter/src/common"
@@ -310,26 +311,26 @@ func (pt *ProcessTemplate) Validate() (field string, err error) {
 }
 
 type ProcessProperty struct {
-	ProcNum            PropertyInt64    `field:"proc_num" json:"proc_num,omitempty" bson:"proc_num,omitempty"`
-	StopCmd            PropertyString   `field:"stop_cmd" json:"stop_cmd,omitempty" bson:"stop_cmd,omitempty"`
-	RestartCmd         PropertyString   `field:"restart_cmd" json:"restart_cmd,omitempty" bson:"restart_cmd,omitempty"`
-	ForceStopCmd       PropertyString   `field:"face_stop_cmd" json:"face_stop_cmd,omitempty" bson:"face_stop_cmd,omitempty"`
-	FuncName           PropertyString   `field:"bk_func_name" json:"bk_func_name,omitempty" bson:"bk_func_name,omitempty"`
-	WorkPath           PropertyString   `field:"work_path" json:"work_path,omitempty" bson:"work_path,omitempty"`
-	BindIP             PropertyBindIP   `field:"bind_ip" json:"bind_ip,omitempty" bson:"bind_ip,omitempty"`
-	Priority           PropertyInt64    `field:"priority" json:"priority,omitempty" bson:"priority,omitempty"`
-	ReloadCmd          PropertyString   `field:"reload_cmd" json:"reload_cmd,omitempty" bson:"reload_cmd,omitempty"`
-	ProcessName        PropertyString   `field:"bk_process_name" json:"bk_process_name,omitempty" bson:"bk_process_name,omitempty"`
-	Port               PropertyPort     `field:"port" json:"port,omitempty" bson:"port,omitempty"`
-	PidFile            PropertyString   `field:"pid_file" json:"pid_file,omitempty" bson:"pid_file,omitempty"`
-	AutoStart          PropertyBool     `field:"auto_start" json:"auto_start,omitempty" bson:"auto_start,omitempty"`
-	AutoTimeGapSeconds PropertyInt64    `field:"auto_time_gap" json:"auto_time_gap,omitempty" bson:"auto_time_gap,omitempty"`
-	StartCmd           PropertyString   `field:"start_cmd" json:"start_cmd,omitempty" bson:"start_cmd,omitempty"`
-	FuncID             PropertyString   `field:"bk_func_id" json:"bk_func_id,omitempty" bson:"bk_func_id,omitempty"`
-	User               PropertyString   `field:"user" json:"user,omitempty" bson:"user,omitempty"`
-	TimeoutSeconds     PropertyInt64    `field:"timeout" json:"timeout,omitempty" bson:"timeout,omitempty"`
-	Protocol           PropertyProtocol `field:"protocol" json:"protocol,omitempty" bson:"protocol,omitempty"`
-	Description        PropertyString   `field:"description" json:"description,omitempty" bson:"description,omitempty"`
+	ProcNum            PropertyInt64       `field:"proc_num" json:"proc_num,omitempty" bson:"proc_num,omitempty"`
+	StopCmd            PropertyString      `field:"stop_cmd" json:"stop_cmd,omitempty" bson:"stop_cmd,omitempty"`
+	RestartCmd         PropertyString      `field:"restart_cmd" json:"restart_cmd,omitempty" bson:"restart_cmd,omitempty"`
+	ForceStopCmd       PropertyString      `field:"face_stop_cmd" json:"face_stop_cmd,omitempty" bson:"face_stop_cmd,omitempty"`
+	FuncName           PropertyString      `field:"bk_func_name" json:"bk_func_name,omitempty" bson:"bk_func_name,omitempty"`
+	WorkPath           PropertyString      `field:"work_path" json:"work_path,omitempty" bson:"work_path,omitempty"`
+	BindIP             PropertyBindIP      `field:"bind_ip" json:"bind_ip,omitempty" bson:"bind_ip,omitempty"`
+	Priority           PropertyInt64       `field:"priority" json:"priority,omitempty" bson:"priority,omitempty"`
+	ReloadCmd          PropertyString      `field:"reload_cmd" json:"reload_cmd,omitempty" bson:"reload_cmd,omitempty"`
+	ProcessName        PropertyString      `field:"bk_process_name" json:"bk_process_name,omitempty" bson:"bk_process_name,omitempty"`
+	Port               PropertyPort        `field:"port" json:"port,omitempty" bson:"port,omitempty"`
+	PidFile            PropertyString      `field:"pid_file" json:"pid_file,omitempty" bson:"pid_file,omitempty"`
+	AutoStart          PropertyBool        `field:"auto_start" json:"auto_start,omitempty" bson:"auto_start,omitempty"`
+	AutoTimeGapSeconds PropertyInt64       `field:"auto_time_gap" json:"auto_time_gap,omitempty" bson:"auto_time_gap,omitempty"`
+	StartCmd           PropertyString      `field:"start_cmd" json:"start_cmd,omitempty" bson:"start_cmd,omitempty"`
+	FuncID             PropertyInt64String `field:"bk_func_id" json:"bk_func_id,omitempty" bson:"bk_func_id,omitempty"`
+	User               PropertyString      `field:"user" json:"user,omitempty" bson:"user,omitempty"`
+	TimeoutSeconds     PropertyInt64       `field:"timeout" json:"timeout,omitempty" bson:"timeout,omitempty"`
+	Protocol           PropertyProtocol    `field:"protocol" json:"protocol,omitempty" bson:"protocol,omitempty"`
+	Description        PropertyString      `field:"description" json:"description,omitempty" bson:"description,omitempty"`
 }
 
 func (pt *ProcessProperty) Validate() (field string, err error) {
@@ -564,6 +565,27 @@ type PropertyInt64 struct {
 }
 
 func (ti *PropertyInt64) Validate() error {
+	return nil
+}
+
+// PropertyInt64String is a string field that parse into int64
+type PropertyInt64String struct {
+	Value *string `field:"value" json:"value" bson:"value"`
+
+	// AsDefaultValue records the relations between process instance's property and
+	// whether it's used as a default value, the empty value can also be a default value.
+	// If the property's value is used as a default value, then this property
+	// can not be changed in all the process instance's created by this process
+	// template. or, it can only be changed to this default value.
+	AsDefaultValue *bool `field:"as_default_value" json:"as_default_value" bson:"as_default_value"`
+}
+
+func (ti *PropertyInt64String) Validate() error {
+	if ti.Value != nil {
+		if _, err := strconv.ParseInt(*ti.Value, 10, 64); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
