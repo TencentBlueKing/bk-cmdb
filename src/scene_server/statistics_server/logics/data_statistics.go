8a616626f6e2afaf0fbf1b55d86c8476aff69dd0
vlon <790004707@qq.com>
2019-05-22 14:29:16 +0800

    feature: statistical report first commit
    Former-commit-id: 91ae3cf2f1078aa8a5bc8268bbe3714fc6a69cad

--- src/scene_server/statistics_server/logics/data_statistics.go
+++ src/scene_server/statistics_server/logics/data_statistics.go
@@ -13,19 +13,13 @@
 package logics
 
 import (
-	"context"
-	"encoding/json"
-	"gopkg.in/redis.v5"
-	"net/http"
-	"time"
-
 	"configcenter/src/auth/extensions"
 	"configcenter/src/common/backbone"
-	"configcenter/src/common/blog"
 	"configcenter/src/common/errors"
 	"configcenter/src/common/language"
-	"configcenter/src/common/metadata"
-	"configcenter/src/scene_server/statistics_server/service"
+	"configcenter/src/common/util"
+	"gopkg.in/redis.v5"
+	"net/http"
 )
 
 type Logics struct {
@@ -40,92 +34,133 @@ type Logics struct {
 	AuthManager *extensions.AuthManager
 }
 
-func (lgc *Logics) TimerUpdateData(ctx context.Context) {
-	go func() {
-		timer := time.NewTicker(12 * time.Hour)
-		for range timer.C {
-			lgc.HostDataStatistics(ctx)
-			lgc.InstDataStatistics(ctx)
-		}
-	}()
-}
-
-func (lgc *Logics) HostDataStatistics(ctx context.Context) {
-	exist, err := lgc.cache.Exists(service.HostInfoStatistics).Result()
-	if err != nil {
-		blog.Errorf("check redis key exists fail, err: %v", err)
-	}
-
-	if !exist {
-		if err := lgc.InitHostCountData(ctx); err != nil {
-			blog.Errorf("init host statistics_server data fail, er: %v", err)
-		}
-		return
-	}
-
-	hostInfoStatistics, err := lgc.cache.LRange(service.HostInfoStatistics, 0, -1).Result()
-	if err != nil {
-		blog.Errorf("get host data from redis fail, error: %v", err)
-		return
-	}
-
-	if len(hostInfoStatistics) == 0 {
-		if err := lgc.InitHostCountData(ctx); err != nil {
-			blog.Errorf("init host statistics_server data fail, er: %v", err)
-		}
-		return
-	}
-
-	if err := lgc.UpdateHostCountData(ctx, hostInfoStatistics); err != nil {
-		blog.Errorf("update host statistics_server data fail, er: %v", err)
-	}
-
-	return
-
-}
-
-func (lgc *Logics) InitHostCountData(ctx context.Context) error {
-	lgc.CoreAPI.TopoServer().Instance().SearchApp()
-}
-
-func (lgc *Logics) UpdateHostCountData(ctx context.Context, hostInfoStatistics []string) error {
-	item := hostInfoStatistics[1]
-	hostData := new(metadata.HostStatisticsData)
-	if err := json.Unmarshal([]byte(item), hostData); err != nil {
-		blog.Errorf("unmarshal host data fail, err: %v", err)
-	}
-
-	countFields := hostData.StatisticsFields
-	for _, field := range countFields {
-
+// NewLogics get logic handle
+func NewLogics(b *backbone.Engine, header http.Header, cache *redis.Client, authManager *extensions.AuthManager) *Logics {
+	lang := util.GetLanguage(header)
+	return &Logics{
+		Engine:      b,
+		header:      header,
+		rid:         util.GetHTTPCCRequestID(header),
+		ccErr:       b.CCErr.CreateDefaultCCErrorIf(lang),
+		ccLang:      b.Language.CreateDefaultCCLanguageIf(lang),
+		user:        util.GetUser(header),
+		ownerID:     util.GetOwnerID(header),
+		cache:       cache,
+		AuthManager: authManager,
 	}
 }
 
-func (lgc *Logics) InstDataStatistics(ctx context.Context) {
-	instInfoStatistics, err := lgc.cache.LRange(service.InstInfoStatistics, 0, -1).Result()
-	if err != nil {
-		blog.Errorf("get host data from redis fail, error: %v", err)
-		return
-	}
-
-	if len(instInfoStatistics) == 0 {
-		if err := lgc.InitInsTCountData(ctx); err != nil {
-			blog.Errorf("init host statistics_server data fail, er: %v", err)
-		}
-		return
-	}
-
-	if err := lgc.UpdateInstCountData(ctx, instInfoStatistics); err != nil {
-		blog.Errorf("update host statistics_server data fail, er: %v", err)
-	}
-
-	return
-}
-
-func (lgc *Logics) InitInsTCountData(ctx context.Context) error {
-
-}
-
-func (lgc *Logics) UpdateInstCountData(ctx context.Context, instInfoStatistics []string) error {
-
-}
+//func (lgc *Logics) TimerUpdateData(ctx context.Context) {
+//	go func() {
+//		timer := time.NewTicker(12 * time.Hour)
+//		for range timer.C {
+//			lgc.HostDataStatistics(ctx)
+//			lgc.InstDataStatistics(ctx)
+//			lgc.RecordHostCountBizLevel(ctx)
+//		}
+//	}()
+//}
+//
+//func (lgc *Logics) RecordHostCountBizLevel(ctx context.Context) {
+//	exist, err := lgc.cache.Exists(types.HostCountBizLevel).Result()
+//	if err != nil {
+//		blog.Errorf("redis key not exists, err: %v", err)
+//	}
+//
+//	if !exist {
+//		return
+//	}
+//
+//	hostInfoStatistics, err := lgc.cache.LRange(types.HostCountBizLevel, 0, -1).Result()
+//	if err != nil {
+//		blog.Errorf("get host count from redis fail, error: %v", err)
+//		return
+//	}
+//
+//	if len(hostInfoStatistics) == 0 {
+//		if err := lgc.InitHostCountData(ctx); err != nil {
+//			blog.Errorf("init host statistics_server data fail, er: %v", err)
+//		}
+//		return
+//	}
+//}
+//
+//func (lgc *Logics) HostDataStatistics(ctx context.Context) {
+//	exist, err := lgc.cache.Exists(types.HostInfoStatistics).Result()
+//	if err != nil {
+//		blog.Errorf("check redis key exists fail, err: %v", err)
+//	}
+//
+//	if !exist {
+//		if err := lgc.InitHostCountData(ctx); err != nil {
+//			blog.Errorf("init host statistics_server data fail, er: %v", err)
+//		}
+//		return
+//	}
+//
+//	hostInfoStatistics, err := lgc.cache.LRange(types.HostInfoStatistics, 0, -1).Result()
+//	if err != nil {
+//		blog.Errorf("get host data from redis fail, error: %v", err)
+//		return
+//	}
+//
+//	if len(hostInfoStatistics) == 0 {
+//		if err := lgc.InitHostCountData(ctx); err != nil {
+//			blog.Errorf("init host statistics_server data fail, er: %v", err)
+//		}
+//		return
+//	}
+//
+//	if err := lgc.UpdateHostCountData(ctx, hostInfoStatistics); err != nil {
+//		blog.Errorf("update host statistics_server data fail, er: %v", err)
+//	}
+//
+//	return
+//
+//}
+//
+//func (lgc *Logics) InitHostCountData(ctx context.Context) error {
+//	lgc.CoreAPI.TopoServer().Instance().SearchApp()
+//}
+//
+//func (lgc *Logics) UpdateHostCountData(ctx context.Context, hostInfoStatistics []string) error {
+//	item := hostInfoStatistics[1]
+//	hostData := new(metadata.HostStatisticsData)
+//	if err := json.Unmarshal([]byte(item), hostData); err != nil {
+//		blog.Errorf("unmarshal host data fail, err: %v", err)
+//	}
+//
+//	countFields := hostData.StatisticsFields
+//	for _, field := range countFields {
+//
+//	}
+//}
+//
+//func (lgc *Logics) InstDataStatistics(ctx context.Context) {
+//	instInfoStatistics, err := lgc.cache.LRange(types.InstInfoStatistics, 0, -1).Result()
+//	if err != nil {
+//		blog.Errorf("get host data from redis fail, error: %v", err)
+//		return
+//	}
+//
+//	if len(instInfoStatistics) == 0 {
+//		if err := lgc.InitInsTCountData(ctx); err != nil {
+//			blog.Errorf("init host statistics_server data fail, er: %v", err)
+//		}
+//		return
+//	}
+//
+//	if err := lgc.UpdateInstCountData(ctx, instInfoStatistics); err != nil {
+//		blog.Errorf("update host statistics_server data fail, er: %v", err)
+//	}
+//
+//	return
+//}
+//
+//func (lgc *Logics) InitInsTCountData(ctx context.Context) error {
+//
+//}
+//
+//func (lgc *Logics) UpdateInstCountData(ctx context.Context, instInfoStatistics []string) error {
+//
+//}
--- src/scene_server/statistics_server/service/service.go
+++ src/scene_server/statistics_server/service/service.go
@@ -13,20 +13,61 @@
 package service
 
 import (
+	"context"
+	"gopkg.in/redis.v5"
+	"net/http"
+
+	"github.com/emicklei/go-restful"
+
 	"configcenter/src/apimachinery/discovery"
 	"configcenter/src/auth/extensions"
+	"configcenter/src/common"
 	"configcenter/src/common/backbone"
 	"configcenter/src/common/errors"
+	"configcenter/src/common/language"
+	"configcenter/src/common/metadata"
+	"configcenter/src/common/metric"
 	"configcenter/src/common/rdapi"
-	"github.com/emicklei/go-restful"
-	"gopkg.in/redis.v5"
+	"configcenter/src/common/types"
+	"configcenter/src/common/util"
+	"configcenter/src/scene_server/statistics_server/logics"
+	"configcenter/src/storage/dal"
 )
 
 type Service struct {
 	*backbone.Engine
 	disc        discovery.DiscoveryInterface
 	CacheDB     *redis.Client
+	db          dal.RDB
 	AuthManager *extensions.AuthManager
+}
+
+type srvComm struct {
+	header        http.Header
+	rid           string
+	ccErr         errors.DefaultCCErrorIf
+	ccLang        language.DefaultCCLanguageIf
+	ctx           context.Context
+	ctxCancelFunc context.CancelFunc
+	user          string
+	ownerID       string
+	lgc           *logics.Logics
+}
+
+func (s *Service) newSrvComm(header http.Header) *srvComm {
+	lang := util.GetLanguage(header)
+	ctx, cancel := s.Engine.CCCtx.WithCancel()
+	return &srvComm{
+		header:        header,
+		rid:           util.GetHTTPCCRequestID(header),
+		ccErr:         s.CCErr.CreateDefaultCCErrorIf(lang),
+		ccLang:        s.Language.CreateDefaultCCLanguageIf(lang),
+		ctx:           ctx,
+		ctxCancelFunc: cancel,
+		user:          util.GetUser(header),
+		ownerID:       util.GetOwnerID(header),
+		lgc:           logics.NewLogics(s.Engine, header, s.CacheDB, s.AuthManager),
+	}
 }
 
 func (s *Service) WebService() *restful.WebService {
@@ -36,10 +77,56 @@ func (s *Service) WebService() *restful.WebService {
 	}
 	ws.Path("/statistics/{version}").Filter(rdapi.AllGlobalFilter(getErrFunc)).Produces(restful.MIME_JSON)
 
-	ws.Route(ws.DELETE("/statistics/batch").To(s.HostStatisticsByOS))
-	ws.Route(ws.GET("/statistics/{bk_supplier_account}/{bk_host_id}").To(s.HostStatisticsByBiz))
-	ws.Route(ws.GET("/statistics/snapshot/{bk_host_id}").To(s.HostStatisticsByRegion))
-	ws.Route(ws.POST("/statistics/add").To(s.HostQuantityChangeBizLevel))
+	ws.Route(ws.DELETE("/statistics/add").To(s.AddStatisticalChart))
+	ws.Route(ws.GET("/statistics/delete/{bk_stic_id}").To(s.DeleteStatisticalChart))
+	ws.Route(ws.GET("/statistics/charts/search").To(s.SearchStatisticalCharts))
+	ws.Route(ws.POST("/statistics/update").To(s.UpdateStatisticalChart))
+	ws.Route(ws.POST("/statistics/biz/module/host/amount").To(s.GetBizModuleHostAmount))
+	ws.Route(ws.POST("/statistics/chart/data").To(s.GetChartData))
+
+	ws.Route(ws.GET("/healthz").To(s.Healthz))
 
 	return ws
+}
+
+func (s *Service) Healthz(req *restful.Request, resp *restful.Response) {
+	meta := metric.HealthMeta{IsHealthy: true}
+
+	// zk health status
+	zkItem := metric.HealthItem{IsHealthy: true, Name: types.CCFunctionalityServicediscover}
+	if err := s.Engine.Ping(); err != nil {
+		zkItem.IsHealthy = false
+		zkItem.Message = err.Error()
+	}
+	meta.Items = append(meta.Items, zkItem)
+
+	// mongodb
+	meta.Items = append(meta.Items, metric.NewHealthItem(types.CCFunctionalityServicediscover, s.db.Ping()))
+
+	// redis
+	meta.Items = append(meta.Items, metric.NewHealthItem(types.CCFunctionalityServicediscover, s.CacheDB.Ping().Err()))
+
+	for _, item := range meta.Items {
+		if item.IsHealthy == false {
+			meta.IsHealthy = false
+			meta.Message = "host server is unhealthy"
+			break
+		}
+	}
+
+	info := metric.HealthInfo{
+		Module:     types.CC_MODULE_REPORTSTATISTIC,
+		HealthMeta: meta,
+		AtTime:     metadata.Now(),
+	}
+
+	answer := metric.HealthResponse{
+		Code:    common.CCSuccess,
+		Data:    info,
+		OK:      meta.IsHealthy,
+		Result:  meta.IsHealthy,
+		Message: meta.Message,
+	}
+	resp.Header().Set("Content-Type", "application/json")
+	resp.WriteEntity(answer)
 }
