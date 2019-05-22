76ee3fc355b97c924d52345f7919a3bfff46b628
vlon <790004707@qq.com>
2019-05-22 14:30:01 +0800

    feature： statistical ui
    Former-commit-id: 22d308250c3b681788082a3440d05bec800a445a

--- src/ui/src/views/statistical-report/index.vue
+++ src/ui/src/views/statistical-report/index.vue
@@ -80,14 +80,14 @@
                     x: ['giraffes', 'orangutans', 'monkeys'],
                     y: [20, 14, 23],
                     name: 'SF Zoo',
-                    type: 'bar'
+                    type: 'line'
                 }
 
                 const trace2 = {
                     x: ['giraffes', 'orangutans', 'monkeys'],
                     y: [12, 18, 29],
                     name: 'LA Zoo',
-                    type: 'bar'
+                    type: 'line'
                 }
 
                 const data = [trace1, trace2]
