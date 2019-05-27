d2fea61846e3920cef998549c850ddc1b51048bb
joe511230 <itsfantacy@hotmail.com>
2019-05-27 21:09:35 +0800

    temp:ui: tree
    Former-commit-id: d301da0c822c1953021f8e495b5e4ad8caa999e6

--- src/ui/src/views/tree/index.vue
+++ src/ui/src/views/tree/index.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="tree-layout">
-        <tree ref="tree" :options="{
-            idKey: idGenerator,
-            nameKey: 'bk_inst_name',
-            childrenKey: 'child'
-        }"></tree>
+        <bk-button @click="addNode">添加</bk-button>
+        <bk-button @click="deleteNode">删除</bk-button>
+        <tree ref="tree"
+            :options="{
+                idKey: idGenerator,
+                nameKey: 'bk_inst_name',
+                childrenKey: 'child'
+            }"
+            :node-icon="getNodeIcon"
+            :default-expand-node="[8, 20]"
+            show-checkbox
+            expand-icon="icon-cc-rect-sub"
+            collapse-icon="icon-cc-rect-add">
+        </tree>
     </div>
 </template>
 
@@ -16,7 +25,8 @@
         },
         data () {
             return {
-                data: []
+                data: [],
+                id: 0
             }
         },
         created () {
@@ -24,13 +34,32 @@
         },
         methods: {
             async getData () {
-                const data = await this.$http.post('find/topoinst/biz/2?level=-1')
-                this.data = data
-                this.$refs.tree.setData(data)
+                const data = await this.$http.post('find/topoinst/biz/5?level=-1')
+                // this.data = data
+                // this.$refs.tree.setData(data)
+                const repeat = []
+                for (let i = 0; i < 300; i++) {
+                    repeat.push(data[0])
+                }
+                this.$refs.tree.setData(repeat)
             },
             idGenerator (node) {
-                return `${node.bk_obj_id}_${node.bk_inst_id}`
-            }
+                return this.id++
+            },
+            getNodeIcon (data) {
+                return 'icon-cc-host'
+            },
+            addNode () {
+                this.$refs.tree.addNode({
+                    bk_inst_id: Math.random(),
+                    bk_inst_name: Math.random(),
+                    bk_obj_id: 'fuck',
+                    bk_obj_name: 'fuck',
+                    child: [],
+                    default: 0
+                }, 8)
+            },
+            deleteNode () {}
         }
     }
 </script>
