f5a1f86887b400e782bf2569065c73f713736e81
hoffermei(梅海峰) <hoffer@vip.qq.com>
2019-05-29 22:47:25 +0800

    创建服务实例接口stuck #2458
    Former-commit-id: 3188cfe3731a5a1a39c8cb9b12e692d56894f2f3

--- src/scene_server/proc_server/app/server.go
+++ src/scene_server/proc_server/app/server.go
@@ -27,6 +27,7 @@ import (
 	"configcenter/src/common/types"
 	"configcenter/src/common/version"
 	"configcenter/src/scene_server/proc_server/app/options"
+	"configcenter/src/scene_server/proc_server/logics"
 	"configcenter/src/scene_server/proc_server/service"
 	"configcenter/src/storage/dal/redis"
 	"configcenter/src/thirdpartyclient/esbserver"
@@ -91,6 +92,9 @@ func Run(ctx context.Context, op *options.ServerOption) error {
 	procSvr.Engine = engine
 	procSvr.EsbServ = esbSrv
 	procSvr.Cache = cacheDB
+	procSvr.Logic = &logics.Logic{
+		Engine: procSvr.Engine,
+	}
 	go procSvr.InitFunc()
 
 	webservice := procSvr.WebService()
