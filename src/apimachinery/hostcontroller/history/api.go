ab169dd125af2cbc135c1b2e16c68005c0f90b47
hoffermei(梅海峰) <hoffer@vip.qq.com>
2019-06-24 21:38:47 +0800

    hostcontroller中hosthistory没有用到，直接移除  #2602

--- src/apimachinery/hostcontroller/history/api.go
+++ src/apimachinery/hostcontroller/history/api.go
@@ -11,39 +11,3 @@
  */
 
 package history
-
-import (
-	"context"
-	"fmt"
-	"net/http"
-
-	"configcenter/src/common/metadata"
-)
-
-func (hi *history) AddHistory(ctx context.Context, user string, h http.Header, dat *metadata.HistoryContent) (resp *metadata.AddHistoryResult, err error) {
-	resp = new(metadata.AddHistoryResult)
-	subPath := fmt.Sprintf("/history/%s", user)
-
-	err = hi.client.Post().
-		WithContext(ctx).
-		Body(dat).
-		SubResource(subPath).
-		WithHeaders(h).
-		Do().
-		Into(resp)
-	return
-}
-
-func (hi *history) GetHistorys(ctx context.Context, user string, start string, limit string, h http.Header) (resp *metadata.GetHistoryResult, err error) {
-	resp = new(metadata.GetHistoryResult)
-	subPath := fmt.Sprintf("/history/%s/%s/%s", user, start, limit)
-
-	err = hi.client.Get().
-		WithContext(ctx).
-		Body(nil).
-		SubResource(subPath).
-		WithHeaders(h).
-		Do().
-		Into(resp)
-	return
-}
