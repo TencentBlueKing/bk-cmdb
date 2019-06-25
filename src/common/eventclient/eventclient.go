fa81df11227accfa3fb0de82a99893bfb626f95c
wusendong <wusendong@gmail.com>
2019-06-25 15:33:12 +0800

    fix compare
    Former-commit-id: 0bf13c8d5ba3969b22c9727c7b797b87a6bfc1f3

--- src/common/eventclient/eventclient.go
+++ src/common/eventclient/eventclient.go
@@ -69,21 +69,21 @@ func (c *ClientViaRedis) Push(ctx context.Context, events ...*metadata.EventInst
 	c.queueLock.Lock()
 	for i := range events {
 		if events[i] != nil {
-			// var allEqual = true
-			// for _, data := range events[i].Data {
-			// 	equal, err := instEqual(data)
-			// 	if err != nil {
-			// 		return fmt.Errorf("[event] compare failed: %v, source data is %#v", err, data)
-			// 	}
-			// 	if !equal {
-			// 		allEqual = false
-			// 		break
-			// 	}
-			// }
-			//
-			// if allEqual {
-			// 	continue
-			// }
+			var allEqual = true
+			for _, data := range events[i].Data {
+				equal, err := instEqual(data)
+				if err != nil {
+					return fmt.Errorf("[event] compare failed: %v, source data is %#v", err, data)
+				}
+				if !equal {
+					allEqual = false
+					break
+				}
+			}
+
+			if allEqual {
+				continue
+			}
 
 			eventID, err := c.rdb.NextSequence(ctx, common.EventCacheEventIDKey)
 			if err != nil {
@@ -180,8 +180,6 @@ func (c *ClientViaRedis) pushToRedis(event *eventtmp) error {
 // instEqual Determine whether the data is consistent before and after the change
 func instEqual(data metadata.EventData) (bool, error) {
 	switch {
-	case data.PreData == data.CurData:
-		return true, nil
 	case data.PreData == nil && data.CurData != nil:
 		return false, nil
 	case data.CurData == nil && data.PreData != nil:
