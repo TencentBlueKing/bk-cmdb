5de9f58843bb81c2c81eaace137d0454df99257b
rentiansheng <rentiansheng@163.com>
2019-06-26 10:16:46 +0800

    depend:support time type, struct value support null   issue #2615
    Former-commit-id: bd47cd8673be4396c8ee9becb932c57712fedeb7

--- vendor/github.com/mongodb/mongo-go-driver/bson/bsoncodec/struct_codec.go
+++ vendor/github.com/mongodb/mongo-go-driver/bson/bsoncodec/struct_codec.go
@@ -199,6 +199,12 @@ func (sc *StructCodec) DecodeValue(r DecodeContext, vr bsonrw.ValueReader, val r
 		if fd.decoder == nil {
 			return ErrNoDecoder{Type: field.Elem().Type()}
 		}
+		if vr.Type() == bsontype.Null {
+			if err := vr.ReadNull(); err != nil {
+				return err
+			}
+			continue
+		}
 
 		if decoder, ok := fd.decoder.(ValueDecoder); ok {
 			err = decoder.DecodeValue(dctx, vr, field.Elem())
