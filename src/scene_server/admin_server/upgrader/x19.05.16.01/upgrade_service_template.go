77890a918339df812616792a261c57c8e6e537d8
hoffermei(梅海峰) <hoffer@vip.qq.com>
2019-07-05 17:27:11 +0800

    迁移进程实例数历史数据的绑定ip字段值 #2697
    Former-commit-id: dd559037fcf262ef7ad0deab6cfaa5fff5faebc3

--- src/scene_server/admin_server/upgrader/x19.05.16.01/upgrade_service_template.go
+++ src/scene_server/admin_server/upgrader/x19.05.16.01/upgrade_service_template.go
@@ -183,6 +183,12 @@ func upgradeServiceTemplate(ctx context.Context, db dal.RDB, conf *upgrader.Conf
 						inst.Metadata = metadata.NewMetaDataFromBusinessID(strconv.FormatInt(bizID, 10))
 						inst.CreateTime = time.Now()
 						inst.LastTime = time.Now()
+						if inst.BindIP != nil {
+							tplBindIP := metadata.SocketBindType(*inst.BindIP)
+							*inst.BindIP = tplBindIP.IP()
+						} else {
+							inst.BindIP = new(string)
+						}
 						blog.InfoJSON("procInst: %s", inst)
 						if err = db.Table(common.BKTableNameBaseProcess).Insert(ctx, inst); err != nil {
 							return err
