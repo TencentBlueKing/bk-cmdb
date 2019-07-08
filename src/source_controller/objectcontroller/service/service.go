aa7b332124cc79ce554b2fcab8a63e6182b89c5c
hoffermei(梅海峰) <hoffer@vip.qq.com>
2019-07-08 19:45:20 +0800

    migrate and remove objectcontroller instance delete interface #2706
    Former-commit-id: 6f521b882d117ada9334a0593e941080c3f8037b

--- src/source_controller/objectcontroller/service/service.go
+++ src/source_controller/objectcontroller/service/service.go
@@ -49,7 +49,6 @@ func (s *Service) WebService() *restful.Container {
 
 	api.Route(api.POST("/identifier/{obj_type}/search").To(s.SearchIdentifier))
 
-	api.Route(api.DELETE("/insts/{obj_type}").To(s.DeleteInstObject))
 	api.Route(api.PUT("/insts/{obj_type}").To(s.UpdateInstObject))
 
 	api.Route(api.POST("/meta/objectatts").To(s.SelectObjectAttWithParams))
