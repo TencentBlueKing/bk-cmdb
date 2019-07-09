63f681087be0f4f90d92ac0af845d8e33dd262c7
Daryl0819 <zhdgmax@qq.com>
2019-07-09 19:38:08 +0800

    feature:ui: 添加标签
    Former-commit-id: c52f671d64cd791e6c1f37977968efbcc3333cc5

---
+++ src/ui/src/views/business-topology/children/batch-edit-label.vue
@@ -0,0 +1,134 @@
+<template>
+    <div class="batch-edit-label">
+        <div class="exisiting-label">
+            <div class="title">
+                {{$t("BusinessTopology['已有标签']")}}
+                <span>{{$t("BusinessTopology['（列表为所选实例标签集合）']")}}</span>
+            </div>
+            <div class="label-set">
+                <span class="label-item">
+                    数据库：mysql
+                    <i class="bk-icon icon-close-circle-shape"></i>
+                </span>
+                <span class="label-item">
+                    数据库：mysql
+                    <i class="bk-icon icon-close-circle-shape"></i>
+                </span>
+                <span class="label-item">
+                    数据库：mysql
+                    <i class="bk-icon icon-close-circle-shape"></i>
+                </span>
+                <span class="label-item">
+                    数据库：mysql
+                    <i class="bk-icon icon-close-circle-shape"></i>
+                </span>
+                <span class="label-item">
+                    数据库：mysql
+                    <i class="bk-icon icon-close-circle-shape"></i>
+                </span>
+                <span class="label-item">
+                    数据库：mysql
+                    <i class="bk-icon icon-close-circle-shape"></i>
+                </span>
+                <span class="label-item">
+                    数据库：mysql
+                    <i class="bk-icon icon-close-circle-shape"></i>
+                </span>
+                <span class="label-item">
+                    数据库：mysql
+                    <i class="bk-icon icon-close-circle-shape"></i>
+                </span>
+                <span class="label-item">
+                    数据库：mysql
+                    <i class="bk-icon icon-close-circle-shape"></i>
+                </span>
+                <span class="label-item">
+                    数据库：mysql
+                    <i class="bk-icon icon-close-circle-shape"></i>
+                </span>
+            </div>
+        </div>
+        <div class="batch-add">
+            <div class="title">{{$t("BusinessTopology['批量添加标签']")}}</div>
+            <slot name="batch-add-label"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            exisitingLabel: {
+                type: Array,
+                default: () => []
+            }
+        },
+        data () {
+            return {
+                localExisitingLabel: this.exisitingLabel
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .batch-edit-label {
+        height: 321px;
+        padding: 6px 16px 20px 26px;
+        .title {
+            color: #63656e;
+            font-size: 14px;
+            padding-bottom: 8px;
+            span {
+                color: #979ba5;
+                font-size: 12px;
+            }
+        }
+        .exisiting-label {
+            padding-bottom: 12px;
+            .label-set {
+                &::-webkit-scrollbar, &::-webkit-scrollbar-thumb {
+                    display: none;
+                }
+                &:hover {
+                    &::-webkit-scrollbar, &::-webkit-scrollbar-thumb {
+                        display: block;
+                    }
+                }
+                @include scrollbar-y;
+                height: 84px;
+                padding: 12px 0 2px 12px;
+                border-radius: 2px;
+                border: 1px solid #c4c6cc;
+                .label-item {
+                    display: inline-block;
+                    height: 24px;
+                    line-height: 24px;
+                    font-size: 12px;
+                    padding: 0 7px;
+                    margin: 0 10px 10px 0;
+                    color: #63656e;
+                    background-color: #f0f1f5;
+                    border-radius: 12px;
+                }
+                .bk-icon {
+                    color: #c4c6cc;
+                    margin-left: 4px;
+                    cursor: pointer;
+                    &:hover {
+                        color: #979ba5;
+                    }
+                }
+            }
+        }
+        .edit-label {
+            padding: 0;
+            /deep/ .scrollbar-box {
+                height: 135px !important;
+            }
+        }
+    }
+</style>
+
+<style lang="scss">
+</style>
