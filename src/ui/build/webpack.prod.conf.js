80d50f146b5a778afe0baf07851a837f3f40cdcb
peibiaoyang <itsfantacy@hotmail.com>
2019-07-29 17:13:41 +0800

    fix:ui: 体验优化

--- src/ui/build/webpack.prod.conf.js
+++ src/ui/build/webpack.prod.conf.js
@@ -58,11 +58,11 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
-    new OptimizeCSSPlugin({
-      cssProcessorOptions: config.build.productionSourceMap
-        ? { safe: true, map: { inline: false } }
-        : { safe: true }
-    }),
+    // new OptimizeCSSPlugin({
+    //   cssProcessorOptions: config.build.productionSourceMap
+    //     ? { safe: true, map: { inline: false } }
+    //     : { safe: true }
+    // }),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
     // see https://github.com/ampedandwired/html-webpack-plugin
