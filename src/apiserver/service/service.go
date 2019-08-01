dcbd6bfa13e9c2a6415f176be85e1f69b6808473
breezeli <breezeli@tencent.com>
2019-08-01 16:56:50 +0800

    feature: add resource convert api, issue #2839

--- src/apiserver/service/service.go
+++ src/apiserver/service/service.go
@@ -72,9 +72,10 @@ func (s *service) WebServices(auth authcenter.AuthConfig) []*restful.WebService 
 		ws.Filter(s.authFilter(getErrFun))
 	}
 	ws.Route(ws.POST("/auth/verify").To(s.AuthVerify))
-	ws.Route(ws.GET("/auth/business-list").To(s.GetAnyAuthorizedAppList))
-	ws.Route(ws.GET("/auth/admin-entrance").To(s.GetAdminEntrance))
+	ws.Route(ws.GET("/auth/business_list").To(s.GetAnyAuthorizedAppList))
+	ws.Route(ws.GET("/auth/admin_entrance").To(s.GetAdminEntrance))
 	ws.Route(ws.POST("/auth/skip_url").To(s.GetUserNoAuthSkipUrl))
+	ws.Route(ws.POST("/auth/convert").To(s.GetCmdbConvertResources))
 	ws.Route(ws.GET("{.*}").Filter(s.URLFilterChan).To(s.Get))
 	ws.Route(ws.POST("{.*}").Filter(s.URLFilterChan).To(s.Post))
 	ws.Route(ws.PUT("{.*}").Filter(s.URLFilterChan).To(s.Put))
@@ -103,16 +104,21 @@ func (s *service) authFilter(errFunc func() errors.CCErrorIf) func(req *restful.
 			return
 		}
 
-		if path == "/api/v3/auth/business-list" {
+		if path == "/api/v3/auth/business_list" {
 			fchain.ProcessFilter(req, resp)
 			return
 		}
-		if path == "/api/v3/auth/admin-entrance" {
+		if path == "/api/v3/auth/admin_entrance" {
 			fchain.ProcessFilter(req, resp)
 			return
 		}
 
 		if path == "/api/v3/auth/skip_url" {
+			fchain.ProcessFilter(req, resp)
+			return
+		}
+
+		if path == "/api/v3/auth/convert" {
 			fchain.ProcessFilter(req, resp)
 			return
 		}
