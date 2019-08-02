57ba23ee583cc1f48fc06fb19b2f78b2887ca3b6
hoffermei(梅海峰) <hoffer@vip.qq.com>
2019-08-02 20:32:10 +0800

    实现一套统一查询语言 #2857

---
+++ src/common/querybuilder/parser.go
@@ -0,0 +1,50 @@
+/*
+ * Tencent is pleased to support the open source community by making 蓝鲸 available.
+ * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package querybuilder
+
+import (
+	"fmt"
+
+	"configcenter/src/common/mapstr"
+)
+
+type RuleGroupData struct {
+	Condition Condition                `json:"condition"`
+	Rules     []map[string]interface{} `json:"rules"`
+}
+
+func RuleParser(data map[string]interface{}) (queryFilter Rule, errKey string, err error) {
+	if _, ok := data["condition"]; ok == true {
+		ruleGroupData := &RuleGroupData{}
+		if err := mapstr.SetValueToStructByTags(ruleGroupData, data); err != nil {
+			return nil, "", fmt.Errorf("decode to rule group struct failed, err: %+v", err)
+		}
+		queryFilter := CombinedRule{
+			Condition: ruleGroupData.Condition,
+			Rules:     make([]Rule, 0),
+		}
+		for idx, item := range ruleGroupData.Rules {
+			qf, errKey, err := RuleParser(item)
+			if err != nil {
+				return nil, fmt.Sprintf("rules[%d].%s", idx, errKey), err
+			}
+			queryFilter.Rules = append(queryFilter.Rules, qf)
+		}
+	} else if _, ok := data["operator"]; ok == true {
+		rule := AtomRule{}
+		if err := mapstr.SetValueToStructByTags(&rule, data); err != nil {
+			return nil, "", fmt.Errorf("decode to rule struct failed, err: %+v", err)
+		}
+	}
+	return nil, "", fmt.Errorf("no query filter found")
+}
