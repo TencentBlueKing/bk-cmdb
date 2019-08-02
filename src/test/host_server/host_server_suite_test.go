f448f94660984973071e1d824ae70daf04896f5b
wcy00000000000000 <2269766985@qq.com>
2019-08-02 19:54:45 +0800

    添加hostserver的部分用例

--- src/test/host_server/host_server_suite_test.go
+++ src/test/host_server/host_server_suite_test.go
@@ -2,7 +2,6 @@ package host_server_test
 
 import (
 	"configcenter/src/test"
-	"context"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
@@ -11,33 +10,17 @@ import (
 
 var header = test.GetHeader()
 var hostServerClient = test.GetClientSet().HostServer()
+var apiServerClient = test.GetClientSet().ApiServer()
 
 func TestHostServer(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "HostServer Suite")
 }
 
-var _ = Describe("user custom test", func() {
-	It("search default user custom", func() {
-		rsp, err := hostServerClient.GetUserCustom(context.Background(), header)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(rsp.Result).To(Equal(true))
-	})
+var _ = BeforeSuite(func() {
+	test.ClearDatabase()
+})
 
-	It("save user custom", func() {
-		input := map[string]interface{}{
-			"index_v2_classify_navigation": []string{"bk_middleware"},
-		}
-		rsp, err := hostServerClient.SaveUserCustom(context.Background(), header, input)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(rsp.Result).To(Equal(true))
-	})
-
-	It("search user custom", func() {
-		rsp, err := hostServerClient.GetUserCustom(context.Background(), header)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(rsp.Result).To(Equal(true))
-		data := rsp.Data.(map[string][]string)
-		Expect(data["index_v2_classify_navigation"][0]).To(Equal("bk_middleware"))
-	})
+var _ = AfterSuite(func() {
+	test.ClearDatabase()
 })
