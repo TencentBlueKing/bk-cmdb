f27edc4829fc478f6bd7805fec94715efc320f10
vlon <790004707@qq.com>
2019-08-06 14:47:47 +0800

    fix: 数量是0的情况，也显示模型名

--- src/common/metadata/operation.go
+++ src/common/metadata/operation.go
@@ -144,3 +144,8 @@ type ChartClassification struct {
 	Inst []ChartConfig `json:"inst"`
 	Nav  []ChartConfig `json:"nav"`
 }
+
+type ObjectIDName struct {
+	ObjectID   string `json:"bk_object_id"`
+	ObjectName string `json:"bk_object_name"`
+}
