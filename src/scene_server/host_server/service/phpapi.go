ce81b3511bb47412887b68870fee11d53a188259
rentiansheng <rentiansheng@163.com>
2019-08-07 20:22:19 +0800

    fix: create plat error. return duplicate json issue #2898
    Former-commit-id: 558e0d5c82748a417c31c60a8be6163d85fb22ca

--- src/scene_server/host_server/service/phpapi.go
+++ src/scene_server/host_server/service/phpapi.go
@@ -1052,7 +1052,7 @@ func (s *Service) CreatePlat(req *restful.Request, resp *restful.Response) {
 	if false == res.Result {
 		blog.Errorf("GetPlat error.err code:%d,err msg:%s,input:%+v,rid:%s", res.Code, res.ErrMsg, input, srvData.rid)
 		resp.WriteHeaderAndJson(http.StatusInternalServerError, res, "application/json")
-
+		return
 	}
 	resp.WriteEntity(meta.Response{
 		BaseResp: meta.SuccessBaseResp,
