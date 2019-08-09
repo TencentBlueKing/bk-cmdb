971625d28c9949e0984ebf69d79b54c2cf04004e
wcy00000000000000 <2269766985@qq.com>
2019-08-09 21:38:10 +0800

    add: proc_server test and corresponding apimachinery files

---
+++ src/apimachinery/procserver/service/template.go
@@ -0,0 +1,64 @@
+package service
+
+import (
+	"context"
+	"net/http"
+
+	"configcenter/src/common/metadata"
+)
+
+func (s *service) CreateServiceTemplate(ctx context.Context, h http.Header, data map[string]interface{}) (resp *metadata.Response, err error) {
+	resp = new(metadata.Response)
+	subPath := "/create/proc/service_template"
+
+	err = s.client.Post().
+		WithContext(ctx).
+		Body(data).
+		SubResource(subPath).
+		WithHeaders(h).
+		Do().
+		Into(resp)
+	return
+}
+
+func (s *service) DeleteServiceTemplate(ctx context.Context, h http.Header, data map[string]interface{}) (resp *metadata.Response, err error) {
+	resp = new(metadata.Response)
+	subPath := "/delete/proc/service_template"
+
+	err = s.client.Delete().
+		WithContext(ctx).
+		Body(data).
+		SubResource(subPath).
+		WithHeaders(h).
+		Do().
+		Into(resp)
+	return
+}
+
+func (s *service) SearchServiceTemplate(ctx context.Context, h http.Header, data map[string]interface{}) (resp *metadata.Response, err error) {
+	resp = new(metadata.Response)
+	subPath := "/findmany/proc/service_template"
+
+	err = s.client.Post().
+		WithContext(ctx).
+		Body(data).
+		SubResource(subPath).
+		WithHeaders(h).
+		Do().
+		Into(resp)
+	return
+}
+
+func (s *service) UpdateServiceTemplate(ctx context.Context, h http.Header, data map[string]interface{}) (resp *metadata.Response, err error) {
+	resp = new(metadata.Response)
+	subPath := "/update/proc/service_template"
+
+	err = s.client.Put().
+		WithContext(ctx).
+		Body(data).
+		SubResource(subPath).
+		WithHeaders(h).
+		Do().
+		Into(resp)
+	return
+}
