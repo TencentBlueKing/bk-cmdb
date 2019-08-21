c4ed41ce6b58defd9d744fc795b41eba00242251
Daryl0819 <zhdgmax@qq.com>
2019-08-21 12:02:43 +0800

    update:ui: 组件调整
    Former-commit-id: c2aaa725b71ab89ad15e7668ee6d1f13d58819c8

--- src/ui/src/components/ui/form/timezone.vue
+++ src/ui/src/components/ui/form/timezone.vue
@@ -56,9 +56,7 @@
             }
         },
         created () {
-            if (!this.disabled) {
-                this.selected = this.value ? this.value : 'Asia/Shanghai'
-            }
+            this.selected = this.value ? this.value : 'Asia/Shanghai'
         }
     }
 </script>
