30d3cdedfe8bcdd81c4d2665a78ebb4a202fd7fa
wcy00000000000000 <2269766985@qq.com>
2019-08-28 21:04:35 +0800

    add: validator in source_controller
    Former-commit-id: 57b84e5fedfdbc46b12eca6fcd90c5dba4c09bdf

---
+++ src/source_controller/validator/type.go
@@ -0,0 +1,65 @@
+/*
+ * Tencent is pleased to support the open source community by making 蓝鲸 available.
+ * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package validator
+
+import (
+	"context"
+
+	"configcenter/src/common/errors"
+	"configcenter/src/common/language"
+	"configcenter/src/common/metadata"
+	"configcenter/src/storage/dal"
+)
+
+// MinMaxOption option for number
+type MinMaxOption struct {
+	Min string `bson:"min" json:"min"`
+	Max string `bson:"max" json:"max"`
+}
+
+// EnumOption enum option
+type EnumOption []EnumVal
+
+// GetDefault returns EnumOption's default value
+func (opt EnumOption) GetDefault() *EnumVal {
+	for index := range opt {
+		if opt[index].IsDefault {
+			return &opt[index]
+		}
+	}
+	return nil
+}
+
+// EnumVal enum option val
+type EnumVal struct {
+	ID        string `bson:"id"           json:"id"`
+	Name      string `bson:"name"         json:"name"`
+	Type      string `bson:"type"         json:"type"`
+	IsDefault bool   `bson:"is_default"   json:"is_default"`
+}
+
+type Validator struct {
+	ownerID string
+	objID   string
+	errif   errors.DefaultCCErrorIf
+	defLang language.DefaultCCLanguageIf
+	db      dal.RDB
+	ctx     context.Context
+
+	propertys     map[string]metadata.Attribute
+	idToProperty  map[int64]metadata.Attribute
+	propertyslice []metadata.Attribute
+	require       map[string]bool
+	requirefields []string
+	shouldIgnore  map[string]bool
+}
