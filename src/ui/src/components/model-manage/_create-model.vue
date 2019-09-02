e9b93e60c35e79083d40d9665627508d6b3e4da7
Daryl0819 <zhdgmax@qq.com>
2019-09-02 17:33:21 +0800

    update:ui: 模型管理调整

--- src/ui/src/components/model-manage/_create-model.vue
+++ src/ui/src/components/model-manage/_create-model.vue
@@ -46,7 +46,7 @@
                             </bk-input>
                             <p class="form-error" :title="errors.first('modelId')">{{errors.first('modelId')}}</p>
                         </div>
-                        <i class="bk-icon icon-info-circle" v-bk-tooltips="$t('下划线，数字，英文小写的组合')"></i>
+                        <i class="icon-cc-exclamation-tips" v-bk-tooltips="$t('下划线，数字，英文小写的组合')"></i>
                     </label>
                     <label>
                         <span class="label-title">{{$t('名称')}}</span>
@@ -60,7 +60,7 @@
                             </bk-input>
                             <p class="form-error" :title="errors.first('modelName')">{{errors.first('modelName')}}</p>
                         </div>
-                        <i class="bk-icon icon-info-circle" v-bk-tooltips="$t('请填写模型名')"></i>
+                        <i class="icon-cc-exclamation-tips" v-bk-tooltips="$t('请填写模型名')"></i>
                     </label>
                 </div>
             </div>
@@ -199,7 +199,7 @@
                 text-align: center;
                 vertical-align: middle;
             }
-            .icon-info-circle {
+            .icon-cc-exclamation-tips {
                 font-size: 18px;
                 color: $cmdbBorderColor;
             }
