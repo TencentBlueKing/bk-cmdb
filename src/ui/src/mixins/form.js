36e5ea2825d0c4daaecc25658294b097cbf988de
Daryl0819 <zhdgmax@qq.com>
2019-09-04 17:06:17 +0800

    update:ui: 删除更多属性分组
    Former-commit-id: 7d9e2120986881eca87f0b9682e8006c3565b00d

--- src/ui/src/mixins/form.js
+++ src/ui/src/mixins/form.js
@@ -43,11 +43,7 @@ export default {
             metadataGroups.sort((groupA, groupB) => groupA[sortKey] - groupB[sortKey])
             const allGroups = [
                 ...publicGroups,
-                ...metadataGroups,
-                {
-                    'bk_group_id': 'none',
-                    'bk_group_name': this.$t('更多属性')
-                }
+                ...metadataGroups
             ]
             allGroups.forEach((group, index) => {
                 group['bk_group_index'] = index
@@ -67,7 +63,7 @@ export default {
         $groupedProperties () {
             return this.$sortedGroups.map(group => {
                 return this.$sortedProperties.filter(property => {
-                    const inGroup = property['bk_property_group'] === group['bk_group_id']
+                    const inGroup = (['default', 'none'].includes(property['bk_property_group']) && group['bk_group_id'] === 'default') || property['bk_property_group'] === group['bk_group_id']
                     const isAsst = ['singleasst', 'multiasst'].includes(property['bk_property_type'])
                     return inGroup && !isAsst
                 })
