bfa85826c0db8e12a6a3f63f2dad9528c05345bc
Daryl0819 <zhdgmax@qq.com>
2019-09-18 18:59:57 +0800

    feature:ui: 同步模板

--- src/ui/src/views/set-sync/sync-index.vue
+++ src/ui/src/views/set-sync/sync-index.vue
@@ -7,6 +7,10 @@
                     <i class="dot"></i>
                     {{$t('新增模块')}}
                 </span>
+                <span class="mr30">
+                    <i class="dot blue"></i>
+                    {{$t('变更模块')}}
+                </span>
                 <span>
                     <i class="dot red"></i>
                     {{$t('删除模块')}}
@@ -15,7 +19,12 @@
             <bk-checkbox class="expand-all fr" v-model="expandAll">{{$t('全部展开')}}</bk-checkbox>
         </div>
         <div class="instance-list">
-            <set-instance class="instance-item" :expand="true"></set-instance>
+            <set-instance class="instance-item"
+                v-for="(instance, index) in diffList"
+                :key="instance.bk_set_id"
+                :instance="instance"
+                :expand="index === 0">
+            </set-instance>
         </div>
         <div class="footer">
             <bk-button theme="primary" class="mr10" @click="handleConfirmSync">{{$t('确认同步')}}</bk-button>
@@ -33,18 +42,58 @@
         },
         data () {
             return {
-                expandAll: false
+                expandAll: false,
+                diffList: []
             }
         },
+        computed: {
+            bizId () {
+                return this.$store.getters['objectBiz/bizId']
+            }
+        },
+        async created () {
+            await this.getDiffData()
+        },
         methods: {
+            async getDiffData () {
+                this.diffList = await this.$store.dispatch('setSync/diffTemplateAndInstances', {
+                    bizId: this.bizId,
+                    setTemplateId: '',
+                    params: {
+                        bk_set_ids: []
+                    },
+                    config: {
+                        requestId: 'diffTemplateAndInstances'
+                    }
+                })
+            },
             handleConfirmSync () {
                 this.$bkInfo({
                     type: 'warning',
                     title: '确定同步拓扑？',
                     // subTitle: '即将同步拓扑模版【正式集群模版】，模版的拓扑结构将会更新到选中的集群实例中',
                     subTitle: '即将批量同步拓扑模版【正式集群模版】到选中的20个集群实例中，模版的拓扑结构将会更新到选中的集群实例中',
-                    extCls: 'set-confirm-sync'
-                    // confirmFn: async () => {}
+                    extCls: 'set-confirm-sync',
+                    confirmFn: async () => {
+                        try {
+                            this.$router.push({
+                                name: 'viewSync'
+                            })
+                            await this.$store.dispatch('setSync/syncTemplateToInstances', {
+                                bizId: this.bizId,
+                                setTemplateId: '',
+                                params: {
+                                    bk_set_ids: []
+                                },
+                                config: {
+                                    requestId: 'syncTemplateToInstances'
+                                }
+                            })
+                            this.$success(this.$t('同步成功'))
+                        } catch (e) {
+                            console.error(e)
+                        }
+                    }
                 })
             }
         }
@@ -69,6 +118,9 @@
             margin-right: 2px;
             &.red {
                 background-color: #FF5656;
+            }
+            &.blue {
+                background-color: #3A84FF;
             }
         }
     }
