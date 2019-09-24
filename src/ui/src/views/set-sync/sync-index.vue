2b3afa9b8571a06131a713596145af52ae4ae43f
Daryl0819 <zhdgmax@qq.com>
2019-09-24 22:11:19 +0800

    update:ui: 同步集群
    Former-commit-id: 214ddad8fafe9e085884a59ed8b7bee4261f1204

--- src/ui/src/views/set-sync/sync-index.vue
+++ src/ui/src/views/set-sync/sync-index.vue
@@ -1,37 +1,53 @@
 <template>
-    <div class="sync-set-wrapper">
-        <div class="title clearfix">
-            <div class="tips fl">
-                <p class="mr10">{{$t('请确认以下模版修改信息：')}}</p>
-                <span class="mr30">
-                    <i class="dot"></i>
-                    {{$t('新增模块')}}
-                </span>
-                <span class="mr30">
-                    <i class="dot blue"></i>
-                    {{$t('变更模块')}}
-                </span>
-                <span>
-                    <i class="dot red"></i>
-                    {{$t('删除模块')}}
-                </span>
+    <div class="sync-set-wrapper" v-bkloading="{ isLoading: $loading('diffTemplateAndInstances') }">
+        <template v-if="noInfo">
+            <div class="no-content">
+                <img src="../../assets/images/no-content.png" alt="no-content">
+                <p>{{$t('无集群模板更新信息')}}</p>
+                <bk-button theme="primary" @click="handleGoback">{{$t('返回')}}</bk-button>
             </div>
-            <bk-checkbox class="expand-all fr" v-model="expandAll" @change="handleExpandAll">{{$t('全部展开')}}</bk-checkbox>
-        </div>
-        <div class="instance-list">
-            <set-instance class="instance-item"
-                ref="setInstance"
-                v-for="(instance, index) in diffList"
-                :key="instance.bk_set_id"
-                :instance="instance"
-                :expand="index === 0">
-            </set-instance>
-        </div>
-        <div class="footer">
-            <bk-button theme="primary" class="mr10" @click="handleConfirmSync">{{$t('确认同步')}}</bk-button>
-            <bk-button class="mr10">{{$t('取消')}}</bk-button>
-            <span>{{$t('已选择20个集群实例')}}</span>
-        </div>
+        </template>
+        <template v-else-if="isLatestInfo">
+            <div class="no-content">
+                <img src="../../assets/images/latest-data.png" alt="no-content">
+                <p>{{$t('最新集群模板信息')}}</p>
+                <bk-button theme="primary" @click="handleGoback">{{$t('返回')}}</bk-button>
+            </div>
+        </template>
+        <template v-else-if="diffList.length">
+            <div class="title clearfix">
+                <div class="tips fl">
+                    <p class="mr10">{{$t('请确认以下模板修改信息')}}：</p>
+                    <span class="mr30">
+                        <i class="dot"></i>
+                        {{$t('新增模块')}}
+                    </span>
+                    <span class="mr30">
+                        <i class="dot blue"></i>
+                        {{$t('变更模块')}}
+                    </span>
+                    <span>
+                        <i class="dot red"></i>
+                        {{$t('删除模块')}}
+                    </span>
+                </div>
+                <bk-checkbox class="expand-all fr" v-model="expandAll" @change="handleExpandAll">{{$t('全部展开')}}</bk-checkbox>
+            </div>
+            <div class="instance-list">
+                <set-instance class="instance-item"
+                    ref="setInstance"
+                    v-for="(instance, index) in diffList"
+                    :key="instance.bk_set_id"
+                    :instance="instance"
+                    :expand="index === 0">
+                </set-instance>
+            </div>
+            <div class="footer">
+                <bk-button theme="primary" class="mr10" @click="handleConfirmSync">{{$t('确认同步')}}</bk-button>
+                <bk-button class="mr10">{{$t('取消')}}</bk-button>
+                <span>{{$tc('已选集群实例', 20, { count: 20 })}}</span>
+            </div>
+        </template>
     </div>
 </template>
 
@@ -44,7 +60,9 @@
         data () {
             return {
                 expandAll: false,
-                diffList: []
+                diffList: [],
+                noInfo: false,
+                isLatestInfo: false
             }
         },
         computed: {
@@ -57,16 +75,23 @@
         },
         methods: {
             async getDiffData () {
-                this.diffList = await this.$store.dispatch('setSync/diffTemplateAndInstances', {
-                    bizId: 3 || this.bizId,
-                    setTemplateId: 1,
-                    params: {
-                        bk_set_ids: [14]
-                    },
-                    config: {
-                        requestId: 'diffTemplateAndInstances'
-                    }
-                })
+                try {
+                    this.diffList = await this.$store.dispatch('setSync/diffTemplateAndInstances', {
+                        bizId: 3 || this.bizId,
+                        setTemplateId: 1,
+                        params: {
+                            bk_set_ids: [14]
+                        },
+                        config: {
+                            requestId: 'diffTemplateAndInstances'
+                        }
+                    })
+                    this.isLatestInfo = false
+                    this.noInfo = false
+                } catch (e) {
+                    console.error(e)
+                    this.noInfo = true
+                }
             },
             handleConfirmSync () {
                 this.$bkInfo({
@@ -101,6 +126,9 @@
                 this.$refs.setInstance.forEach(instance => {
                     instance.localExpand = expand
                 })
+            },
+            handleGoback () {
+
             }
         }
     }
@@ -109,6 +137,21 @@
 <style lang="scss" scoped>
     .sync-set-wrapper {
         padding: 0 20px;
+    }
+    .no-content {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        font-size: 16px;
+        color: #63656e;
+        text-align: center;
+        transform: translate(-50%, -50%);
+        img {
+            width: 130px;
+        }
+        p {
+            padding: 20px 0 30px;
+        }
     }
     .tips {
         display: flex;
