6081a3c515a0df1844c8e5dff791a7e856c0a591
Daryl0819 <zhdgmax@qq.com>
2019-09-26 19:02:10 +0800

    update:ui: 字段展示优化
    Former-commit-id: bc757f451bdccdc32bfa04885c4440409cce9081

--- src/ui/src/components/ui/details/details.vue
+++ src/ui/src/components/ui/details/details.vue
@@ -18,7 +18,7 @@
                                 <slot :name="property['bk_property_id']">
                                     <span class="property-value clearfix fl" v-if="property.unit">
                                         <span class="property-value-text fl">{{getValue(property)}}</span>
-                                        <span class="property-value-unit fl">{{property.unit}}</span>
+                                        <span class="property-value-unit fl" v-if="getValue(property) !== '--'">{{property.unit}}</span>
                                     </span>
                                     <span class="property-value fl" v-else>{{getValue(property)}}</span>
                                 </slot>
