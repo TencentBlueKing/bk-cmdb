46b50ec2232688dad5e00ed8c31d9ab6c1898fef
hoffermei <meihaifeng.hust@gmail.com>
2019-10-01 11:43:35 +0800

    服务模板相关DB变更：移除metadata #3232

--- src/scene_server/admin_server/imports.go
+++ src/scene_server/admin_server/imports.go
@@ -58,4 +58,5 @@ import (
 	_ "configcenter/src/scene_server/admin_server/upgrader/x19.09.03.04"
 	_ "configcenter/src/scene_server/admin_server/upgrader/x19.09.03.05"
 	_ "configcenter/src/scene_server/admin_server/upgrader/x19.09.03.06"
+	_ "configcenter/src/scene_server/admin_server/upgrader/x19.09.03.07"
 )
