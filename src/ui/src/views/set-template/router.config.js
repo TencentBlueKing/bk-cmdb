89afbb58ad1b7a6dc3faf5a2878032762a8b0613
Daryl0819 <zhdgmax@qq.com>
2019-10-12 18:55:46 +0800

    update:ui: 同步历史
    Former-commit-id: 013452ec0ce24b627b305dd4525ae33ca97de01f

--- src/ui/src/views/set-template/router.config.js
+++ src/ui/src/views/set-template/router.config.js
@@ -28,7 +28,7 @@ export default [{
     })
 }, {
     name: 'syncHistory',
-    path: 'set/template/history/:templateId?',
+    path: 'set/instance/history/:templateId?',
     component: () => import('./sync-history.vue'),
     meta: new Meta({
         owner: MENU_BUSINESS,
