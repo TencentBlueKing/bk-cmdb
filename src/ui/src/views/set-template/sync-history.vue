f67ffb142072ad2c057ccabdbd760ea58ec55cce
Daryl0819 <zhdgmax@qq.com>
2019-10-12 21:17:02 +0800

    update:ui: 完善同步历史

--- src/ui/src/views/set-template/sync-history.vue
+++ src/ui/src/views/set-template/sync-history.vue
@@ -4,16 +4,18 @@
             <bk-date-picker style="width: 300px;" class="fl"
                 type="daterange"
                 :placeholder="$t('选择日期范围')"
-                v-model="searchDate">
+                @change="hanldeFilterByDate">
             </bk-date-picker>
             <bk-input style="width: 240px;" class="fl ml10"
                 right-icon="icon-search"
                 v-model="searchName"
-                :placeholder="$t('集群名称')">
+                :placeholder="$t('集群名称')"
+                @enter="getData">
             </bk-input>
         </div>
-        <bk-table v-bkloading="{ isLoading: $loading('getSyncHistory') }"
-            :data="list"
+        <bk-table class="history-table"
+            v-bkloading="{ isLoading: $loading('getSyncHistory') }"
+            :data="displayList"
             :pagination="pagination"
             :max-height="$APP.height - 229"
             @sort-change="handleSortChange"
@@ -45,7 +47,7 @@
                     <span v-else>--</span>
                 </template>
             </bk-table-column>
-            <bk-table-column :label="$t('同步时间')" prop="sync_time" sortable="custom">
+            <bk-table-column :label="$t('同步时间')" prop="last_time" sortable="custom">
                 <template slot-scope="{ row }">
                     <span>{{row.last_time ? $tools.formatTime(row.last_time, 'YYYY-MM-DD HH:mm:ss') : '--'}}</span>
                 </template>
@@ -68,17 +70,43 @@
                 searchName: '',
                 searchDate: [],
                 list: [],
+                listWithTopo: [],
                 pagination: {
                     count: 0,
                     current: 1,
                     ...this.$tools.getDefaultPaginationConfig()
                 },
-                listSort: 'sync_time'
+                listSort: 'last_time'
             }
         },
         computed: {
+            business () {
+                return this.$store.state.objectBiz.bizId
+            },
             templateId () {
                 return this.$route.params.templateId
+            },
+            setsId () {
+                const ids = this.list.map(item => item.bk_set_id)
+                return [...new Set(ids)]
+            },
+            displayList () {
+                const list = this.$tools.clone(this.list)
+                return list.map(item => {
+                    const otherParams = {
+                        topo_path: [],
+                        host_count: 0
+                    }
+                    const setInfo = this.listWithTopo.find(set => set.bk_set_id === item.bk_set_id)
+                    if (setInfo) {
+                        otherParams.topo_path = setInfo.topo_path || []
+                        otherParams.host_count = setInfo.host_count || 0
+                    }
+                    return {
+                        ...item,
+                        ...otherParams
+                    }
+                })
             },
             searchParams () {
                 const params = {
@@ -90,12 +118,16 @@
                         sort: this.listSort
                     }
                 }
+                if (this.searchDate.length) {
+                    params.start_time = this.searchDate[0] || ''
+                    params.end_time = this.searchDate[1] || ''
+                }
                 return params
             }
         },
         created () {
             this.getSetTemplateInfo()
-            this.getHistoryList()
+            this.getData()
         },
         methods: {
             setBreadcrumbs () {
@@ -132,6 +164,10 @@
                 }
                 return '--'
             },
+            async getData () {
+                await this.getHistoryList()
+                this.setsId.length && this.getSetInstancesWithTopo()
+            },
             async getSetTemplateInfo () {
                 try {
                     const info = await this.$store.dispatch('setTemplate/getSingleSetTemplateInfo', {
@@ -147,7 +183,7 @@
             async getHistoryList () {
                 try {
                     const data = await this.$store.dispatch('setTemplate/getSyncHistory', {
-                        bizId: this.$store.state.objectBiz.bizId,
+                        bizId: this.business,
                         params: this.searchParams,
                         config: {
                             requestId: 'getSyncHistory'
@@ -160,17 +196,46 @@
                     this.list = []
                 }
             },
+            async getSetInstancesWithTopo () {
+                try {
+                    const data = await this.$store.dispatch('setTemplate/getSetInstancesWithTopo', {
+                        bizId: this.business,
+                        setTemplateId: this.templateId,
+                        params: {
+                            limit: {
+                                start: 0,
+                                limit: this.pagination.limit
+                            },
+                            bk_set_ids: this.setsId
+                        },
+                        config: {
+                            requestId: 'getSetInstancesWithTopo'
+                        }
+                    })
+                    this.listWithTopo = data.info || []
+                } catch (e) {
+                    console.error(e)
+                    this.listWithTopo = []
+                }
+            },
             handleSortChange (sort) {
                 this.listSort = this.$tools.getSort(sort)
                 this.handlePageChange(1)
             },
             handlePageChange (current) {
                 this.pagination.current = current
-                this.getHistoryList()
+                this.getData()
             },
             handleSizeChange (size) {
                 this.pagination.limit = size
-                this.getHistoryList(1)
+                this.handlePageChange(1)
+            },
+            hanldeFilterByDate (daterange) {
+                daterange = daterange.filter(date => date)
+                this.searchDate = daterange.map((date, index) => {
+                    return index === 0 ? (date + ' 00:00:00') : (date + ' 23:59:59')
+                })
+                this.getData()
             }
         }
     }
@@ -182,5 +247,27 @@
     }
     .options {
         padding-bottom: 15px;
+    }
+    .history-table {
+        .sync-status {
+            color: #63656E;
+            .bk-icon {
+                margin-top: -2px;
+            }
+            .svg-icon {
+                @include inlineBlock;
+                margin-top: -4px;
+                width: 16px;
+            }
+            &.fail {
+                color: #EA3536;
+                .bk-icon {
+                    color: #63656E;
+                }
+            }
+            &.success {
+                color: #2DCB56;
+            }
+        }
     }
 </style>
