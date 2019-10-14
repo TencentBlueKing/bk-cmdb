d00d69c20afd76d6131fc9bc1b9223dafb1cb12c
hoffermei <meihaifeng.hust@gmail.com>
2019-10-14 10:46:08 +0800

    3.6权限测试微调 #3267
    Former-commit-id: 1ba105cb59d6c25ca675d6f6975b4e0e8b034a75

--- src/auth/parser/settemplate.go
+++ src/auth/parser/settemplate.go
@@ -13,91 +13,159 @@
 package parser
 
 import (
-	"configcenter/src/auth/meta"
-	"configcenter/src/common"
+	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/tidwall/gjson"
 	"net/http"
 	"regexp"
 	"strconv"
+
+	"configcenter/src/auth/meta"
 )
 
 var SetTemplateAuthConfigs = []AuthConfig{
 	{
-		Name:                  "createSetTemplatePattern",
+		Name:                  "CreateSetTemplateRegex",
 		Description:           "创建集群模板",
-		Regex:                 regexp.MustCompile(`^/api/v3/create/topo/set_template/bk_biz_id/([0-9]+)$`),
+		Regex:                 regexp.MustCompile(`^/api/v3/create/topo/set_template/bk_biz_id/([0-9]+)/?$`),
 		HTTPMethod:            http.MethodPost,
 		RequiredBizInMetadata: true,
 		ResourceType:          meta.SetTemplate,
 		ResourceAction:        meta.Create,
 	}, {
-		Name:                  "updateSetTemplate",
+		Name:                  "UpdateSetTemplateRegex",
 		Description:           "更新集群模板",
-		Regex:                 regexp.MustCompile(`^/api/v3/update/topo/set_template/([0-9]+)/bk_biz_id/([0-9]+)$`),
+		Regex:                 regexp.MustCompile(`^/api/v3/update/topo/set_template/([0-9]+)/bk_biz_id/([0-9]+)/?$`),
 		HTTPMethod:            http.MethodPut,
 		RequiredBizInMetadata: true,
 		ResourceType:          meta.SetTemplate,
 		ResourceAction:        meta.Update,
 		InstanceIDGetter: func(request *RequestContext, re *regexp.Regexp) (int64s []int64, e error) {
-			templateID := gjson.GetBytes(request.Body, common.BKFieldID).Int()
-			if templateID <= 0 {
-				return nil, errors.New("invalid set template")
-			}
-			return []int64{templateID}, nil
-		},
-	}, {
-		Name:                  "getSetTemplate",
-		Description:           "获取集群模板",
-		Regex:                 regexp.MustCompile(`^/api/v3/find/topo/set_template/([0-9]+)/bk_biz_id/([0-9]+)$`),
-		HTTPMethod:            http.MethodGet,
-		RequiredBizInMetadata: true,
-		ResourceType:          meta.SetTemplate,
-		ResourceAction:        meta.FindMany,
-		InstanceIDGetter: func(request *RequestContext, re *regexp.Regexp) (int64s []int64, e error) {
 			ss := re.FindStringSubmatch(request.URI)
-			if len(ss) < 1 {
-				return nil, errors.New("getSetTemplate regex match nothing")
+			if len(ss) < 2 {
+				return nil, errors.New("UpdateSetTemplateRegex regex doesn't match")
 			}
-			id, err := strconv.ParseInt(ss[0], 10, 64)
+			id, err := strconv.ParseInt(ss[1], 10, 64)
 			if err != nil {
-				return nil, fmt.Errorf("getSetTemplate regex parse match to int failed, err: %+v", err)
+				return nil, fmt.Errorf("UpdateSetTemplateRegex regex parse match to int failed, err: %+v", err)
 			}
 			return []int64{id}, nil
 		},
 	}, {
-		Name:                  "listSetTemplate",
-		Description:           "列表查询集群模板",
-		Regex:                 regexp.MustCompile(`^/api/v3/findmany/topo/set_template/bk_biz_id/([0-9]+)$`),
-		HTTPMethod:            http.MethodPost,
-		RequiredBizInMetadata: true,
-		ResourceType:          meta.SetTemplate,
-		ResourceAction:        meta.FindMany,
-	}, {
-		Name:                  "listSetTemplateWeb",
-		Description:           "列表查询集群模板",
-		Regex:                 regexp.MustCompile(`^/api/v3/findmany/topo/set_template/bk_biz_id/([0-9]+)/web$`),
-		HTTPMethod:            http.MethodPost,
-		RequiredBizInMetadata: true,
-		ResourceType:          meta.SetTemplate,
-	}, {
-		Name:                  "deleteSetTemplatePattern",
+		Name:                  "DeleteSetTemplateRegex",
 		Description:           "删除集群模板",
-		Pattern:               "/api/v3/deletemany/topo/set_template/bk_biz_id/([0-9]+)",
+		Regex:                 regexp.MustCompile(`^/api/v3/deletemany/topo/set_template/bk_biz_id/([0-9]+)/?$`),
 		HTTPMethod:            http.MethodDelete,
 		RequiredBizInMetadata: true,
 		ResourceType:          meta.SetTemplate,
 		ResourceAction:        meta.Delete,
 		InstanceIDGetter: func(request *RequestContext, re *regexp.Regexp) (int64s []int64, e error) {
 			// TODO
-			return []int64{}, nil
+			data := &struct {
+				SetTemplateIDs []int64 `json:"set_template_ids" mapstructure:"set_template_ids"`
+			}{}
+			if err := json.Unmarshal(request.Body, data); err != nil {
+				return nil, fmt.Errorf("unmarshal failed, err: %+v", err)
+			}
+			return data.SetTemplateIDs, nil
 		},
+	}, {
+		Name:                  "GetSetTemplateRegex",
+		Description:           "获取集群模板",
+		Regex:                 regexp.MustCompile(`^/api/v3/find/topo/set_template/([0-9]+)/bk_biz_id/([0-9]+)/?$`),
+		HTTPMethod:            http.MethodGet,
+		RequiredBizInMetadata: true,
+		ResourceType:          meta.SetTemplate,
+		ResourceAction:        meta.Find,
+		InstanceIDGetter: func(request *RequestContext, re *regexp.Regexp) (int64s []int64, e error) {
+			ss := re.FindStringSubmatch(request.URI)
+			if len(ss) < 2 {
+				return nil, errors.New("getSetTemplate regex match nothing")
+			}
+			id, err := strconv.ParseInt(ss[1], 10, 64)
+			if err != nil {
+				return nil, fmt.Errorf("getSetTemplate regex parse match to int failed, err: %+v", err)
+			}
+			return []int64{id}, nil
+		},
+	}, {
+		Name:                  "ListSetTemplateRegex",
+		Description:           "列表查询集群模板",
+		Regex:                 regexp.MustCompile(`^/api/v3/findmany/topo/set_template/bk_biz_id/([0-9]+)/?$`),
+		HTTPMethod:            http.MethodPost,
+		RequiredBizInMetadata: true,
+		ResourceType:          meta.SetTemplate,
+		ResourceAction:        meta.FindMany,
+	}, {
+		Name:                  "ListSetTemplateWebRegex",
+		Description:           "列表查询集群模板-Web",
+		Regex:                 regexp.MustCompile(`^/api/v3/findmany/topo/set_template/bk_biz_id/([0-9]+)/web/?$`),
+		HTTPMethod:            http.MethodPost,
+		RequiredBizInMetadata: true,
+		ResourceType:          meta.SetTemplate,
+		ResourceAction:        meta.FindMany,
+	}, {
+		Name:                  "ListSetTplRelatedSvcTplRegex",
+		Description:           "查询集群模板关联的服务模板列表",
+		Regex:                 regexp.MustCompile(`^/api/v3/findmany/topo/set_template/([0-9]+)/bk_biz_id/([0-9]+)/service_templates/?$`),
+		HTTPMethod:            http.MethodGet,
+		RequiredBizInMetadata: true,
+		ResourceType:          meta.ProcessServiceTemplate,
+		ResourceAction:        meta.FindMany,
+	}, {
+		Name:                  "ListSetTplRelatedSetsWebRegex",
+		Description:           "查询集群模板关联的服务模板列表-Web",
+		Regex:                 regexp.MustCompile(`^/api/v3/findmany/topo/set_template/([0-9]+)/bk_biz_id/([0-9]+)/sets/web/?$`),
+		HTTPMethod:            http.MethodPost,
+		RequiredBizInMetadata: true,
+		ResourceType:          meta.ProcessServiceTemplate,
+		ResourceAction:        meta.FindMany,
+	}, {
+		Name:                  "DiffSetTplWithInstRegex",
+		Description:           "对比集群模板与集群之间的差异",
+		Regex:                 regexp.MustCompile(`^/api/v3/findmany/topo/set_template/([0-9]+)/bk_biz_id/([0-9]+)/diff_with_instances/?$`),
+		HTTPMethod:            http.MethodPost,
+		RequiredBizInMetadata: true,
+		ResourceType:          meta.ModelSet,
+		ResourceAction:        meta.FindMany,
+	}, {
+		Name:                  "SyncSetTplToInstRegex",
+		Description:           "用集群模板同步集群",
+		Regex:                 regexp.MustCompile(`^/api/v3/updatemany/topo/set_template/([0-9]+)/bk_biz_id/([0-9]+)/sync_to_instances/?$`),
+		HTTPMethod:            http.MethodPost,
+		RequiredBizInMetadata: true,
+		ResourceType:          meta.ModelSet,
+		ResourceAction:        meta.Update,
+	}, {
+		Name:                  "GetSetSyncStatusRegex",
+		Description:           "获取集群同步状态",
+		Regex:                 regexp.MustCompile(`^/api/v3/findmany/topo/set_template/([0-9]+)/bk_biz_id/([0-9]+)/instances_sync_status/?$`),
+		HTTPMethod:            http.MethodPost,
+		RequiredBizInMetadata: true,
+		ResourceType:          meta.ModelSet,
+		ResourceAction:        meta.FindMany,
+	}, {
+		Name:                  "ListSetTemplateSyncStatusRegex",
+		Description:           "获取集群同步状态",
+		Regex:                 regexp.MustCompile(`^/api/v3/findmany/topo/set_template_sync_status/bk_biz_id/([0-9]+)/?$`),
+		HTTPMethod:            http.MethodPost,
+		RequiredBizInMetadata: true,
+		ResourceType:          meta.ModelSet,
+		ResourceAction:        meta.FindMany,
+	}, {
+		Name:                  "ListSetTemplateSyncHistoryRegex",
+		Description:           "集群模板的同步历史记录",
+		Regex:                 regexp.MustCompile(`^/api/v3/findmany/topo/set_template_sync_history/bk_biz_id/([0-9]+)/?$`),
+		HTTPMethod:            http.MethodPost,
+		RequiredBizInMetadata: true,
+		ResourceType:          meta.ModelSet,
+		ResourceAction:        meta.FindMany,
+		InstanceIDGetter:      nil,
 	},
 }
 
 func (ps *parseStream) SetTemplate() *parseStream {
-	resources, err := MatchAndGenerateBizInURLIAMResource(ServiceTemplateAuthConfigs, ps.RequestCtx)
+	resources, err := MatchAndGenerateBizInURLIAMResource(SetTemplateAuthConfigs, ps.RequestCtx)
 	if err != nil {
 		ps.err = err
 	}
