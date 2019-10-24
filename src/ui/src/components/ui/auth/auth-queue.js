98de282ef9fc8fe29425665155dbc9c1599bc626
Daryl0819 <zhdgmax@qq.com>
2019-10-24 16:31:27 +0800

    update:ui: 鉴权组件优化
    Former-commit-id: c078f268b104e58c7af14c6a1eefca905f18c7f4

--- src/ui/src/components/ui/auth/auth-queue.js
+++ src/ui/src/components/ui/auth/auth-queue.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import equal from 'deep-equal'
+import deepClone from 'clone-deep'
 import $http from '@/api'
 import { GET_AUTH_META } from '@/dictionary/auth'
 
@@ -15,20 +16,22 @@ export default new Vue({
     watch: {
         async queue () {
             if (!this.queue.length) return
-            const resources = this.queue.map(item => {
+            const queue = deepClone(this.queue)
+            const authInstances = deepClone(this.authInstances)
+            this.queue = []
+            this.authInstances = []
+            const resources = queue.map(item => {
                 const meta = GET_AUTH_META(item.data.type, item.data)
                 delete meta.scope
                 return meta
             })
             const authData = await $http.post('auth/verify', { resources })
-            this.authInstances.forEach(instance => {
-                const index = this.queue.findIndex(item => equal(item.resource, instance.resource))
+            authInstances.forEach(instance => {
+                const index = queue.findIndex(item => equal(item.resource, instance.resource))
                 if (index > -1) {
                     instance.component.updateAuth(authData[index])
                 }
             })
-            this.queue = []
-            this.authInstances = []
         }
     },
     methods: {
