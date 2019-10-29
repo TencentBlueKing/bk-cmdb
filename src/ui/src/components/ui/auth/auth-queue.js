d7de3589d5eba094d5fffab68b0a8625e030d4c2
Daryl0819 <zhdgmax@qq.com>
2019-10-29 21:10:15 +0800

    update:ui: 鉴权组件修改

--- src/ui/src/components/ui/auth/auth-queue.js
+++ src/ui/src/components/ui/auth/auth-queue.js
@@ -19,16 +19,30 @@ export default new Vue({
             const authInstances = [...this.authInstances]
             this.queue = []
             this.authInstances = []
-            const resources = queue.map(item => {
-                const meta = GET_AUTH_META(item.data.type, item.data)
-                delete meta.scope
-                return meta
+            const params = queue.map(item => {
+                const types = Array.isArray(item.data.type) ? item.data.type : [item.data.type]
+                const metas = types.map(type => {
+                    const meta = GET_AUTH_META(type, item.data)
+                    delete meta.scope
+                    return meta
+                })
+                return metas
+            })
+            const resources = []
+            params.forEach(metas => {
+                resources.push(...metas)
             })
             const authData = await $http.post('auth/verify', { resources })
             authInstances.forEach(instance => {
-                const index = queue.findIndex(item => equal(item.data, instance.data))
-                if (index > -1) {
-                    instance.component.updateAuth(authData[index])
+                const findIndex = queue.findIndex(item => equal(item.data, instance.data))
+                const types = Array.isArray(instance.data.type) ? instance.data.type : [instance.data.type]
+                if (findIndex > -1) {
+                    const auths = []
+                    types.forEach((type, index) => {
+                        const authIndex = findIndex + index
+                        authData[authIndex] && auths.push(authData[authIndex])
+                    })
+                    instance.component.updateAuth(auths)
                 }
             })
         }
