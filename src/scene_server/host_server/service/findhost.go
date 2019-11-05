e61c854a677b0df0d418c40ed697a858f0d3961f
wcy00000000000000 <2269766985@qq.com>
2019-11-05 12:08:25 +0800

    add: hosts/app/{bk_biz_id}/list_hosts_topo api

--- src/scene_server/host_server/service/findhost.go
+++ src/scene_server/host_server/service/findhost.go
@@ -18,6 +18,7 @@ import (
 
 	"configcenter/src/common"
 	"configcenter/src/common/blog"
+	"configcenter/src/common/condition"
 	meta "configcenter/src/common/metadata"
 	"configcenter/src/common/util"
 	"github.com/emicklei/go-restful"
@@ -132,5 +133,183 @@ func (s *Service) ListHostsWithNoBiz(req *restful.Request, resp *restful.Respons
 	}
 
 	result := meta.NewSuccessResponse(host)
+	_ = resp.WriteEntity(result)
+}
+
+// ListBizHostsTopo list hosts under business specified by path parameter with their topology information
+func (s *Service) ListBizHostsTopo(req *restful.Request, resp *restful.Response) {
+	header := req.Request.Header
+	ctx := util.NewContextFromHTTPHeader(header)
+	rid := util.ExtractRequestIDFromContext(ctx)
+	srvData := s.newSrvComm(header)
+	defErr := srvData.ccErr
+
+	parameter := &meta.ListHostsWithNoBizParameter{}
+	if err := json.NewDecoder(req.Request.Body).Decode(parameter); err != nil {
+		blog.Errorf("ListHostByTopoNode failed, decode body failed, err: %#v, rid:%s", err, srvData.rid)
+		_ = resp.WriteError(http.StatusBadRequest, &meta.RespError{Msg: defErr.Error(common.CCErrCommJSONUnmarshalFailed)})
+		return
+	}
+	bizID, err := util.GetInt64ByInterface(req.PathParameter("bk_biz_id"))
+	if err != nil {
+		_ = resp.WriteError(http.StatusBadRequest, &meta.RespError{Msg: defErr.Errorf(common.CCErrCommParamsInvalid, "bk_app_id")})
+		return
+	}
+	if bizID == 0 {
+		_ = resp.WriteError(http.StatusBadRequest, &meta.RespError{Msg: defErr.Errorf(common.CCErrCommParamsInvalid, "bk_app_id")})
+		return
+	}
+
+	if parameter.Page.Limit == 0 {
+		parameter.Page.Limit = common.BKMaxPageSize
+	}
+	if parameter.Page.Limit > common.BKMaxPageLimit {
+		blog.Errorf("ListBizHosts failed, page limit %d exceed max pageSize %d, rid:%s", parameter.Page.Limit, common.BKMaxPageLimit, srvData.rid)
+		_ = resp.WriteError(http.StatusBadRequest, &meta.RespError{Msg: defErr.Error(common.CCErrCommJSONUnmarshalFailed)})
+		return
+	}
+
+	// search all hosts
+	option := meta.ListHosts{
+		BizID:              bizID,
+		HostPropertyFilter: parameter.HostPropertyFilter,
+		Page:               parameter.Page,
+	}
+	data, err := s.CoreAPI.CoreService().Host().ListHosts(ctx, header, option)
+	if err != nil {
+		blog.Errorf("find host failed, err: %s, input:%#v, rid:%s", err.Error(), parameter, rid)
+		_ = resp.WriteError(http.StatusBadRequest, &meta.RespError{Msg: defErr.Error(common.CCErrHostGetFail)})
+		return
+	}
+
+	// search all hosts' host module relations
+	hostIDs := make([]int64, 0)
+	for _, host := range data.Info {
+		hostID, err := util.GetInt64ByInterface(host[common.BKHostIDField])
+		if err != nil {
+			blog.ErrorJSON("host: %s bk_host_id field invalid, rid:%s", host, rid)
+			_ = resp.WriteError(http.StatusInternalServerError, &meta.RespError{Msg: err})
+			return
+		}
+		hostIDs = append(hostIDs, hostID)
+	}
+	relationCond := meta.HostModuleRelationRequest{
+		ApplicationID: bizID,
+		HostIDArr:     hostIDs,
+	}
+	relations, err := srvData.lgc.GetConfigByCond(srvData.ctx, relationCond)
+	if nil != err {
+		blog.ErrorJSON("read host module relation error: %s, input: %s, rid: %s", err, data, srvData.rid)
+		_ = resp.WriteError(http.StatusInternalServerError, &meta.RespError{Msg: err})
+		return
+	}
+
+	// search all module and set info
+	setIDs := make([]int64, 0)
+	moduleIDs := make([]int64, 0)
+	relation := make(map[int64]map[int64][]int64)
+	for _, r := range relations {
+		setIDs = append(setIDs, r.SetID)
+		moduleIDs = append(moduleIDs, r.ModuleID)
+		if setModule, ok := relation[r.HostID]; ok {
+			setModule[r.SetID] = append(setModule[r.SetID], r.ModuleID)
+			relation[r.HostID] = setModule
+		} else {
+			setModule := make(map[int64][]int64)
+			setModule[r.SetID] = append(setModule[r.SetID], r.ModuleID)
+			relation[r.HostID] = setModule
+		}
+	}
+	setIDs = util.IntArrayUnique(setIDs)
+	moduleIDs = util.IntArrayUnique(moduleIDs)
+
+	cond := condition.CreateCondition()
+	cond.Field(common.BKSetIDField).In(setIDs)
+	query := &meta.QueryCondition{
+		Fields:    []string{common.BKSetIDField, common.BKSetNameField},
+		Condition: cond.ToMapStr(),
+	}
+	sets, err := s.CoreAPI.CoreService().Instance().ReadInstance(ctx, header, common.BKInnerObjIDSet, query)
+	if err != nil {
+		blog.ErrorJSON("get set by condition: %s failed, err: %+v, rid: %s", cond.ToMapStr(), err, rid)
+		_ = resp.WriteError(http.StatusInternalServerError, &meta.RespError{Msg: err})
+		return
+	}
+	setMap := make(map[int64]string)
+	for _, set := range sets.Data.Info {
+		setID, err := set.Int64(common.BKSetIDField)
+		if err != nil {
+			blog.ErrorJSON("set %s id invalid, error: %s, rid: %s", set, err, rid)
+			_ = resp.WriteError(http.StatusInternalServerError, &meta.RespError{Msg: err})
+			return
+		}
+		setName, err := set.String(common.BKSetNameField)
+		if err != nil {
+			blog.ErrorJSON("set %s name invalid, error: %s, rid: %s", set, err, rid)
+			_ = resp.WriteError(http.StatusInternalServerError, &meta.RespError{Msg: err})
+			return
+		}
+		setMap[setID] = setName
+	}
+
+	cond = condition.CreateCondition()
+	cond.Field(common.BKModuleIDField).In(moduleIDs)
+	query = &meta.QueryCondition{
+		Fields:    []string{common.BKModuleIDField, common.BKModuleNameField},
+		Condition: cond.ToMapStr(),
+	}
+	modules, err := s.CoreAPI.CoreService().Instance().ReadInstance(ctx, header, common.BKInnerObjIDModule, query)
+	if err != nil {
+		blog.ErrorJSON("get module by condition: %s failed, err: %+v, rid: %s", cond.ToMapStr(), err, rid)
+		_ = resp.WriteError(http.StatusInternalServerError, &meta.RespError{Msg: err})
+		return
+	}
+	moduleMap := make(map[int64]interface{})
+	for _, module := range modules.Data.Info {
+		moduleID, err := module.Int64(common.BKModuleIDField)
+		if err != nil {
+			blog.ErrorJSON("module %s id invalid, error: %s, rid: %s", module, err, rid)
+			_ = resp.WriteError(http.StatusInternalServerError, &meta.RespError{Msg: err})
+			return
+		}
+		moduleName, err := module.String(common.BKModuleNameField)
+		if err != nil {
+			blog.ErrorJSON("module %s name invalid, error: %s, rid: %s", module, err, rid)
+			_ = resp.WriteError(http.StatusInternalServerError, &meta.RespError{Msg: err})
+			return
+		}
+		moduleMap[moduleID] = moduleName
+	}
+
+	// format the output
+	hosts := meta.ListHostResult{
+		Count: data.Count,
+	}
+	for _, host := range data.Info {
+		hostTopo := make(map[string]interface{})
+		hostTopo["host"] = host
+		topos := make([]interface{}, 0)
+		hostID, _ := util.GetInt64ByInterface(host[common.BKHostIDField])
+		if setModule, ok := relation[hostID]; ok {
+			for setID, moduleIDs := range setModule {
+				topo := make(map[string]interface{})
+				topo[common.BKSetIDField] = setID
+				topo[common.BKSetNameField] = setMap[setID]
+				modules := make([]interface{}, 0)
+				for _, moduleID := range moduleIDs {
+					module := make(map[string]interface{})
+					module[common.BKModuleIDField] = moduleID
+					module[common.BKModuleNameField] = moduleMap[moduleID]
+					modules = append(modules, module)
+				}
+				topo["module"] = modules
+				topos = append(topos, topo)
+			}
+		}
+		hostTopo["topo"] = topos
+		hosts.Info = append(hosts.Info, hostTopo)
+	}
+
+	result := meta.NewSuccessResponse(hosts)
 	_ = resp.WriteEntity(result)
 }
