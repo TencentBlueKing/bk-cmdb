bd72dac9ea6a8c77243efea00b0dcbcbc5333729
Daryl0819 <zhdgmax@qq.com>
2019-11-28 16:18:47 +0800

    update:ui: 添加user指令
    Former-commit-id: e6ac71b08d2cadb7dfe078e8bb149a43e8462f5a

--- src/ui/src/directives/index.js
+++ src/ui/src/directives/index.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import vClickOutside from 'v-click-outside'
 import cursor from './cursor.js'
 import transferDom from './transfer-dom.js'
+import user from './user.js'
 
 Vue.use(vClickOutside)
 Vue.use(cursor)
+Vue.use(user)
 Vue.directive('transfer-dom', transferDom)
 
 export default {
