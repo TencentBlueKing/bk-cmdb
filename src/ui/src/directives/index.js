48d5494d8799f3afaf5b0017637c6ab14537b327
Daryl0819 <zhdgmax@qq.com>
2019-11-28 21:29:27 +0800

    feature: 新增v-overflow-tips指令
    Former-commit-id: 62a9cdfa3c2d22231e14f3ab64779e894ab5c58e

--- src/ui/src/directives/index.js
+++ src/ui/src/directives/index.js
@@ -3,10 +3,12 @@ import vClickOutside from 'v-click-outside'
 import cursor from './cursor.js'
 import transferDom from './transfer-dom.js'
 import user from './user.js'
+import overflowTips from './overflow-tips'
 
 Vue.use(vClickOutside)
 Vue.use(cursor)
 Vue.use(user)
+Vue.use(overflowTips)
 Vue.directive('transfer-dom', transferDom)
 
 export default {
