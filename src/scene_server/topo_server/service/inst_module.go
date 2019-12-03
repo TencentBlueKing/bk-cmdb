63f0ca59eeddd61ae80bc4b0ae64553d36927d21
hoffermei <meihaifeng.hust@gmail.com>
2019-12-03 19:45:07 +0800

    bugfix #3507

--- src/scene_server/topo_server/service/inst_module.go
+++ src/scene_server/topo_server/service/inst_module.go
@@ -421,7 +421,7 @@ func (s *Service) UpdateModuleHostApplyEnableStatus(params types.ContextParams, 
 		}
 		if len(ruleIDs) > 0 {
 			deleteRuleOption := metadata.DeleteHostApplyRuleOption{
-				RuleIDs: nil,
+				RuleIDs: ruleIDs,
 			}
 			if ccErr := s.Engine.CoreAPI.CoreService().HostApplyRule().DeleteHostApplyRule(params.Context, params.Header, bizID, deleteRuleOption); ccErr != nil {
 				blog.ErrorJSON("SearchRuleRelatedModules failed, ListHostApplyRule failed, bizID: %s, listRuleOption: %s, rid: %s", bizID, listRuleOption, params.ReqID)
