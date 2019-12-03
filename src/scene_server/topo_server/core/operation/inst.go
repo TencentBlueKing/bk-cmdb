7ae8946e403b290f626ccd39bd90fcedb4adee13
vlon <790004707@qq.com>
2019-12-03 17:19:59 +0800

    fix: 删除实例代码bug

--- src/scene_server/topo_server/core/operation/inst.go
+++ src/scene_server/topo_server/core/operation/inst.go
@@ -425,7 +425,6 @@ func (c *commonInst) hasHost(params types.ContextParams, targetInst inst.Inst, c
 
 func (c *commonInst) DeleteInstByInstID(params types.ContextParams, obj model.Object, instID []int64, needCheckHost bool) error {
 	object := obj.Object()
-	objID := object.ID
 	objectID := object.ObjectID
 
 	cond := condition.CreateCondition()
@@ -460,30 +459,11 @@ func (c *commonInst) DeleteInstByInstID(params types.ContextParams, obj model.Ob
 		auditFilter := condition.CreateCondition().ToMapStr()
 		preAudit := NewSupplementary().Audit(params, c.clientSet, delInst.obj, c).CreateSnapshot(delInst.instID, auditFilter)
 
-		// if this instance has been bind to a instance by the association, then this instance should not be deleted.
-		innerCond := condition.CreateCondition()
-		innerCond.Field(common.BKAsstObjIDField).Eq(objID)
-		innerCond.Field(common.BKAsstInstIDField).Eq(delInst.instID)
-		err := c.asst.CheckBeAssociation(params, obj, innerCond)
-		if nil != err {
-			return err
-		}
-
-		// this instance has not be bind to another instance, we can delete all the associations it created
-		// by the association with other instances.
-		innerCond = condition.CreateCondition()
-		innerCond.Field(common.BKObjIDField).Eq(objID)
-		innerCond.Field(common.BKInstIDField).Eq(delInst.instID)
-		if err := c.asst.DeleteInstAssociation(params, innerCond); nil != err {
-			blog.Errorf("[operation-inst] failed to delete the inst asst, err: %s, rid: %s", err.Error(), params.ReqID)
-			return err
-		}
-
 		// delete this instance now.
 		delCond := condition.CreateCondition()
 		delCond.Field(delInst.obj.GetInstIDFieldName()).In(delInst.instID)
 		if delInst.obj.IsCommon() {
-			delCond.Field(common.BKObjIDField).Eq(objID)
+			delCond.Field(common.BKObjIDField).Eq(objectID)
 		}
 		// clear association
 		dc := &metadata.DeleteOption{Condition: delCond.ToMapStr()}
