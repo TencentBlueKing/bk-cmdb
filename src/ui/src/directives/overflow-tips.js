903cb94274652f5b45a75bc262b2546487e3e0d7
Daryl0819 <zhdgmax@qq.com>
2019-12-06 14:14:22 +0800

    minor: 代码优化

--- src/ui/src/directives/overflow-tips.js
+++ src/ui/src/directives/overflow-tips.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 
-const _this = Vue.prototype
-
 const instanceMap = new Map()
 
 const popoverOptions = {
@@ -16,7 +14,12 @@ const createDom = el => {
     const css = window.getComputedStyle(el, null)
     const dom = document.createElement('div')
     const width = parseFloat(css['width']) ? Math.ceil(parseFloat(css['width'])) + 'px' : css['width']
-    dom.style.cssText = `width: ${width}; line-height: ${css['line-height']}; font-size: ${css['font-size']}; word-break: ${css['word-break']}`
+    dom.style.cssText = `
+        width: ${width};
+        line-height: ${css['line-height']};
+        font-size: ${css['font-size']};
+        word-break: ${css['word-break']}
+    `
     dom.textContent = el.textContent
     return dom
 }
@@ -41,7 +44,7 @@ const setData = el => {
         textContent: el.textContent
     }
     if (isOverflow(el)) {
-        const instance = _this.$bkPopover(el, {
+        const instance = Vue.prototype.$bkPopover(el, {
             content: el.textContent,
             ...popoverOptions
         })
