05621bec9a104b39d4525a4afd0d040c112e0346
hoffermei <meihaifeng.hust@gmail.com>
2019-12-09 10:16:05 +0800

    支持按名称排序；规范分页参数校验方式 #3387
    Former-commit-id: b1043047bd56050dc9fa94165aa81bd3578257b8

--- src/scene_server/topo_server/service/inst_business.go
+++ src/scene_server/topo_server/service/inst_business.go
@@ -188,9 +188,20 @@ func (s *Service) UpdateBusinessStatus(params types.ContextParams, pathParams, q
 // 1. have any authorized resources in a business.
 // 2. only returned with a few field for this business info.
 func (s *Service) SearchReducedBusinessList(params types.ContextParams, pathParams, queryParams ParamsGetter, data mapstr.MapStr) (interface{}, error) {
+	page := metadata.BasePage{
+		Limit: common.BKNoLimit,
+	}
+	sortParam := queryParams("sort")
+	if len(sortParam) > 0 {
+		page.Sort = sortParam
+	}
+	if errKey, err := page.Validate(true); err != nil {
+		blog.Errorf("[api-biz] SearchReducedBusinessList failed, page parameter invalid, errKey: %s, err: %s, rid: %s", errKey, err.Error(), params.ReqID)
+		return nil, params.Err.Errorf(common.CCErrCommParamsInvalid, errKey)
+	}
 	query := &metadata.QueryBusinessRequest{
 		Fields: []string{common.BKAppIDField, common.BKAppNameField, "business_dept_id", "business_dept_name"},
-		Page:   metadata.BasePage{},
+		Page:   page,
 		Condition: mapstr.MapStr{
 			common.BKDataStatusField: mapstr.MapStr{common.BKDBNE: common.DataStatusDisabled},
 			common.BKDefaultField:    0,
@@ -551,6 +562,17 @@ func (s *Service) GetInternalModuleWithStatistics(params types.ContextParams, pa
 
 // ListAllBusinessSimplify list all businesses with return only several fields
 func (s *Service) ListAllBusinessSimplify(params types.ContextParams, pathParams, queryParams ParamsGetter, data mapstr.MapStr) (interface{}, error) {
+	page := metadata.BasePage{
+		Limit: common.BKNoLimit,
+	}
+	sortParam := queryParams("sort")
+	if len(sortParam) > 0 {
+		page.Sort = sortParam
+	}
+	if errKey, err := page.Validate(true); err != nil {
+		blog.Errorf("[api-biz] ListAllBusinessSimplify failed, page parameter invalid, errKey: %s, err: %s, rid: %s", errKey, err.Error(), params.ReqID)
+		return nil, params.Err.Errorf(common.CCErrCommParamsInvalid, errKey)
+	}
 
 	fields := []string{
 		common.BKAppIDField,
