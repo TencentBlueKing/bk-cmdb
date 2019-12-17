5766b461cec06f791ac10f5ff272b48148f7710d
Daryl0819 <zhdgmax@qq.com>
2019-12-17 21:20:24 +0800

    update:ui: 拓展cmdb-tips组件
    Former-commit-id: cfe6026c98c398d7345f333ab336cb245e7835c5

--- src/ui/src/components/ui/other/tips.vue
+++ src/ui/src/components/ui/other/tips.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="tips" :style="tipsStyle">
+    <div class="cmdb-tips" :style="tipsStyle">
         <i :class="icon" v-if="icon" :style="iconStyle"></i>
-        <p class="tips-content">
+        <p class="tips-content" :class="{ 'ellipsis': localEllipsis, 'pr20': showClose }">
             <slot></slot>
+            <a class="more" v-if="moreLink" :href="moreLink" target="_blank">{{$t('更多详情')}} &gt;&gt;</a>
         </p>
+        <i class="icon-cc-tips-close" v-if="showClose" @click="handleClose"></i>
     </div>
 </template>
 
@@ -22,29 +24,75 @@
             iconStyle: {
                 type: Object,
                 default: () => ({})
+            },
+            ellipsis: {
+                type: Boolean,
+                default: false
+            },
+            moreLink: {
+                type: String,
+                default: ''
+            },
+            showClose: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data () {
+            return {
+                localEllipsis: false
+            }
+        },
+        watch: {
+            moreLink (link) {
+                this.localEllipsis = link ? false : this.ellipsis
+            }
+        },
+        methods: {
+            handleClose () {
+                this.$emit('close')
             }
         }
     }
 </script>
 
 <style lang="scss" scoped>
-    .tips {
+    .cmdb-tips {
         display: flex;
-        height: 30px;
-        line-height: 30px;
-        font-size: 14px;
-        background: rgba(240, 248, 255, 1);
+        font-size: 12px;
+        background: #F0F8FF;
         border-radius: 2px;
-        border: 1px solid rgba(163, 197, 253, 1);
+        border: 1px solid #A3C5FD;
+        padding: 6px 16px;
         .icon {
-            flex: 18px 0 0;
-            font-size: 18px;
-            line-height: 30px;
+            flex: 16px 0 0;
+            text-align: center;
+            line-height: 16px;
+            font-size: 16px;
             color: #3A84FF;
-            margin: 0px 5px;
+            margin-right: 5px;
         }
         .tips-content {
-            @include ellipsis;
+            flex: 1;
+            &.ellipsis {
+                @include ellipsis;
+            }
+            .more {
+                display: inline-block;
+                margin-left: 20px;
+                color: #3A84FF;
+                &:hover {
+                    text-decoration: underline;
+                }
+            }
+        }
+        .icon-cc-tips-close {
+            align-self: center;
+            width: 14px;
+            height: 14px;
+            font-size: 14px;
+            color: #979BA5;
+            cursor: pointer;
         }
     }
 </style>
