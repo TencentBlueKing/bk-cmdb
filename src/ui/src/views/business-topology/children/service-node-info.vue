43af6dff67b923bd1d833df34b6c9a73165b21ce
yangyy <yang.bele@gmail.com>
2019-12-23 21:44:07 +0800

    业务拓扑中tab页disabled改为在内部提示
    Former-commit-id: c79c459fa49be3aac296c74ce90fe6542d5814c8

--- src/ui/src/views/business-topology/children/service-node-info.vue
+++ src/ui/src/views/business-topology/children/service-node-info.vue
@@ -218,7 +218,7 @@
                 immediate: true,
                 handler (active) {
                     if (active) {
-                        this.refresh()
+                        this.refresh && this.refresh()
                     }
                 }
             }
