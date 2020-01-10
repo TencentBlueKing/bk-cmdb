d2fae5c71bdefff1f3516bc94597099509122bd8
rentiansheng <rentiansheng@163.com>
2020-01-10 14:54:58 +0800

    refactor: delete not use  code issue #3759
    Former-commit-id: 7e72ffc0e9e2d14ae161f4e348162a882d29a5d0

--- src/web_server/middleware/user/plugins/method/skip_login/userinfo.go
+++ src/web_server/middleware/user/plugins/method/skip_login/userinfo.go
@@ -13,17 +13,10 @@
 package skip_login
 
 import (
-	"encoding/json"
 	"fmt"
 
-	"configcenter/src/apimachinery/util"
 	"configcenter/src/common"
-	"configcenter/src/common/blog"
-	"configcenter/src/common/http/httpclient"
 	"configcenter/src/common/metadata"
-	commonutil "configcenter/src/common/util"
-	"configcenter/src/thirdpartyclient/esbserver"
-	"configcenter/src/thirdpartyclient/esbserver/esbutil"
 	webCommon "configcenter/src/web_server/common"
 	"configcenter/src/web_server/middleware/user/plugins/manager"
 
@@ -38,42 +31,6 @@ func init() {
 		HandleFunc: &user{},
 	}
 	manager.RegisterPlugin(plugin)
-}
-
-type loginResultData struct {
-	UserName string `json:"username"`
-	ChName   string `json:"chname"`
-	Phone    string `json:"Phone"`
-	Email    string `json:"email"`
-	Role     string `json:"role"`
-	Language string `json:"language"`
-	OwnerUin string `json:"owner_uin"`
-}
-
-type loginResult struct {
-	Message string
-	Code    string
-	Result  bool
-	Data    *loginResultData
-}
-
-type userListResult struct {
-	Message string     `json:"message"`
-	Data    []userInfo `json:"data"`
-	Code    string     `json:"code"`
-	Result  bool       `json:"result"`
-}
-
-type userInfo struct {
-	UserName string `json:"username"`
-	QQ       string `json:"qq"`
-	Role     string `json:"role"`
-	Language string `json:"language"`
-	Phone    string `json:"phone"`
-	WxUserid string `json:"wx_userid"`
-	Email    string `json:"email"`
-	Chname   string `json:"chname"`
-	TimeZone string `json:"time_zone"`
 }
 
 type user struct {
@@ -106,107 +63,17 @@ func (m *user) LoginUser(c *gin.Context, config map[string]string, isMultiOwner 
 	return user, true
 }
 
-func (m *user) getEsbClient(config map[string]string) (esbserver.EsbClientInterface, error) {
-	esbAddr, addrOk := config["esb.addr"]
-	esbAppCode, appCodeOk := config["esb.appCode"]
-	esbAppSecret, appSecretOk := config["esb.appSecret"]
-	if addrOk == false || appCodeOk == false || appSecretOk == false {
-		return nil, fmt.Errorf("esb config not found or incomplete, %+v", config)
-	}
-	tlsConfig, err := util.NewTLSClientConfigFromConfig("esb", config)
-	if err != nil {
-		return nil, fmt.Errorf("parse esb tls config failed, config: %+v, err: %+v", config, err)
-	}
-	apiMachineryConfig := &util.APIMachineryConfig{
-		QPS:       1000,
-		Burst:     1000,
-		TLSConfig: &tlsConfig,
-	}
-	defaultCfg := &esbutil.EsbConfig{
-		Addrs:     esbAddr,
-		AppCode:   esbAppCode,
-		AppSecret: esbAppSecret,
-	}
-	esbSrv, err := esbserver.NewEsb(apiMachineryConfig, nil, defaultCfg, nil)
-	if err != nil {
-		return nil, fmt.Errorf("create esb client failed. err: %v", err)
-	}
-	return esbSrv, nil
-}
-
 // GetUserList get user list from paas
 func (m *user) GetUserList(c *gin.Context, config map[string]string) ([]*metadata.LoginSystemUserInfo, error) {
-	rid := commonutil.GetHTTPCCRequestID(c.Request.Header)
-	accountURL, ok := config["site.bk_account_url"]
-	if !ok {
-		// try to use esb user list api
-		esbClient, err := m.getEsbClient(config)
-		if err != nil {
-			blog.Warnf("get esb client failed, err: %+v, rid: %s", err, rid)
-			return nil, fmt.Errorf("config site.bk_account_url not found")
-		}
-		result, err := esbClient.User().ListUsers(c.Request.Context(), c.Request.Header)
-		if err != nil {
-			blog.Warnf("get users by esb client failed, http failed, err: %+v, rid: %s", err, rid)
-			return nil, fmt.Errorf("get users by esb client failed, http failed")
-		}
-		users := make([]*metadata.LoginSystemUserInfo, 0)
-		for _, userInfo := range result.Data {
-			user := &metadata.LoginSystemUserInfo{
-				CnName: userInfo.DisplayName,
-				EnName: userInfo.Username,
-			}
-			users = append(users, user)
-		}
-		return users, nil
-	}
-	session := sessions.Default(c)
-	skipLogin := session.Get(webCommon.IsSkipLogin)
-	skipLogins, ok := skipLogin.(string)
-	if ok && "1" == skipLogins {
-		blog.V(5).Infof("use skip login flag: %v, rid: %s", skipLogin, rid)
-		adminData := []*metadata.LoginSystemUserInfo{
-			{
-				CnName: "admin",
-				EnName: "admin",
-			},
-		}
 
-		return adminData, nil
+	adminData := []*metadata.LoginSystemUserInfo{
+		{
+			CnName: "admin",
+			EnName: "admin",
+		},
 	}
 
-	token := session.Get(common.HTTPCookieBKToken)
-	getURL := fmt.Sprintf(accountURL, token)
-	httpClient := httpclient.NewHttpClient()
-
-	if err := httpClient.SetTlsNoVerity(); err != nil {
-		blog.Warnf("httpClient.SetTlsNoVerity failed, err: %s, rid: %s", err.Error(), rid)
-	}
-	reply, err := httpClient.GET(getURL, nil, nil)
-
-	if nil != err {
-		blog.Errorf("get user list error：%v, rid: %s", err, rid)
-		return nil, fmt.Errorf("http do error:%s", err.Error())
-	}
-	blog.V(5).Infof("get user list url: %s, return：%s, rid: %s", getURL, reply, rid)
-	var result userListResult
-	err = json.Unmarshal([]byte(reply), &result)
-	if nil != err || false == result.Result {
-		blog.Errorf("get user list error：%v, error code:%s, error message: %s, rid: %s", err, result.Code, result.Message, rid)
-		return nil, fmt.Errorf("get user list reply error")
-	}
-	userListArr := make([]*metadata.LoginSystemUserInfo, 0)
-	for _, user := range result.Data {
-		cellData := make(map[string]interface{})
-		cellData["chinese_name"] = user.Chname
-		cellData["english_name"] = user.UserName
-		userListArr = append(userListArr, &metadata.LoginSystemUserInfo{
-			CnName: user.Chname,
-			EnName: user.UserName,
-		})
-	}
-
-	return userListArr, nil
+	return adminData, nil
 }
 
 func (m *user) GetLoginUrl(c *gin.Context, config map[string]string, input *metadata.LogoutRequestParams) string {
