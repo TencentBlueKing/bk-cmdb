d57456af3272f21955e6706cb5c91856f9bddc20
wcy00000000000000 <2269766985@qq.com>
2020-01-10 12:12:28 +0800

    内置服务分类添加翻译
    Former-commit-id: be3b1d2277050546b7f8b32f33611ebbbdb0c594

--- src/source_controller/coreservice/service/translate.go
+++ src/source_controller/coreservice/service/translate.go
@@ -14,6 +14,7 @@ package service
 
 import (
 	"context"
+	"strings"
 
 	"configcenter/src/common"
 	"configcenter/src/common/blog"
@@ -74,4 +75,8 @@ func (s *coreService) TranslateClassificationName(defLang language.DefaultCCLang
 
 func (s *coreService) TranslateOperationChartName(defLang language.DefaultCCLanguageIf, att metadata.ChartConfig) string {
 	return util.FirstNotEmptyString(defLang.Language("operation_chart_"+att.ReportType), att.Name, att.ReportType)
+}
+
+func (s *coreService) TranslateServiceCategory(defLang language.DefaultCCLanguageIf, att *metadata.ServiceCategory) string {
+	return util.FirstNotEmptyString(defLang.Language("service_category_"+strings.Replace(att.Name, " ", "_", -1)), att.Name)
 }
