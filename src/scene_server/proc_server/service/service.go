1f23937b2a8587b646481f627aa19a87ec903b64
wcy00000000000000 <2269766985@qq.com>
2020-05-07 21:55:15 +0800

    healthz依赖组件更新

--- src/scene_server/proc_server/service/service.go
+++ src/scene_server/proc_server/service/service.go
@@ -188,7 +188,7 @@ func (ps *ProcServer) Healthz(req *restful.Request, resp *restful.Response) {
 	}
 
 	info := metric.HealthInfo{
-		Module:     types.CC_MODULE_HOST,
+		Module:     types.CC_MODULE_PROC,
 		HealthMeta: meta,
 		AtTime:     metadata.Now(),
 	}
