d8dcaf90cbc24de93c1c87ab07ec8e3310b58ced
abstractmj <abstractmj@qq.com>
2020-05-12 22:17:02 +0800

    feature: add upgrade function for container server;

--- src/scene_server/admin_server/imports.go
+++ src/scene_server/admin_server/imports.go
@@ -87,5 +87,5 @@ import (
 	_ "configcenter/src/scene_server/admin_server/upgrader/y3.8.202004151435"
 	_ "configcenter/src/scene_server/admin_server/upgrader/y3.8.202004241035"
 	_ "configcenter/src/scene_server/admin_server/upgrader/y3.8.202004291536"
-	_ "configcenter/src/scene_server/admin_server/upgrader/y3.8.202005151212"
+	_ "configcenter/src/scene_server/admin_server/upgrader/y3.8.202005121212"
 )
