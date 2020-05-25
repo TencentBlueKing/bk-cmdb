ba4c9980983bb237ae4fc21e65ceceedcdf0011e
Dravening <282864784@qq.com>
2020-05-25 13:38:17 +0800

    fix: 查询已归档业务时，忽略大小写

--- src/ui/src/views/business/archived.vue
+++ src/ui/src/views/business/archived.vue
@@ -159,7 +159,7 @@
                     }
                 }
                 if (this.filter.name) {
-                    params.condition.bk_biz_name = { '$regex': this.filter.name }
+                    params.condition.bk_biz_name = this.filter.name
                 }
                 return params
             },
