c9ee9eeaa4faf8b0ef75c0270148993bea923d63
wcy00000000000000 <2269766985@qq.com>
2020-05-28 21:21:37 +0800

    定义将主机特殊字段数组解析为字符串的结构 #4265

---
+++ src/common/metadata/host.go
@@ -0,0 +1,168 @@
+/*
+ * Tencent is pleased to support the open source community by making 蓝鲸 available.,
+ * Copyright (C) 2017,-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the ",License",); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an ",AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package metadata
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"configcenter/src/common"
+	"configcenter/src/common/blog"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/bsoncodec"
+	"go.mongodb.org/mongo-driver/bson/bsonrw"
+	"go.mongodb.org/mongo-driver/bson/bsontype"
+	"go.mongodb.org/mongo-driver/x/bsonx"
+	"go.mongodb.org/mongo-driver/x/bsonx/bsoncore"
+)
+
+// host map with string type ip and operator, can only get host from db with this map
+type HostMapStr map[string]interface{}
+
+func (h *HostMapStr) UnmarshalBSON(b []byte) error {
+	if h == nil {
+		return bsonx.ErrNilDocument
+	}
+	elements, err := bsoncore.Document(b).Elements()
+	if err != nil {
+		return err
+	}
+
+	if *h == nil {
+		*h = map[string]interface{}{}
+	}
+	for _, element := range elements {
+		rawValue := element.Value()
+		switch element.Key() {
+		case common.BKHostInnerIPField:
+			innerIP, err := parseBsonStringArrayValueToString(rawValue)
+			if err != nil {
+				return err
+			}
+			(*h)[common.BKHostInnerIPField] = innerIP
+		case common.BKHostOuterIPField:
+			outerIP, err := parseBsonStringArrayValueToString(rawValue)
+			if err != nil {
+				return err
+			}
+			(*h)[common.BKHostOuterIPField] = outerIP
+		case common.BKOperatorField:
+			operator, err := parseBsonStringArrayValueToString(rawValue)
+			if err != nil {
+				return err
+			}
+			(*h)[common.BKOperatorField] = operator
+		case common.BKBakOperatorField:
+			bakOperator, err := parseBsonStringArrayValueToString(rawValue)
+			if err != nil {
+				return err
+			}
+			(*h)[common.BKBakOperatorField] = bakOperator
+		default:
+			dc := bsoncodec.DecodeContext{Registry: bson.DefaultRegistry}
+			vr := bsonrw.NewBSONValueReader(rawValue.Type, rawValue.Data)
+			decoder, err := bson.NewDecoderWithContext(dc, vr)
+			if err != nil {
+				return err
+			}
+			value := new(interface{})
+			err = decoder.Decode(value)
+			if err != nil {
+				return err
+			}
+			(*h)[element.Key()] = *value
+		}
+	}
+	return nil
+}
+
+func parseBsonStringArrayValueToString(value bsoncore.Value) (string, error) {
+	switch value.Type {
+	case bsontype.Array:
+		rawArray, rem, ok := bsoncore.ReadArray(value.Data)
+		if !ok {
+			return "", bsoncore.NewInsufficientBytesError(value.Data, rem)
+		}
+		array, err := rawArray.Values()
+		if err != nil {
+			return "", err
+		}
+		var buf bytes.Buffer
+		arrayLen := len(array)
+		for index, arrayValue := range array {
+			if arrayValue.Type != bsontype.String {
+				return "", fmt.Errorf("invalid BSON type %v", arrayValue.Type)
+			}
+			str, rem, ok := bsoncore.ReadString(arrayValue.Data)
+			if !ok {
+				return "", bsoncore.NewInsufficientBytesError(arrayValue.Data, rem)
+			}
+			buf.WriteString(str)
+			if index != arrayLen-1 {
+				buf.WriteByte(',')
+			}
+		}
+		return buf.String(), nil
+	case bsontype.String:
+		str, rem, ok := bsoncore.ReadString(value.Data)
+		if !ok {
+			return "", bsoncore.NewInsufficientBytesError(value.Data, rem)
+		}
+		return str, nil
+	case bsontype.Null:
+		return "", nil
+	default:
+		return "", fmt.Errorf("invalid BSON type %v", value.Type)
+	}
+}
+
+// special field whose string array value is parsed into string value from db
+type StringArrayToString string
+
+func (s *StringArrayToString) UnmarshalBSONValue(typo bsontype.Type, raw []byte) error {
+	if s == nil {
+		return bsonx.ErrNilDocument
+	}
+	value := bsoncore.Value{
+		Type: typo,
+		Data: raw,
+	}
+	str, err := parseBsonStringArrayValueToString(value)
+	if err != nil {
+		return err
+	}
+	*s = StringArrayToString(str)
+	return err
+}
+
+// convert host ip and operator fields value from string to array
+func ConvertHostSpecialStringToArray(host map[string]interface{}) map[string]interface{} {
+	specialFields := []string{common.BKHostInnerIPField, common.BKHostOuterIPField, common.BKOperatorField, common.BKBakOperatorField}
+	for _, field := range specialFields {
+		switch value := host[field].(type) {
+		case string:
+			if len(value) == 0 {
+				host[field] = make([]string, 0)
+			} else {
+				host[field] = strings.Split(value, ",")
+			}
+		case []string:
+		case nil:
+		default:
+			blog.Errorf("host %s type invalid, value %v", field, host[field])
+		}
+	}
+	return host
+}
