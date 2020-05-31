ca0d153273534258e8e3e07c4f8061cb196ac78a
abstractmj <abstractmj@qq.com>
2020-06-01 00:19:03 +0800

    fix: modify bk_pod_labels to accept mesos labels

--- src/common/pattern.go
+++ src/common/pattern.go
@@ -25,7 +25,7 @@ const (
 	PatternPort = `(([1-9][0-9]{0,3})|([1-5][0-9]{4})|(6[0-4][0-9]{3})|(65[0-4][0-9]{2})|(655[0-2][0-9])|(6553[0-5]))`
 
 	// PatternPodLabels regular pattern for pod labels
-	PatternPodLabels = `^\s*\{(\s*\"[\S]+\":\s*\"[\S]+\"\s*(,\s*\"[\S]+\":\s*\"[\S]+\"\s*)?)?\}\s*$`
+	PatternPodLabels = `^\s*\{(\s*\"[\S]+\":\s*\"[\S ]+\"\s*(,\s*\"[\S]+\":\s*\"[\S ]+\"\s*)?)?\}\s*$`
 	// PatternPodAnnotations regular pattern for pod annotations
 	PatternPodAnnotations = PatternPodLabels
 )
