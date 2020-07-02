c87adec8e919ddb02224329cc2e5e2fb23a2ff55
LieLieLiekey <1249553079@qq.com>
2020-07-02 19:41:31 +0800

    改变代码风格,使之符合代码规范

--- src/scene_server/datacollection/collections/hostsnap/ginkgo_parsesetter_test.go
+++ src/scene_server/datacollection/collections/hostsnap/ginkgo_parsesetter_test.go
@@ -1,3 +1,15 @@
+/*
+ * Tencent is pleased to support the open source community by making 蓝鲸 available.
+ * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package hostsnap
 
 import (
@@ -5,6 +17,7 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/tidwall/gjson"
 )
+
 const defaultjson = `{
     "data":
         {
@@ -27,18 +40,22 @@ const defaultjson = `{
                 }
         }
 }`
-//使用parse函数得到disk
+
+// 使用parse函数得到disk.
 func getDiskFromParse(value *gjson.Result) (uint64,string){
 	setter ,str:= parseSetter(value,"127.0.0.1","0.0.0.0")
-	// 获取bk_disk字段
+	
+	// 获取bk_disk字段.
 	cv,ok:= setter["bk_disk"]
 	Expect(ok).To(BeTrue())
-	// 将结果转化为uint64
+	
+	// 将结果转化为uint64.
 	results,ok := cv.(uint64)
 	Expect(ok).To(BeTrue())
 	return results,str
 }
-//自己计算disk
+
+// 自己计算disk.
 func getDiskFromSelf(value *gjson.Result) uint64{
 	var sum uint64
 	arr := value.Get("data.disk.usage.#.total").Array()
@@ -47,27 +64,33 @@ func getDiskFromSelf(value *gjson.Result) uint64{
 	}
 	return sum
 }
-//使用parse函数得到mem
+
+// 使用parse函数得到mem.
 func getMemFromParse(value *gjson.Result) (uint64,string){
 	setter ,str:= parseSetter(value,"127.0.0.1","0.0.0.0")
-	// 获取bk_disk字段
+	
+	// 获取bk_disk字段.
 	cv,ok:= setter["bk_mem"]
 	Expect(ok).To(BeTrue())
-	// 将结果转化为uint64
+	
+	// 将结果转化为uint64.
 	results,ok := cv.(uint64)
 	Expect(ok).To(BeTrue())
 	return results,str
 }
-//自己计算mem
+
+// 自己计算mem.
 func getMemFromSelf(value *gjson.Result) uint64{
 	return  value.Get("data.mem.meminfo.total").Uint() >> 10 >> 10
 }
+
 var _ = Describe("Hostsnap", func() {
 	Context("test key-disk", func() {
 		It("", func() {
-			// 解析为gjson.Result
+			// 解析为gjson.Result.
 			gson := gjson.Parse(defaultjson)
-			// 求出parse函数得出的结果与理论结果
+			
+			// 求出parse函数得出的结果与理论结果.
 			shouldout :=getDiskFromSelf(&gson)
 			result,_ := getDiskFromParse(&gson)
 
@@ -75,11 +98,13 @@ var _ = Describe("Hostsnap", func() {
 
 		})
 	})
+
 	Context("test key-mem",func(){
 		It("", func() {
-			// 解析为gjson.Result
+			// 解析为gjson.Result.
 			gson := gjson.Parse(defaultjson)
-			// 求出parse函数得出的结果与理论结果
+			
+			// 求出parse函数得出的结果与理论结果.
 			shouldout := getMemFromSelf(&gson)
 			result,_:=getMemFromParse(&gson)
 
