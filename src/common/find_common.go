f9e07c4164c55878e5bde15449e7a460552a5c8d
guohuliu <1774687347@qq.com>
2020-08-24 19:30:35 +0800

    FEATURE：Upgrade elastic 5->7 and modify the original synchronization scheme

--- src/common/find_common.go
+++ src/common/find_common.go
@@ -1,26 +1,33 @@
 package common
 
+import (
+	"fmt"
+	"strings"
+)
+
 const (
 	CMDBINDEX       = "cmdb"
-	INDICES         = "indices"
 	TypeHost        = "host"
 	TypeObject      = "object"
-	TypeModel       = "model"
-	TypeProcess     = "process"
 	TypeApplication = "biz"
 
-	TypeAggName  = "type_agg"
-	TypeAggField = "_type"
+	IndexAggName  = "index_agg"
+	IndexAggField = "_index"
 
-	BkObjIdAggName    = "bk_obj_id_agg"
-	BkObjIdAggField   = "bk_obj_id.keyword"
 	BkBizMetaKey      = "metadata.label.bk_biz_id"
 	BkSupplierAccount = "bk_supplier_account"
 )
 
 var (
-	CmdbFindTypes = []string{BKTableNameBaseInst, BKTableNameBaseHost, BKTableNameObjDes}
+	CmdbFindTypes = []string{BKTableNameBaseInst, BKTableNameBaseHost}
 	SpecialChar   = []string{"`", "~", "!", "@", "#", "$", "%", "^", "&", "*",
 		"(", ")", "-", "_", "=", "+", "[", "{", "]", "}",
 		"\\", "|", ";", ":", "'", "\"", ",", "<", ".", ">", "/", "?"}
 )
+
+// GetIndexName get the index of es through mongo's db and collection
+func GetIndexName(dbName string, collectionName string) string {
+	dbName = strings.ToLower(dbName)
+	collectionName = strings.ToLower(collectionName)
+	return fmt.Sprintf("%s.%s", dbName, collectionName)
+}
