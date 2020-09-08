292216b55307cb73d75438569f4eb6dec24cedee
peibiaoyang <itsfantacy@hotmail.com>
2020-09-08 21:03:46 +0800

    update:ui: 因nextTick问题，回滚vue版本至2.5.22

--- src/ui/src/views/resource/transfer/host-store.js
+++ src/ui/src/views/resource/transfer/host-store.js
@@ -1,63 +1,68 @@
 import Vue from 'vue'
-class HostStore {
-    constructor () {
-        this.hosts = []
-        this.businessList = []
-    }
-
-    get isSelected () {
-        return !!this.hosts.length
-    }
-
-    get bizSet () {
-        const bizSet = new Set()
-        this.hosts.forEach(host => {
-            const [biz] = host.biz
-            bizSet.add(biz.bk_biz_id)
-        })
-        return bizSet
-    }
-
-    get isSameBiz () {
-        return this.bizSet.size === 1
-    }
-    get isAllIdleModule () {
-        return this.hosts.every(host => {
-            const [module] = host.module
-            return module.default === 1
-        })
-    }
-
-    get isAllIdleSet () {
-        return this.hosts.every(host => {
-            const [module] = host.module
-            return module.default !== 0
-        })
-    }
-
-    get uniqueBusiness () {
-        if (this.isSameBiz) {
-            const [bizId] = Array.from(this.bizSet)
-            return this.businessList.find(business => business.bk_biz_id === bizId)
+const store = new Vue({
+    data () {
+        return {
+            hosts: [],
+            businessList: []
         }
-        return null
-    }
+    },
+    computed: {
+        isSelected () {
+            return !!this.hosts.length
+        },
+    
+        bizSet () {
+            const bizSet = new Set()
+            this.hosts.forEach(host => {
+                const [biz] = host.biz
+                bizSet.add(biz.bk_biz_id)
+            })
+            return bizSet
+        },
+    
+        isSameBiz () {
+            return this.bizSet.size === 1
+        },
 
-    clear () {
-        this.hosts = []
+        isAllIdleModule () {
+            return this.hosts.every(host => {
+                const [module] = host.module
+                return module.default === 1
+            })
+        },
+    
+        isAllIdleSet () {
+            return this.hosts.every(host => {
+                const [module] = host.module
+                return module.default !== 0
+            })
+        },
+    
+        uniqueBusiness () {
+            if (this.isSameBiz) {
+                const [bizId] = Array.from(this.bizSet)
+                return this.businessList.find(business => business.bk_biz_id === bizId)
+            }
+            return null
+        }
+    },
+    methods: {
+        clear () {
+            this.hosts = []
+        },
+    
+        setSelected (hosts = []) {
+            this.hosts = hosts
+        },
+    
+        getSelected () {
+            return this.hosts
+        },
+    
+        setBusinessList (businessList) {
+            this.businessList = businessList
+        }
     }
+})
 
-    setSelected (hosts = []) {
-        this.hosts = hosts
-    }
-
-    getSelected () {
-        return this.hosts
-    }
-
-    setBusinessList (businessList) {
-        this.businessList = businessList
-    }
-}
-
-export default Vue.observable(new HostStore())
+export default store
