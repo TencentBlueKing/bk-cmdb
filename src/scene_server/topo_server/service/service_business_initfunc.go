ff668d325fd4b3cf6ad545b7cce72444f5b43781
Dravening <282864784@qq.com>
2020-09-08 19:55:21 +0800

    feature: 增加查询实例所有关联接口(将实例视为关联源模型和关联目标模型)。

--- src/scene_server/topo_server/service/service_business_initfunc.go
+++ src/scene_server/topo_server/service/service_business_initfunc.go
@@ -140,6 +140,7 @@ func (s *Service) initBusinessAssociation(web *restful.WebService) {
 
 	// inst association methods
 	utility.AddHandler(rest.Action{Verb: http.MethodPost, Path: "/find/instassociation", Handler: s.SearchAssociationInst})
+	utility.AddHandler(rest.Action{Verb: http.MethodPost, Path: "/find/instassociation/related", Handler: s.SearchAssociationRelatedInst})
 	utility.AddHandler(rest.Action{Verb: http.MethodPost, Path: "/create/instassociation", Handler: s.CreateAssociationInst})
 	utility.AddHandler(rest.Action{Verb: http.MethodDelete, Path: "/delete/instassociation/{association_id}", Handler: s.DeleteAssociationInst})
 
