164b55f40f301d3f5f9f6e589b0ca85e59ed12ac
peibiaoyang <itsfantacy@hotmail.com>
2020-09-09 22:52:20 +0800

    uodate:ui: 调整云账户与云任务在审计中的tab位置

--- src/ui/src/views/audit/children/audit-target-selector.vue
+++ src/ui/src/views/audit/children/audit-target-selector.vue
@@ -47,10 +47,12 @@
                     ]),
                     resource: new Set([
                         'business',
-                        'cloud_area',
                         'model_instance',
                         'instance_association',
-                        'resource_directory'
+                        'resource_directory',
+                        'cloud_area',
+                        'cloud_account',
+                        'cloud_sync_task'
                     ]),
                     other: new Set([
                         'model_group',
@@ -60,9 +62,7 @@
                         'model_association',
                         'model_attribute_group',
                         'event',
-                        'association_kind',
-                        'cloud_account',
-                        'cloud_sync_task'
+                        'association_kind'
                     ])
                 })
             }
