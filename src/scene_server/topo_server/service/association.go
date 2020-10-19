566ee87a0a7830a8780d7f9d94526f4fdd5e50a7
Dravening <282864784@qq.com>
2020-10-19 15:36:53 +0800

    refactor: 规范代码，修正生成错误的函数;函数边界控制的值改用常量。

--- src/scene_server/topo_server/service/association.go
+++ src/scene_server/topo_server/service/association.go
@@ -380,24 +380,24 @@ func (s *Service) SearchAssociationInst(ctx *rest.Contexts) {
 func (s *Service) SearchAssociationRelatedInst(ctx *rest.Contexts) {
 	request := &metadata.SearchAssociationRelatedInstRequest{}
 	if err := ctx.DecodeInto(request); err != nil {
-		ctx.RespAutoError(ctx.Kit.CCError.New(common.CCErrCommParamsInvalid, err.Error()))
+		ctx.RespAutoError(ctx.Kit.CCError.Errorf(common.CCErrCommParamsInvalid, err.Error()))
 		return
 	}
 	//check condition
 	if request.Condition.InstID == 0 || request.Condition.ObjectID == "" {
-		ctx.RespAutoError(ctx.Kit.CCError.New(common.CCErrCommParamsInvalid, "'bk_inst_id' and 'bk_obj_id' should not be empty."))
+		ctx.RespAutoError(ctx.Kit.CCError.Errorf(common.CCErrCommParamsInvalid, "'bk_inst_id' and 'bk_obj_id' should not be empty."))
 		return
 	}
 	//check fields,if there's none param,return err.
 	if len(request.Fields) == 0 {
-		ctx.RespAutoError(ctx.Kit.CCError.New(common.CCErrCommParamsInvalid, "there should be at least one param in 'fields'."))
+		ctx.RespAutoError(ctx.Kit.CCError.Errorf(common.CCErrCommParamsInvalid, "there should be at least one param in 'fields'."))
 		return
 	}
 	//Use id as sort parameters
 	request.Page.Sort = common.BKFieldID
 	//check Maximum limit
-	if request.Page.Limit > 500 {
-		ctx.RespAutoError(ctx.Kit.CCError.New(common.CCErrCommParamsInvalid, "The maximum limit should be less than 500."))
+	if request.Page.Limit > common.BKMaxInstanceLimit {
+		ctx.RespAutoError(ctx.Kit.CCError.Errorf(common.CCErrCommParamsInvalid, "The maximum limit should be less than 500."))
 		return
 	}
 
@@ -407,7 +407,7 @@ func (s *Service) SearchAssociationRelatedInst(ctx *rest.Contexts) {
 		return
 	}
 
-	if ret.Code != 0 {
+	if err := ret.CCError(); err != nil {
 		ctx.RespAutoError(ctx.Kit.CCError.New(ret.Code, ret.ErrMsg))
 		return
 	}
