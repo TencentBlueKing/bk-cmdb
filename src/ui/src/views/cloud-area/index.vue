9950d86f174a4c3b08e833af5d91db3b57b8ceea
peibiaoyang <itsfantacy@hotmail.com>
2020-10-19 21:59:51 +0800

    optimize:ui: 云区域主机数量异步获取

--- src/ui/src/views/cloud-area/index.vue
+++ src/ui/src/views/cloud-area/index.vue
@@ -73,7 +73,9 @@
                 <template slot-scope="{ row }">{{getVpcInfo(row) | formatter('singlechar')}}</template>
             </bk-table-column>
             <bk-table-column :label="$t('主机数量')" prop="host_count" show-overflow-tooltip>
-                <template slot-scope="{ row }">{{row.host_count | formatter('int')}}</template>
+                <template slot-scope="{ row }">
+                    <loading :loading="!row.hasOwnProperty('host_count')">{{row.host_count | formatter('int')}}</loading>
+                </template>
             </bk-table-column>
             <bk-table-column :label="$t('最近编辑')" prop="last_time" sortable="custom" show-overflow-tooltip>
                 <template slot-scope="{ row }">{{row.last_time | formatter('time')}}</template>
@@ -81,19 +83,21 @@
             <bk-table-column :label="$t('编辑人')" prop="bk_last_editor" show-overflow-tooltip></bk-table-column>
             <bk-table-column :label="$t('操作')" fixed="right">
                 <template slot-scope="{ row }">
-                    <cmdb-auth
-                        :ignore="row.bk_account_id === 0"
-                        :auth="{ type: $OPERATION.D_CLOUD_AREA, relation: [row.bk_cloud_id] }">
-                        <link-button slot-scope="{ disabled }"
-                            :disabled="!isRemovable(row) || disabled"
-                            v-bk-tooltips="{
-                                disabled: isRemovable(row) || disabled,
-                                content: getRemoveTips(row)
-                            }"
-                            @click="handleDelete(row)">
-                            {{$t('删除')}}
-                        </link-button>
-                    </cmdb-auth>
+                    <loading :loading="!row.hasOwnProperty('host_count')">
+                        <cmdb-auth
+                            :ignore="row.bk_account_id === 0"
+                            :auth="{ type: $OPERATION.D_CLOUD_AREA, relation: [row.bk_cloud_id] }">
+                            <link-button slot-scope="{ disabled }"
+                                :disabled="!isRemovable(row) || disabled"
+                                v-bk-tooltips="{
+                                    disabled: isRemovable(row) || disabled,
+                                    content: getRemoveTips(row)
+                                }"
+                                @click="handleDelete(row)">
+                                {{$t('删除')}}
+                            </link-button>
+                        </cmdb-auth>
+                    </loading>
                 </template>
             </bk-table-column>
         </bk-table>
@@ -102,13 +106,15 @@
 
 <script>
     import CmdbVendor from '@/components/ui/other/vendor'
+    import Loading from '@/components/loading/loading.vue'
     import throttle from 'lodash.throttle'
     import { MENU_RESOURCE_CLOUD_RESOURCE } from '@/dictionary/menu-symbol'
     import TaskRegionSelector from '@/views/cloud-resource/children/task-region-selector'
     export default {
         components: {
             CmdbVendor,
-            TaskRegionSelector
+            TaskRegionSelector,
+            Loading
         },
         data () {
             return {
@@ -117,7 +123,8 @@
                 pagination: this.$tools.getDefaultPaginationConfig(),
                 sort: 'bk_cloud_id',
                 request: {
-                    search: Symbol('search')
+                    search: Symbol('search'),
+                    count: []
                 },
                 scheduleSearch: throttle(this.handlePageChange, 800, { leading: false, trailing: true }),
                 rowInEdit: null
@@ -238,10 +245,46 @@
                     }
                     this.list = data.info
                     this.pagination.count = data.count
+                    data.count && this.getHostCount()
                 } catch (e) {
                     console.error(e)
                     this.list = []
                     this.pagination.count = 0
+                }
+            },
+            async getHostCount () {
+                try {
+                    this.$http.cancelRequest(this.request.count.splice(0))
+                    const reduceIds = this.list.reduce((accumulator, current) => {
+                        let last = accumulator.pop() || []
+                        if (last.length < 50) {
+                            last.push(current.bk_cloud_id)
+                        } else {
+                            last = [current.bk_cloud_id]
+                        }
+                        accumulator.push(last)
+                        return accumulator
+                    }, [])
+                    const results = await Promise.all(reduceIds.map(ids => {
+                        const requestId = Symbol('count')
+                        this.request.count.push(requestId)
+                        return this.$store.dispatch('cloud/area/getHostCount', {
+                            params: {
+                                bk_cloud_ids: ids
+                            },
+                            config: {
+                                requestId: requestId
+                            }
+                        })
+                    }))
+                    results.forEach(result => {
+                        result.forEach(data => {
+                            const row = this.list.find(row => row.bk_cloud_id === data.bk_cloud_id) || {}
+                            this.$set(row, 'host_count', data.host_count)
+                        })
+                    })
+                } catch (error) {
+                    console.error(error)
                 }
             },
             getVpcInfo (row) {
