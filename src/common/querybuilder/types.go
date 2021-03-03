0286724e0071994225d1ccbaf60b63fd78ad076c
wcy00000000000000 <2269766985@qq.com>
2021-03-03 16:35:04 +0800

    修复解析到$not的条件为字符串的问题

--- src/common/querybuilder/types.go
+++ src/common/querybuilder/types.go
@@ -281,7 +281,7 @@ func (r AtomRule) ToMgo() (mgoFiler map[string]interface{}, key string, err erro
 		}
 	case OperatorNotBeginsWith:
 		filter[r.Field] = map[string]interface{}{
-			common.BKDBNot: fmt.Sprintf("^%s", r.Value),
+			common.BKDBNot: map[string]interface{}{common.BKDBLIKE: fmt.Sprintf("^%s", r.Value)},
 		}
 	case OperatorContains:
 		filter[r.Field] = map[string]interface{}{
@@ -289,7 +289,7 @@ func (r AtomRule) ToMgo() (mgoFiler map[string]interface{}, key string, err erro
 		}
 	case OperatorNotContains:
 		filter[r.Field] = map[string]interface{}{
-			common.BKDBNot: fmt.Sprintf("%s", r.Value),
+			common.BKDBNot: map[string]interface{}{common.BKDBLIKE: fmt.Sprintf("%s", r.Value)},
 		}
 	case OperatorsEndsWith:
 		filter[r.Field] = map[string]interface{}{
@@ -297,7 +297,7 @@ func (r AtomRule) ToMgo() (mgoFiler map[string]interface{}, key string, err erro
 		}
 	case OperatorNotEndsWith:
 		filter[r.Field] = map[string]interface{}{
-			common.BKDBNot: fmt.Sprintf("%s$", r.Value),
+			common.BKDBNot: map[string]interface{}{common.BKDBLIKE: fmt.Sprintf("%s$", r.Value)},
 		}
 	case OperatorIsEmpty:
 		// array empty
@@ -339,7 +339,7 @@ type CombinedRule struct {
 
 var (
 	// 嵌套层级的深度按树的高度计算，查询条件最大深度为3即最多嵌套2层
-	MaxDeep           = 3
+	MaxDeep = 3
 )
 
 func (r CombinedRule) GetDeep() int {
