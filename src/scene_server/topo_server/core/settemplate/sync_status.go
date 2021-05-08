e2c9f03b09709e075474fbf5a3f15bb50fee38e8
rentiansheng <rentiansheng@163.com>
2021-05-08 19:48:44 +0800

    feature: display set template instance use set base info  issue #5238

--- src/scene_server/topo_server/core/settemplate/sync_status.go
+++ src/scene_server/topo_server/core/settemplate/sync_status.go
@@ -264,3 +264,78 @@ func (st *setTemplate) TriggerCheckSetTemplateSyncingStatus(kit *rest.Kit, bizID
 	}
 	return nil
 }
+
+func (st *setTemplate) ListSetTemplateSyncStatus(kit *rest.Kit, bizID int64,
+	option metadata.ListSetTemplateSyncStatusOption) (metadata.MultipleSetTemplateSyncStatus, errors.CCErrorCoder) {
+
+	responseInfo := metadata.MultipleSetTemplateSyncStatus{}
+
+	filterTemp := &metadata.QueryCondition{
+		Page:      option.Page,
+		Condition: mapstr.MapStr{common.BKSetTemplateIDField: option.SetTemplateID, common.BKAppIDField: bizID},
+	}
+	if len(option.SetIDs) != 0 {
+		filterTemp.Condition[common.BKSetIDField] = mapstr.MapStr{common.BKDBIN: option.SetIDs}
+	}
+	filterTemp.Fields = []string{common.BKSetIDField, common.BKSetNameField, common.BkSupplierAccount}
+
+	var setInfoResp metadata.ResponseSetInstance
+	err := st.client.CoreService().Instance().ReadInstanceStruct(kit.Ctx, kit.Header,
+		common.BKInnerObjIDSet, filterTemp, &setInfoResp)
+	if err != nil {
+		blog.ErrorJSON("ListSetTemplateSyncStatus failed, core service find set template failed,"+
+			" option: %s, err: %s, rid: %s", filterTemp, err, kit.Rid)
+		return metadata.MultipleSetTemplateSyncStatus{}, err
+	}
+	if err := setInfoResp.CCError(); err != nil {
+		blog.ErrorJSON("ListSetTemplateSyncStatus failed, core service find set template http reply failed,"+
+			" option: %s, err: %s, rid: %s", filterTemp, err, kit.Rid)
+		return metadata.MultipleSetTemplateSyncStatus{}, err
+	}
+
+	responseInfo.Count = int64(setInfoResp.Data.Count)
+	setIDs := make([]int64, len(setInfoResp.Data.Info))
+	responseInfo.Info = make([]metadata.SetTemplateSyncStatus, len(setInfoResp.Data.Info))
+
+	for idx, setInfo := range setInfoResp.Data.Info {
+		setIDs[idx] = setInfo.SetID
+		// setInfoResp 只返回了部分字段，新加字段注意修改
+		responseInfo.Info[idx] = metadata.SetTemplateSyncStatus{
+			SetID:           setInfo.SetID,
+			Name:            setInfo.SetName,
+			BizID:           bizID,
+			SetTemplateID:   option.SetTemplateID,
+			SupplierAccount: setInfo.SupplierAccount,
+		}
+	}
+
+	// 使用存在模块
+	option.SetIDs = setIDs
+	result, err := st.client.CoreService().SetTemplate().ListSetTemplateSyncStatus(kit.Ctx, kit.Header, bizID, option)
+	if err != nil {
+		blog.ErrorJSON("ListSetTemplateSyncStatus failed, core service search failed, option: %s, err: %s, rid: %s",
+			option, err.Error(), kit.Rid)
+		return metadata.MultipleSetTemplateSyncStatus{}, err
+	}
+
+	setTempSyncMap := make(map[int64]metadata.SetTemplateSyncStatus, len(result.Info))
+	// 处理当前需要同步任务的状态
+	for _, info := range result.Info {
+		setTempSyncMap[info.SetID] = info
+		if !info.Status.IsFinished() {
+			go func(info metadata.SetTemplateSyncStatus) {
+				st.TriggerCheckSetTemplateSyncingStatus(kit.NewKit(), info.BizID, info.SetTemplateID, info.SetID)
+			}(info)
+		}
+
+	}
+	// 如果在同步表中有数据，使用同步表中的数据
+	for idx, row := range responseInfo.Info {
+		if newRow, ok := setTempSyncMap[row.SetID]; ok {
+			responseInfo.Info[idx] = newRow
+		}
+	}
+
+	return responseInfo, nil
+
+}
