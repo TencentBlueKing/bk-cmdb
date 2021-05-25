eedf06688d3fdb01c874b69e3412e2ee5b5e902b
mrmgxxxx <meloji@tencent.com>
2021-05-25 14:38:42 +0800

    fix: recover event server tests build. issue #5272

--- src/test/event_server/event_server_suite_test.go
+++ src/test/event_server/event_server_suite_test.go
@@ -1,1 +1,32 @@
 package event_server_test
+
+import (
+	"testing"
+
+	"configcenter/src/test"
+	"configcenter/src/test/reporter"
+	"configcenter/src/test/util"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var header = test.GetHeader()
+var clientSet = test.GetClientSet()
+var eventServerClient = clientSet.EventServer()
+
+func TestEventServer(t *testing.T) {
+	RegisterFailHandler(util.Fail)
+	reporters := []Reporter{
+		reporter.NewHtmlReporter(test.GetReportDir()+"eventserver.html", test.GetReportUrl(), true),
+	}
+	RunSpecsWithDefaultAndCustomReporters(t, "EventServer Suite", reporters)
+}
+
+var _ = BeforeSuite(func() {
+	test.ClearDatabase()
+})
+
+var _ = Describe("event server test", func() {
+	// NOTE: add more tests here.
+})
