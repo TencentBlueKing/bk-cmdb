40186e02a510857ef09acb674cd91d9d17222cc8
rentiansheng <rentiansheng@163.com>
2021-06-17 14:25:00 +0800

    feature: sync db table index issue #5085

--- src/scene_server/admin_server/service/service.go
+++ src/scene_server/admin_server/service/service.go
@@ -85,6 +85,7 @@ func (s *Service) WebService() *restful.Container {
 	api.Route(api.POST("/migrate/specify/version/{distribution}/{ownerID}").To(s.migrateSpecifyVersion))
 	api.Route(api.POST("/migrate/config/refresh").To(s.refreshConfig))
 	api.Route(api.POST("/migrate/dataid").To(s.migrateDataID))
+	api.Route(api.POST("/migrate/sync/db/index").To(s.RunSyncDBIndex))
 	api.Route(api.GET("/healthz").To(s.Healthz))
 
 	container.Add(api)
