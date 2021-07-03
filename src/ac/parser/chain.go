367c5d8a196b1fbf55c4e51479fc514c4e72448a
chalice-1831 <844589474@qq.com>
2021-07-03 16:06:40 +0800

    feature 优化ac匹配路由效率

--- src/ac/parser/chain.go
+++ src/ac/parser/chain.go
@@ -6,12 +6,12 @@ import (
 	"net/http"
 	"regexp"
 
+	"github.com/tidwall/gjson"
+
 	"configcenter/src/ac/meta"
 	"configcenter/src/common"
 	"configcenter/src/common/backbone"
 	"configcenter/src/common/blog"
-
-	"github.com/tidwall/gjson"
 )
 
 type RequestContext struct {
@@ -92,16 +92,7 @@ func (ps *parseStream) Parse() (*meta.AuthAttribute, error) {
 		validateVersion().
 		validateResourceAction().
 		validateUserAndSupplier().
-		cacheRelated().
-		adminRelated().
-		hostRelated().
-		topology().
-		topologyLatest().
-		netCollectorRelated().
-		processRelated().
-		eventRelated().
-		cloudRelated().
-		// finalizer must be at the end of the check chains.
+		matchAuthRoute().
 		finalizer()
 
 	if ps.err != nil {
