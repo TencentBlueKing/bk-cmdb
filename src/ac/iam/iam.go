2319f35c06981da0cfd51473a4b009ed7bdc0f60
ZQHcode <1550050864@qq.com>
2021-10-20 11:00:55 +0800

    fix: CI warnings

--- src/ac/iam/iam.go
+++ src/ac/iam/iam.go
@@ -35,6 +35,7 @@ type IAM struct {
 	Client iamClientInterface
 }
 
+// NewIAM new iam client
 func NewIAM(tls *util.TLSClientConfig, cfg AuthConfig, reg prometheus.Registerer) (*IAM, error) {
 	blog.V(5).Infof("new iam with parameters tls: %+v, cfg: %+v", tls, cfg)
 	if !auth.EnableAuthorize() {
@@ -71,6 +72,7 @@ func NewIAM(tls *util.TLSClientConfig, cfg AuthConfig, reg prometheus.Registerer
 	}, nil
 }
 
+// RegisterSystem register system to iam
 func (i IAM) RegisterSystem(ctx context.Context, host string, objects []metadata.Object) error {
 	if !auth.EnableAuthorize() {
 		return nil
@@ -648,15 +650,18 @@ type authorizer struct {
 	authClientSet authserver.AuthServerClientInterface
 }
 
+// NewAuthorizer new authorizer
 func NewAuthorizer(clientSet apimachinery.ClientSetInterface) *authorizer {
 	return &authorizer{authClientSet: clientSet.AuthServer()}
 }
 
+// AuthorizeBatch batch authorization will not pass if one of them does not have permission
 func (a *authorizer) AuthorizeBatch(ctx context.Context, h http.Header, user meta.UserInfo,
 	resources ...meta.ResourceAttribute) ([]types.Decision, error) {
 	return a.authorizeBatch(ctx, h, true, user, resources...)
 }
 
+// AuthorizeAnyBatch batch authorization will pass if one of them has permission
 func (a *authorizer) AuthorizeAnyBatch(ctx context.Context, h http.Header, user meta.UserInfo,
 	resources ...meta.ResourceAttribute) ([]types.Decision, error) {
 	return a.authorizeBatch(ctx, h, false, user, resources...)
@@ -775,16 +780,19 @@ func (a *authorizer) ListAuthorizedResources(ctx context.Context, h http.Header,
 	return a.authClientSet.ListAuthorizedResources(ctx, h, input)
 }
 
+// GetNoAuthSkipUrl get no auth skip url
 func (a *authorizer) GetNoAuthSkipUrl(ctx context.Context, h http.Header,
 	input *metadata.IamPermission) (string, error) {
 	return a.authClientSet.GetNoAuthSkipUrl(ctx, h, input)
 }
 
+// GetPermissionToApply get permission to apply
 func (a *authorizer) GetPermissionToApply(ctx context.Context, h http.Header,
 	input []meta.ResourceAttribute) (*metadata.IamPermission, error) {
 	return a.authClientSet.GetPermissionToApply(ctx, h, input)
 }
 
+// RegisterResourceCreatorAction register resourceCreator Action
 func (a *authorizer) RegisterResourceCreatorAction(ctx context.Context, h http.Header,
 	input metadata.IamInstanceWithCreator) (
 	[]metadata.IamCreatorActionPolicy, error) {
@@ -792,6 +800,7 @@ func (a *authorizer) RegisterResourceCreatorAction(ctx context.Context, h http.H
 	return a.authClientSet.RegisterResourceCreatorAction(ctx, h, input)
 }
 
+// BatchRegisterResourceCreatorAction batch register resourceCreator action
 func (a *authorizer) BatchRegisterResourceCreatorAction(ctx context.Context, h http.Header,
 	input metadata.IamInstancesWithCreator) (
 	[]metadata.IamCreatorActionPolicy, error) {
