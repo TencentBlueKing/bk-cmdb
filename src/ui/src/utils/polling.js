34613262917c54dcc8e679971fdb41a676fe1dc2
liuli <liumt1993@gmail.com>
2021-11-05 15:41:57 +0800

    fix: 补充注释优化轮询代码

--- src/ui/src/utils/polling.js
+++ src/ui/src/utils/polling.js
@@ -9,37 +9,38 @@ class Polling {
     this.callback = callback
     this.duration = duration
     this.isStop = true // 轮询停止标识，true 为停止，false 为轮询中。
-    this.start()
   }
 
   // 启动轮询
   start() {
-    // 避免多次开启同一个轮询器会重复开启多个 timer，所以默认进来就停止之前的 timer。
-    this.stop()
-
+    if (this.pollingTimer) return false
     this.isStop = false
 
     try {
       const pull = () => {
+        // 停掉以后，避免再往队列里面插入新任务。
         if (this.isStop) return false
+
         this.pollingTimer = setTimeout(() => {
+          // 有可能轮询已经停止了，但是还有任务在队列中，所以需要告知正在队列中的任务，你可以停下来了！
+          if (this.isStop) return false
           this.callback()
           pull()
         }, this.duration)
       }
 
-      setTimeout(pull, this.duration)
+      this.pollingTimer = setTimeout(pull, this.duration)
     } catch (e) {
       this.stop()
       throw Error(e)
     }
   }
 
-  // 停止轮询，在没有引用时也会销毁轮询
+  // 停止轮询
   stop() {
-    this.isStop = true
     this.pollingTimer = null
     clearTimeout(this.pollingTimer)
+    this.isStop = true
   }
 }
 
