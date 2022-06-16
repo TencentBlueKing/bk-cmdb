6faf3b2bd751db2fa1c89a35c27f0734a1119606
yangyy <yang.bele@gmail.com>
2022-06-16 10:24:00 +0800

    fix: 关闭构建流程中的eslint检查解决过时依赖包兼容问题

--- src/ui/config/index.js
+++ src/ui/config/index.js
@@ -55,7 +55,7 @@ const dev = {
     // Use Eslint Loader?
     // If true, your code will be linted during bundling and
     // linting errors and warnings will be shown in the console.
-    useEslint: true,
+    useEslint: false,
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
     showEslintErrorsInOverlay: true,
