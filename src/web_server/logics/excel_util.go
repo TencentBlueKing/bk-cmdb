2d50e48ac697adf59917cacdde7fd8341067a9ad
laodiu <1024371919@qq.com>
2022-06-20 20:10:00 +0800

    调整error和rid位置

--- src/web_server/logics/excel_util.go
+++ src/web_server/logics/excel_util.go
@@ -99,8 +99,8 @@ func checkExcelHeader(ctx context.Context, sheet *xlsx.Sheet, fields map[string]
 	// valid excel three row is instance property fields
 	// indicating that the third line of the excel template was deleted
 	if len(sheet.Rows[headerRow-1].Cells) < 2 && isCheckHeader {
-		blog.Errorf("err: %v, no found fields %s, rid: %s", defLang.Language("web_import_field_not_found"),
-			strings.Join(errCells, ","), rid)
+		blog.Errorf("not found fields %s, err: %v, rid: %s", strings.Join(errCells, ","),
+			defLang.Language("web_import_field_not_found"), rid)
 		return ret, errors.New(defLang.Language("web_import_field_not_found"))
 	}
 
