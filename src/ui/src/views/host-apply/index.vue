d442f4de0c0e929e77c5e10b7995af47af103293
yangyy <yang.bele@gmail.com>
2022-07-20 16:08:45 +0800

    fix: 修复自动应用当模块所属模板已配置规则时跳转查看的模板规则数据未更新问题

--- src/ui/src/views/host-apply/index.vue
+++ src/ui/src/views/host-apply/index.vue
@@ -199,8 +199,11 @@
           this.$refs.details.reset()
         }
 
-        // 业务拓扑模式且模板配置了自动应用，则不需要请求数据
+        // 业务拓扑模式且模板配置了自动应用，则不需要请求数据，但需要将规则数据重置
         if (this.isModuleMode && this.currentNode.service_template_host_apply_enabled) {
+          this.initRuleList = []
+          this.hasRule = false
+          this.checkedPropertyIdList = []
           return
         }
 
