ef7f1a34c32cb9cc92232c5d832ec9bdf1bb66fd
linzhihan <1320679481@qq.com>
2023-02-15 16:13:51 +0800

    fix: 项目页面查看权限暂时无需限制

--- src/ui/src/views/project/index.vue
+++ src/ui/src/views/project/index.vue
@@ -87,7 +87,7 @@
       </bk-table-column>
       <bk-table-column :label="$t('操作')" fixed="right">
         <template slot-scope="{ row }">
-          <cmdb-auth @click.native.stop :auth="{ type: $OPERATION.BUSINESS_ARCHIVE, relation: [row.id] }">
+          <cmdb-auth @click.native.stop :auth="{ type: $OPERATION.U_PROJECT }">
             <template slot-scope="{ disabled }">
               <bk-popconfirm
                 v-if="row.bk_status === 'disabled'"
@@ -122,24 +122,9 @@
 
       <cmdb-table-empty
         slot="empty"
+        :auth="{ type: $OPERATION.C_INST, relation: [model.id] }"
         :stuff="table.stuff"
-        :auth="{ type: $OPERATION.C_PROJECT }">
-        <i18n path="项目列表提示语" class="table-empty-tips">
-          <template #auth>
-            <bk-link theme="primary" @click="handleApplyPermission">{{$t('申请查看权限')}}</bk-link>
-          </template>
-          <template #create>
-            <cmdb-auth :auth="{ type: $OPERATION.C_PROJECT }">
-              <bk-button slot-scope="{ disabled }" text
-                theme="primary"
-                class="text-btn"
-                :disabled="disabled"
-                @click="handleCreate">
-                {{$t('立即创建')}}
-              </bk-button>
-            </cmdb-auth>
-          </template>
-        </i18n>
+        @create="handleCreate">
       </cmdb-table-empty>
     </bk-table>
 
@@ -219,7 +204,6 @@
 </template>
 
 <script>
-  import { translateAuth } from '@/setup/permission'
   import { mapState, mapActions, mapGetters } from 'vuex'
   import RouterQuery from '@/router/query'
   import throttle from 'lodash.throttle'
@@ -645,18 +629,6 @@
           .finally(() => {
             this.batchUpdateSlider.loading = false
           })
-      },
-      async handleApplyPermission() {
-        try {
-          const permission = translateAuth({
-            type: this.$OPERATION.R_PROJECT,
-            relation: []
-          })
-          const url = await this.$store.dispatch('auth/getSkipUrl', { params: permission })
-          window.open(url)
-        } catch (e) {
-          console.error(e)
-        }
       },
       handleConfirm(status, row) {
         const params = {
