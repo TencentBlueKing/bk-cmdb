56d5c2ee538bebe5c3a93ebe999ce81f5d07be90
dlutstone <dlutstone@hotmail.com>
2023-03-16 20:36:19 +0800

    代码规范

--- src/common/pidfile.go
+++ src/common/pidfile.go
@@ -28,7 +28,7 @@ var pidfile string
 
 func init() {
 	cwd, err := os.Getwd()
-	if nil != err {
+	if err !=nil  {
 		blog.Errorf("get current path failed. Error:%s", err.Error())
 	}
 	pidfile = cwd + "/pid/" + filepath.Base(os.Args[0]) + ".pid"
