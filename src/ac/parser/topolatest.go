f93099ddde46ed2ddb5239af2f105970267f2439
wjyfelen <wjyfelen@163.com>
2023-04-18 17:32:21 +0800

    增加路由匹配 (#6843)

--- src/ac/parser/topolatest.go
+++ src/ac/parser/topolatest.go
@@ -2366,8 +2366,10 @@ func (ps *parseStream) objectAttributeGroupLatest() *parseStream {
 }
 
 const (
-	createObjectAttributeLatestPattern   = "/api/v3/create/objectattr"
-	findObjectAttributeLatestPattern     = "/api/v3/find/objectattr"
+	createObjectAttributeLatestPattern  = "/api/v3/create/objectattr"
+	findObjectAttributeLatestPattern    = "/api/v3/find/objectattr"
+	findObjectAttributeForLatestPattern = "/api/v3/find/objectattr/web"
+
 	findHostObjectAttributeLatestPattern = "/api/v3/find/objectattr/host"
 )
 
@@ -2593,6 +2595,42 @@ func (ps *parseStream) objectAttributeLatest() *parseStream {
 		return ps
 	}
 
+	// get object's attribute operation.
+	if ps.hitPattern(findObjectAttributeForLatestPattern, http.MethodPost) {
+		val, err := ps.RequestCtx.getValueFromBody(common.BKObjIDField)
+		if err != nil {
+			ps.err = err
+			return ps
+		}
+		modelCond := val.Value()
+		models, err := ps.searchModels(mapstr.MapStr{common.BKObjIDField: modelCond})
+		if err != nil {
+			ps.err = err
+			return ps
+		}
+
+		bizID, err := ps.RequestCtx.getBizIDFromBody()
+		if err != nil {
+			ps.err = err
+			return ps
+		}
+
+		for _, model := range models {
+			ps.Attribute.Resources = append(ps.Attribute.Resources,
+				meta.ResourceAttribute{
+					// 注意：业务ID是否为0表示两种不同的操作
+					// case 0: 读取模型的公有属性
+					// case ~0: 读取业务私有属性+公有属性
+					BusinessID: bizID,
+					Basic: meta.Basic{
+						Type:   meta.ModelAttribute,
+						Action: meta.FindMany,
+					},
+					Layers: []meta.Item{{Type: meta.Model, InstanceID: model.ID}},
+				})
+		}
+		return ps
+	}
 	if ps.hitPattern(findHostObjectAttributeLatestPattern, http.MethodPost) {
 		model, err := ps.getOneModel(mapstr.MapStr{common.BKObjIDField: common.BKInnerObjIDHost})
 		if err != nil {
