5656cdc1f6a1db38b299ca86e3b33b64059c11e9
ZQHcode <51312405+ZQHcode@users.noreply.github.com>
2023-04-19 17:12:24 +0800

    修复选择主机id导出时没有分页的问题 (#6851)

--- src/web_server/logics/excel.go
+++ src/web_server/logics/excel.go
@@ -185,7 +185,7 @@ func (lgc *Logics) BuildHostExcelFromData(c *gin.Context, objID string, fields m
 		}
 
 		if len(hostInfo) == 0 {
-			continue
+			return nil
 		}
 
 		hostInfo, err = lgc.HandleExportEnumQuoteInst(c, header, hostInfo, objID, fields, rid)
