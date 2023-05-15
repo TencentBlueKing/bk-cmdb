f5fbf3e1bee7321cda529abb9a55ba7711ac6ef3
扬扬扬扬扬 <2269766985@qq.com>
2023-05-15 17:14:01 +0800

    feature: add list field template api. (#6984)
    * feature: add list field template api.

---
+++ src/scene_server/topo_server/service/field_template/field_template.go
@@ -0,0 +1,49 @@
+/*
+ * Tencent is pleased to support the open source community by making
+ * 蓝鲸智云 - 配置平台 (BlueKing - Configuration System) available.
+ * Copyright (C) 2017 THL A29 Limited,
+ * a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ * We undertake not to change the open source license (MIT license) applicable
+ * to the current version of the project delivered to anyone in the future.
+ */
+
+package fieldtmpl
+
+import (
+	"configcenter/src/common/blog"
+	"configcenter/src/common/http/rest"
+	"configcenter/src/common/metadata"
+)
+
+// ListFieldTemplate list field templates.
+// NOTICE: this api only returns basic info of field template, do not need to authorize
+func (s *service) ListFieldTemplate(cts *rest.Contexts) {
+	opt := new(metadata.CommonQueryOption)
+	if err := cts.DecodeInto(opt); err != nil {
+		cts.RespAutoError(err)
+		return
+	}
+
+	if rawErr := opt.Validate(); rawErr.ErrCode != 0 {
+		cts.RespAutoError(rawErr.ToCCError(cts.Kit.CCError))
+		return
+	}
+
+	// list field templates
+	res, err := s.clientSet.CoreService().FieldTemplate().ListFieldTemplate(cts.Kit.Ctx, cts.Kit.Header, opt)
+	if err != nil {
+		blog.Errorf("list field templates failed, err: %v, req: %+v, rid: %s", err, opt, cts.Kit.Rid)
+		cts.RespAutoError(err)
+		return
+	}
+
+	cts.RespEntity(res)
+}
