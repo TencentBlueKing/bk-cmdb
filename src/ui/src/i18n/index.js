c38012d5213739ab6a0c76cfa42cbacdcb6b3fa4
yangyy <yang.bele@gmail.com>
2023-07-06 16:48:48 +0800

    feat: 添加desktopUrl配置

--- src/ui/src/i18n/index.js
+++ src/ui/src/i18n/index.js
@@ -45,8 +45,8 @@ export const changeLocale = async (locale) => {
     domain: siteConfig?.cookieDomain || window.location.hostname.replace(/^.*(\.[^.]+\.[^.]+)$/, '$1'),
   })
 
-  if (siteConfig?.esbUrl) {
-    const url = `${siteConfig.esbUrl}/api/c/compapi/v2/usermanage/fe_update_user_language/`
+  if (siteConfig?.desktopUrl) {
+    const url = `${siteConfig.desktopUrl}/api/c/compapi/v2/usermanage/fe_update_user_language/`
     try {
       await jsonp(url, { language: cookieValue })
     } finally {
