9bd2f01ac5f3154c26fb4cb148c040c50872e7f2
ZQHcode <1550050864@qq.com>
2024-03-01 15:29:38 +0800

    操作系统类型新增"MacOS"

--- src/scene_server/admin_server/imports.go
+++ src/scene_server/admin_server/imports.go
@@ -104,4 +104,5 @@ import (
 	_ "configcenter/src/scene_server/admin_server/upgrader/y3.13.202401221600"
 	_ "configcenter/src/scene_server/admin_server/upgrader/y3.13.202402181900"
 	_ "configcenter/src/scene_server/admin_server/upgrader/y3.13.202402191140"
+	_ "configcenter/src/scene_server/admin_server/upgrader/y3.13.202402281158"
 )
