3f27e3f964a6f3401f75c28523884f11cfe9f49d
chen.zhang <422734124@qq.com>
2024-05-13 15:59:27 +0800

    feat: 主机和通用实例表格支持shift一次性选择多个项 (#7970)

--- src/ui/src/views/general-model/index.vue
+++ src/ui/src/views/general-model/index.vue
@@ -109,6 +109,7 @@
       :data="table.list"
       :pagination="table.pagination"
       :max-height="$APP.height - filterTagHeight - 190"
+      :shift-multi-checked="true"
       @sort-change="handleSortChange"
       @page-limit-change="handleSizeChange"
       @page-change="handlePageChange"
