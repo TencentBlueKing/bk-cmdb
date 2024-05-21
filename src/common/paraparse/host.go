c8fdcb745b5bf4108e72c22ddf13c43b023ba035
wcy00000000000000 <2269766985@qq.com>
2024-05-21 16:14:47 +0800

    fix: search host problem

--- src/common/paraparse/host.go
+++ src/common/paraparse/host.go
@@ -38,7 +38,12 @@ func ParseHostParams(input []metadata.ConditionItem) (map[string]interface{}, er
 	output := make(map[string]interface{})
 	for _, i := range input {
 		switch i.Operator {
-		case common.BKDBEQ, common.BKDBNE:
+		case common.BKDBEQ:
+			output[i.Field], err = common.ConvertIpv6ToFullWord(i.Field, i.Value)
+			if err != nil {
+				return nil, err
+			}
+		case common.BKDBNE:
 			value, err := common.ConvertIpv6ToFullWord(i.Field, i.Value)
 			if err != nil {
 				return nil, err
