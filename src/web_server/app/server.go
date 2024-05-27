27dbaec7e8c1a34cae25060533edc8ea1aa115a8
wcy00000000000000 <2269766985@qq.com>
2024-05-27 14:50:12 +0800

    feat: add bkRepoUrl config

--- src/web_server/app/server.go
+++ src/web_server/app/server.go
@@ -153,6 +153,7 @@ func (w *WebServer) onServerConfigUpdate(previous, current cc.ProcessConfig) {
 	w.Config.Site.BkDomain, _ = cc.String("webServer.site.bkDomain")
 	w.Config.Site.HelpDocUrl, _ = cc.String("webServer.site.helpDocUrl")
 	w.Config.Site.BkComponentApiUrl, _ = cc.String("webServer.site.bkComponentApiUrl")
+	w.Config.Site.BkRepoUrl, _ = cc.String("webServer.site.bkRepoUrl")
 
 	w.Config.Session.Name, _ = cc.String("webServer.session.name")
 	w.Config.Session.MultipleOwner, _ = cc.String("webServer.session.multipleOwner")
