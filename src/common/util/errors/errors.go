99111233b413ba01d6835a7f291f14e736f494be
扬扬扬扬扬 <2269766985@qq.com>
2024-08-12 12:09:54 +0800

    fix: some problems (#8140)
    --story=119042538

---
+++ src/common/util/errors/errors.go
@@ -0,0 +1,40 @@
+/*
+ * Tencent is pleased to support the open source community by making
+ * 蓝鲸智云 - 配置平台 (BlueKing - Configuration System) available.
+ * Copyright (C) 2017 THL A29 Limited,
+ * a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ * We undertake not to change the open source license (MIT license) applicable
+ * to the current version of the project delivered to anyone in the future.
+ */
+
+// Package errors is error related util package
+package errors
+
+import (
+	"configcenter/src/common"
+	"configcenter/src/common/errors"
+	"configcenter/src/common/http/rest"
+	"configcenter/src/storage/dal"
+	"configcenter/src/storage/driver/mongodb"
+)
+
+// ConvDBInsertError convert db insert error to
+func ConvDBInsertError(kit *rest.Kit, db dal.RDB, err error) errors.CCErrorCoder {
+	if err == nil {
+		return nil
+	}
+
+	if db.IsDuplicatedError(err) {
+		return kit.CCError.CCErrorf(common.CCErrCommDuplicateItem, mongodb.GetDuplicateKey(err))
+	}
+
+	return kit.CCError.CCError(common.CCErrCommDBInsertFailed)
+}
