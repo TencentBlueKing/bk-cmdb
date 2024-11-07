8da9a660ec3f7726ead10c74c0acce4c191349ed
YuanWang <102893179+bd-xiaowang@users.noreply.github.com>
2024-11-07 16:05:00 +0800

    删除内置业务集'全业务-蓝盾测试部署专用' (#8250)
    --story=120221179

--- src/scene_server/admin_server/imports.go
+++ src/scene_server/admin_server/imports.go
@@ -96,12 +96,10 @@ import (
 	// v3.12.x
 	_ "configcenter/src/scene_server/admin_server/upgrader/y3.12.202309211735"
 	_ "configcenter/src/scene_server/admin_server/upgrader/y3.12.202310301633"
-	_ "configcenter/src/scene_server/admin_server/upgrader/y3.12.202310302130"
 	_ "configcenter/src/scene_server/admin_server/upgrader/y3.12.202311061800"
 
 	// v3.13.x
 	_ "configcenter/src/scene_server/admin_server/upgrader/y3.13.202401221600"
-	_ "configcenter/src/scene_server/admin_server/upgrader/y3.13.202402181900"
 	_ "configcenter/src/scene_server/admin_server/upgrader/y3.13.202402191140"
 	_ "configcenter/src/scene_server/admin_server/upgrader/y3.13.202402281158"
 	_ "configcenter/src/scene_server/admin_server/upgrader/y3.13.202403151855"
