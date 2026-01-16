a7f022bab2b660dcd319e77e8d8ff4e70cf2e4e3
chen.zhang <422734124@qq.com>
2026-01-16 11:02:28 +0800

    fix: 文档链接更新 --bug=152511659

--- src/ui/src/components/layout/header.vue
+++ src/ui/src/components/layout/header.vue
@@ -134,7 +134,7 @@
       ...mapGetters('objectBiz', ['bizId']),
       ...mapGetters('globalConfig', ['config']),
       helpDocUrl() {
-        return `${this.$Site.helpDocUrl}/markdown/CMDB/UserGuide/Introduce/Overview.md`
+        return `${this.$Site.helpDocUrl}/markdown/ZH/CMDB/3.10/UserGuide/Introduce/Overview.md`
       },
       visibleMenu() {
         return menu.filter((menuItem) => {
