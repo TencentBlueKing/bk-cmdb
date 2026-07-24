03dc275d8b895cfd3744be4a51624ade84afb516
扬扬扬扬扬 <2269766985@qq.com>
2026-07-24 10:58:00 +0800

    更新helm chart的bitnami仓库地址 (#8678)
    * docs: change helm bitnami repo
    
    --story=136294948
    
    * fix: set & module sync creator not returned
    
    --bug=160953493

--- src/scene_server/topo_server/logics/settemplate/sync_status.go
+++ src/scene_server/topo_server/logics/settemplate/sync_status.go
@@ -386,7 +386,7 @@ func (st *setTemplate) rearrangeSetTempSyncStatus(kit *rest.Kit, option *metadat
 	}
 
 	setOpt := &metadata.QueryCondition{
-		Fields: []string{common.BKSetIDField, common.CreatorField, common.CreateTimeField,
+		Fields: []string{common.BKSetIDField, common.BKCreatedBy, common.CreateTimeField,
 			common.LastTimeField},
 		Page:           metadata.BasePage{Limit: common.BKNoLimit},
 		Condition:      mapstr.MapStr{common.BKSetIDField: mapstr.MapStr{common.BKDBIN: compensateSetIDs}},
@@ -406,7 +406,7 @@ func (st *setTemplate) rearrangeSetTempSyncStatus(kit *rest.Kit, option *metadat
 	for _, set := range setRes.Data.Info {
 		status := metadata.APITaskSyncStatus{
 			InstID:     set.SetID,
-			Creator:    set.Creator,
+			Creator:    set.CreatedBy,
 			CreateTime: set.CreateTime.Time,
 			LastTime:   set.LastTime.Time,
 		}
